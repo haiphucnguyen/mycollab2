c4056373c3963f9b609d66713e7cf6b1e2556973
Nghi Tran <nghitra@esofthead.com>
2012-11-07 08:40:43 +0700

    Set window caption

--- mycollab-web/src/main/java/com/esofthead/mycollab/web/MainWindowContainer.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/web/MainWindowContainer.java
@@ -1,13 +1,12 @@
 package com.esofthead.mycollab.web;
 
 import com.vaadin.ui.Window;
-import com.vaadin.ui.themes.ChameleonTheme;
-import com.vaadin.ui.themes.Reindeer;
 
 public class MainWindowContainer extends Window {
 	private static final long serialVersionUID = 1L;
 
 	public MainWindowContainer() {
-//		this.setStyleName(ChameleonTheme.);
+		// this.setStyleName(ChameleonTheme.);
+		this.setCaption("MyCollab");
 	}
 }
