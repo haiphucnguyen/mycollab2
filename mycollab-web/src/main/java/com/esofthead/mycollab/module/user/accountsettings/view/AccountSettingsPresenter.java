5861b3aec269c10c5149b8cb783a78f01087a922
Nghi Tran <nghitran@esofthead.com>
2012-11-30 10:12:15 +0700

    add AccountSettingPresenter

---
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/user/accountsettings/view/AccountSettingsPresenter.java
@@ -0,0 +1,19 @@
+package com.esofthead.mycollab.module.user.accountsettings.view;
+
+import com.esofthead.mycollab.vaadin.mvp.Presenter;
+import com.vaadin.ui.ComponentContainer;
+
+public class AccountSettingsPresenter implements Presenter {
+
+	private final AccountSettingsView view;
+
+	public AccountSettingsPresenter(AccountSettingsView view) {
+		this.view = view;
+	}
+
+	@Override
+	public void go(ComponentContainer container) {
+		// TODO Auto-generated method stub
+
+	}
+}
