22e613a611e282f3231bcace223acdaa2a1c5149
Nguyen Hai <hainguyen@esofthead.com>
2012-12-10 13:51:31 +0700

    Add field in search criteria

--- mycollab-core/src/main/java/com/esofthead/mycollab/core/arguments/BooleanSearchField.java
+++ mycollab-core/src/main/java/com/esofthead/mycollab/core/arguments/BooleanSearchField.java
@@ -1,14 +1,25 @@
 package com.esofthead.mycollab.core.arguments;
 
 public class BooleanSearchField extends SearchField {
+    
+        public static String IS = "is";
+        
+        public static String NOT = "is not";
+    
 	private boolean value;
 	
+        private String comparision;
 	
 	
 	public BooleanSearchField(String oper, boolean value) {
-		this.operation = oper;
-		this.value = value;
+		this(oper, BooleanSearchField.IS, value);
 	}
+        
+        public BooleanSearchField(String oper, String comparision, boolean value) {
+            this.operation = oper;
+            this.comparision = comparision;
+            this.value = value;
+        }
 
 	public boolean isValue() {
 		return value;
--- mycollab-core/src/main/java/com/esofthead/mycollab/core/arguments/DateSearchField.java
+++ mycollab-core/src/main/java/com/esofthead/mycollab/core/arguments/DateSearchField.java
@@ -3,19 +3,38 @@ package com.esofthead.mycollab.core.arguments;
 import java.util.Date;
 
 public class DateSearchField extends SearchField {
-	private Date value;
 
-	public DateSearchField(String oper, Date value) {
-		this.operation = oper;
-		this.value = value;
-	}
+    public static String LESSTHAN = "<";
+    public static String LESSTHANEQUAL = "<=";
+    public static String GREATERTHAN = ">";
+    public static String GRREATERTHANEQUAL = ">=";
+    private Date value;
+    private String comparision;
 
-	public Date getValue() {
-		return value;
-	}
+    public DateSearchField(String oper, Date value) {
+        this(oper, DateSearchField.LESSTHAN, value);
+    }
 
-	public void setValue(Date value) {
-		this.value = value;
-	}
+    public DateSearchField(String oper, String comparision, Date value) {
+        this.operation = oper;
+        this.value = value;
+        this.comparision = comparision;
+    }
+
+    public Date getValue() {
+        return value;
+    }
+
+    public void setValue(Date value) {
+        this.value = value;
+    }
+
+    public String getComparision() {
+        return comparision;
+    }
+
+    public void setComparision(String comparision) {
+        this.comparision = comparision;
+    }
 
 }
