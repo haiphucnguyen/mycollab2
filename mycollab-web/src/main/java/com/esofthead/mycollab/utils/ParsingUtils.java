f20778e2bbbf0f369eb81bc8f869bd129c7c1fe4
linh nguyen <linhnguyen@esofthead.com>
2012-12-17 15:43:17 +0700

    valid parse email

--- mycollab-web/src/main/java/com/esofthead/mycollab/utils/ParsingUtils.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/utils/ParsingUtils.java
@@ -3,22 +3,49 @@ package com.esofthead.mycollab.utils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.esofthead.mycollab.common.domain.MailRecipientField;
 
 public class ParsingUtils {
+	
+	private static final String EMAIL_PATTERN = 
+		"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+		+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+	
 	public static List<MailRecipientField> parseEmailField(String emailField)
 			throws InvalidEmailException {
+		
+		Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+		Matcher matcherEmail;
 		if (emailField == null || emailField.trim().equals("")) {
-			return null;
+			throw new InvalidEmailException("The email is not valid!");
 		} else {
 			List<MailRecipientField> fields = new ArrayList<MailRecipientField>();
 			StringTokenizer emailTokenizer = new StringTokenizer(emailField,
 					",");
 			while (emailTokenizer.hasMoreElements()) {
-				String email = emailTokenizer.nextToken();
-				MailRecipientField mailField = new MailRecipientField(email);
-				fields.add(mailField);
+				String email = emailTokenizer.nextToken().trim();
+				MailRecipientField mailField = null;
+				if ((email.indexOf("<") > -1) && (email.indexOf(">") == (email.length() - 1)) && (email.indexOf(">") > email.indexOf("<")) ) {
+					String name = email.substring(0, email.indexOf("<"));
+					String mail = email.substring(email.indexOf("<") + 1, email.indexOf(">"));
+					matcherEmail = pattern.matcher(mail);
+					if (matcherEmail.matches() && !name.contains("@")) {
+						mailField = new MailRecipientField(mail, name);
+					}
+				} else {
+					matcherEmail = pattern.matcher(email);
+					if (matcherEmail.matches()) {
+						mailField = new MailRecipientField(email);
+					}
+				}
+				if (mailField != null) {
+					fields.add(mailField);
+				} else {
+					throw new InvalidEmailException("The email is not valid!");
+				}
 			}
 			return fields;
 		}
