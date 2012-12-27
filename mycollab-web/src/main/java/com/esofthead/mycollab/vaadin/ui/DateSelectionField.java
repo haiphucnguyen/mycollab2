918a5516a59743bb36dcd48fdbb74fbdf7c2e021
linh nguyen <linhnguyen@esofthead.com>
2012-12-27 10:48:02 +0700

    update search pane

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/DateSelectionField.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/DateSelectionField.java
@@ -72,74 +72,87 @@ public class DateSelectionField extends GridLayout{
 		}
 	}
 	
-	public SearchField getValues() {
+	public SearchField getValue() {
 		String filterStr = (String) dateSelectionBox.getValue();
 		filterStr = (filterStr != null) ? filterStr : "";
 		Date fDate = (Date) dateStart.getValue();
 		Date tDate = (Date) dateEnd.getValue();
 		
-		if (filterStr.equals(DateSelectionComboBox.EQUAL)) {
-			
-			return new DateSearchField(SearchField.AND, DateSearchField.EQUAL, fDate);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.NOTON)) {
-			
-			return new DateSearchField(SearchField.AND, DateSearchField.NOTEQUAL, fDate);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.AFTER)) {
-			
-			return new DateSearchField(SearchField.AND, DateSearchField.GREATERTHAN, fDate);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.BEFORE)) {
-			
-			return new DateSearchField(SearchField.AND, DateSearchField.LESSTHAN, fDate);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.LAST7DAYS)) {
-			
-			return getLastNumberDays(7);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.NEXT7DAYS)) {
-			
-			return getNextNumberDays(7);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.LAST30DAYS)) {
-			
-			return getLastNumberDays(30);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.NEXT30DAYS)) {
-			
-			return getNextNumberDays(30);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.LASTMONTH)) {
-			
-			return getMonthFilterByDuration(Calendar.getInstance().get(Calendar.MONTH) - 1);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.THISMONTH)) {
-			
-			return getMonthFilterByDuration(Calendar.getInstance().get(Calendar.MONTH));
-			
-		} else if (filterStr.equals(DateSelectionComboBox.NEXTMONTH)) {
-			
-			return getMonthFilterByDuration(Calendar.getInstance().get(Calendar.MONTH) + 1);
-			
-		} else if (filterStr.equals(DateSelectionComboBox.LASTYEAR)) {
-			
-		} else if (filterStr.equals(DateSelectionComboBox.THISMONTH)) {
-			
-		} else if (filterStr.equals(DateSelectionComboBox.NEXTYEAR)) {
-			
-		} else if (filterStr.equals(DateSelectionComboBox.ISBETWEEN)) {
-			
+		if (filterStr.equals(DateSelectionComboBox.ISBETWEEN)) {
+			if (fDate == null || tDate == null) return null;
 			return new RangeDateSearchField(fDate, tDate);
-			
+		}
+		
+		if (filterStr.equals(DateSelectionComboBox.EQUAL)) {
+			if (fDate == null) return null;
+			return new DateSearchField(SearchField.AND, DateSearchField.EQUAL, fDate);
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.NOTON)) {
+			if (fDate == null) return null;
+			return new DateSearchField(SearchField.AND, DateSearchField.NOTEQUAL, fDate);
+		}
+		
+		if (filterStr.equals(DateSelectionComboBox.AFTER)) {
+			if (fDate == null) return null;
+			return new DateSearchField(SearchField.AND, DateSearchField.GREATERTHAN, fDate);
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.BEFORE)) {
+			if (fDate == null) return null;
+			return new DateSearchField(SearchField.AND, DateSearchField.LESSTHAN, fDate);
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.LAST7DAYS)) {
+			return getLastNumberDays(7);
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.NEXT7DAYS)) {
+			return getNextNumberDays(7);
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.LAST30DAYS)) {
+			return getLastNumberDays(30);
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.NEXT30DAYS)) {
+			return getNextNumberDays(30);
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.LASTMONTH)) {
+			return getMonthFilterByDuration(Calendar.getInstance().get(Calendar.MONTH) - 1);
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.THISMONTH)) {
+			return getMonthFilterByDuration(Calendar.getInstance().get(Calendar.MONTH));
+		}
+		
+		if (filterStr.equals(DateSelectionComboBox.NEXTMONTH)) {
+			return getMonthFilterByDuration(Calendar.getInstance().get(Calendar.MONTH) + 1);
+		}
+		
+		if (filterStr.equals(DateSelectionComboBox.LASTYEAR)) {
+			return getYearFilterByDuration(Calendar.getInstance().get(Calendar.YEAR) - 1);
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.THISYEAR)) {
+			return getYearFilterByDuration(Calendar.getInstance().get(Calendar.YEAR));
+		}
+
+		if (filterStr.equals(DateSelectionComboBox.NEXTYEAR)) {
+			return getYearFilterByDuration(Calendar.getInstance().get(Calendar.YEAR) + 1);
 		}
 		
 		return null;
 	}
 	
 	
+	public void setDefaultSelection() {
+		dateSelectionBox.setValue(null);
+	}
+	
 	private RangeDateSearchField getLastNumberDays(int duration) {
-		Date fDate = DateTimeUtils.subtractOrAddDayDuration(new Date(), duration);
+		Date fDate = DateTimeUtils.subtractOrAddDayDuration(new Date(), -duration);
 		Date tDate = new Date();
 		return new RangeDateSearchField(fDate, tDate);
 	}
@@ -150,13 +163,23 @@ public class DateSelectionField extends GridLayout{
 		return new RangeDateSearchField(fDate, tDate);
 	}
 	
+	private RangeDateSearchField getYearFilterByDuration(int yearDuration) {
+		
+		Calendar c = Calendar.getInstance();
+		c.set(yearDuration, 0, 1);
+		int yearMaxDays = c.getActualMaximum(Calendar.DAY_OF_YEAR);
+		System.out.println("days of year " + yearMaxDays);
+		Date fDate = c.getTime();
+		Date tDate = DateTimeUtils.subtractOrAddDayDuration((Date) fDate.clone(), yearMaxDays -1);
+		return new RangeDateSearchField(fDate, tDate);
+	}
 	
 	private RangeDateSearchField getMonthFilterByDuration(int monthDuration) {
 		Calendar c = Calendar.getInstance();
-		int monthMaxDays = c.getActualMaximum(Calendar.DAY_OF_MONTH);
 		c.set(Calendar.getInstance().get(Calendar.YEAR), monthDuration, 1);
+		int monthMaxDays = c.getActualMaximum(Calendar.DAY_OF_MONTH);
 		Date fDate = c.getTime();
-		Date tDate = DateTimeUtils.subtractOrAddDayDuration(new Date(), monthMaxDays);
+		Date tDate = DateTimeUtils.subtractOrAddDayDuration(fDate, monthMaxDays -1);
 		return new RangeDateSearchField(fDate, tDate);
 	}
 	
@@ -183,6 +206,9 @@ public class DateSelectionField extends GridLayout{
 		for (int i = 0; i < this.getColumns(); i++) {
 			removeComponent(i, 1);
 		}
+		
+		dateStart.setValue(new Date());
+		dateStart.setValue(new Date());
 	}
     
     public void setDateFormat(String dateFormat) {
