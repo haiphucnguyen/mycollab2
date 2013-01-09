51b2a677da0da464ac76e3528c2f5d35da8346f4
Nguyen Hai <hainguyen@esofthead.com>
2013-01-09 10:50:20 +0700

    Update common component

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/AdvancedSearchLayout.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/AdvancedSearchLayout.java
@@ -1,4 +1,4 @@
-package com.esofthead.mycollab.module.crm.ui.components;
+package com.esofthead.mycollab.vaadin.ui;
 
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.CustomLayout;
@@ -6,24 +6,24 @@ import com.vaadin.ui.CustomLayout;
 @SuppressWarnings("serial")
 public abstract class AdvancedSearchLayout extends CustomLayout {
 
-	public AdvancedSearchLayout() {
-		super("advancedSearch");
-		setStyleName("advancedSearchLayout");
-		initLayout();
-	}
+    public AdvancedSearchLayout() {
+        super("advancedSearch");
+        setStyleName("advancedSearchLayout");
+        initLayout();
+    }
 
-	public void initLayout() {
-		ComponentContainer header = constructHeader();
-		ComponentContainer body = constructBody();
-		ComponentContainer footer = constructFooter();
-		this.addComponent(header, "advSearchHeader");
-		this.addComponent(body, "advSearchBody");
-		this.addComponent(footer, "advSearchFooter");
-	}
+    public void initLayout() {
+        ComponentContainer header = constructHeader();
+        ComponentContainer body = constructBody();
+        ComponentContainer footer = constructFooter();
+        this.addComponent(header, "advSearchHeader");
+        this.addComponent(body, "advSearchBody");
+        this.addComponent(footer, "advSearchFooter");
+    }
 
-	public abstract ComponentContainer constructHeader();
+    public abstract ComponentContainer constructHeader();
 
-	public abstract ComponentContainer constructBody();
+    public abstract ComponentContainer constructBody();
 
-	public abstract ComponentContainer constructFooter();
+    public abstract ComponentContainer constructFooter();
 }
