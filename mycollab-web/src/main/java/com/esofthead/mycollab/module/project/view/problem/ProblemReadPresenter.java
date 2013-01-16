65215d5af694ad7e7382aa3ab6526bc7b8b686dd
Nguyen Hai <hainguyen@esofthead.com>
2013-01-16 09:44:37 +0700

    Fix auto login after sign out

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/problem/ProblemReadPresenter.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/problem/ProblemReadPresenter.java
@@ -18,112 +18,112 @@ import com.vaadin.ui.Window;
 
 public class ProblemReadPresenter extends AbstractPresenter<ProblemReadView> {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	public ProblemReadPresenter() {
-		super(ProblemReadView.class);
-		bind();
-	}
+    public ProblemReadPresenter() {
+        super(ProblemReadView.class);
+        bind();
+    }
 
-	private void bind() {
-		view.getPreviewFormHandlers().addFormHandler(
-				new DefaultPreviewFormHandler<Problem>() {
-					@Override
-					public void onEdit(Problem data) {
-						EventBus.getInstance().fireEvent(
-								new ProblemEvent.GotoEdit(this, data));
-					}
+    private void bind() {
+        view.getPreviewFormHandlers().addFormHandler(
+                new DefaultPreviewFormHandler<Problem>() {
+                    @Override
+                    public void onEdit(Problem data) {
+                        EventBus.getInstance().fireEvent(
+                                new ProblemEvent.GotoEdit(this, data));
+                    }
 
-					@Override
-					public void onDelete(Problem data) {
-						ProblemService riskService = AppContext
-								.getSpringBean(ProblemService.class);
-						riskService.removeWithSession(data.getId(),
-								AppContext.getUsername());
-						EventBus.getInstance().fireEvent(
-								new ProblemEvent.GotoList(this, null));
-					}
+                    @Override
+                    public void onDelete(Problem data) {
+                        ProblemService riskService = AppContext
+                                .getSpringBean(ProblemService.class);
+                        riskService.removeWithSession(data.getId(),
+                                AppContext.getUsername());
+                        EventBus.getInstance().fireEvent(
+                                new ProblemEvent.GotoList(this, null));
+                    }
 
-					@Override
-					public void onClone(Problem data) {
-						Problem cloneData = (Problem) data.copy();
-						cloneData.setId(null);
-						EventBus.getInstance().fireEvent(
-								new ProblemEvent.GotoEdit(this, cloneData));
-					}
+                    @Override
+                    public void onClone(Problem data) {
+                        Problem cloneData = (Problem) data.copy();
+                        cloneData.setId(null);
+                        EventBus.getInstance().fireEvent(
+                                new ProblemEvent.GotoEdit(this, cloneData));
+                    }
 
-					@Override
-					public void onCancel() {
-						EventBus.getInstance().fireEvent(
-								new ProblemEvent.GotoList(this, null));
-					}
+                    @Override
+                    public void onCancel() {
+                        EventBus.getInstance().fireEvent(
+                                new ProblemEvent.GotoList(this, null));
+                    }
 
-					@Override
-					public void gotoNext(Problem data) {
-						ProblemService problemService = AppContext
-								.getSpringBean(ProblemService.class);
-						ProblemSearchCriteria criteria = new ProblemSearchCriteria();
-						SimpleProject project = (SimpleProject) AppContext
-								.getVariable("project");
-						criteria.setProjectId(new NumberSearchField(
-								SearchField.AND, project.getId()));
-						criteria.setId(new NumberSearchField(data.getId(),
-								NumberSearchField.GREATHER));
-						Integer nextId = problemService
-								.getNextItemKey(criteria);
-						if (nextId != null) {
-							EventBus.getInstance().fireEvent(
-									new ProblemEvent.GotoRead(this, nextId));
-						} else {
-							view.getWindow().showNotification("Information",
-									"You are already in the last record",
-									Window.Notification.TYPE_HUMANIZED_MESSAGE);
-						}
+                    @Override
+                    public void gotoNext(Problem data) {
+                        ProblemService problemService = AppContext
+                                .getSpringBean(ProblemService.class);
+                        ProblemSearchCriteria criteria = new ProblemSearchCriteria();
+                        SimpleProject project = (SimpleProject) AppContext
+                                .getVariable("project");
+                        criteria.setProjectId(new NumberSearchField(
+                                SearchField.AND, project.getId()));
+                        criteria.setId(new NumberSearchField(data.getId(),
+                                NumberSearchField.GREATHER));
+                        Integer nextId = problemService
+                                .getNextItemKey(criteria);
+                        if (nextId != null) {
+                            EventBus.getInstance().fireEvent(
+                                    new ProblemEvent.GotoRead(this, nextId));
+                        } else {
+                            view.getWindow().showNotification("Information",
+                                    "You are already in the last record",
+                                    Window.Notification.TYPE_HUMANIZED_MESSAGE);
+                        }
 
-					}
+                    }
 
-					@Override
-					public void gotoPrevious(Problem data) {
-						ProblemService problemService = AppContext
-								.getSpringBean(ProblemService.class);
-						ProblemSearchCriteria criteria = new ProblemSearchCriteria();
-						SimpleProject project = (SimpleProject) AppContext
-								.getVariable("project");
-						criteria.setProjectId(new NumberSearchField(
-								SearchField.AND, project.getId()));
-						criteria.setId(new NumberSearchField(data.getId(),
-								NumberSearchField.LESSTHAN));
-						Integer nextId = problemService
-								.getPreviousItemKey(criteria);
-						if (nextId != null) {
-							EventBus.getInstance().fireEvent(
-									new ProblemEvent.GotoRead(this, nextId));
-						} else {
-							view.getWindow().showNotification("Information",
-									"You are already in the first record",
-									Window.Notification.TYPE_HUMANIZED_MESSAGE);
-						}
-					}
-				});
-	}
+                    @Override
+                    public void gotoPrevious(Problem data) {
+                        ProblemService problemService = AppContext
+                                .getSpringBean(ProblemService.class);
+                        ProblemSearchCriteria criteria = new ProblemSearchCriteria();
+                        SimpleProject project = (SimpleProject) AppContext
+                                .getVariable("project");
+                        criteria.setProjectId(new NumberSearchField(
+                                SearchField.AND, project.getId()));
+                        criteria.setId(new NumberSearchField(data.getId(),
+                                NumberSearchField.LESSTHAN));
+                        Integer nextId = problemService
+                                .getPreviousItemKey(criteria);
+                        if (nextId != null) {
+                            EventBus.getInstance().fireEvent(
+                                    new ProblemEvent.GotoRead(this, nextId));
+                        } else {
+                            view.getWindow().showNotification("Information",
+                                    "You are already in the first record",
+                                    Window.Notification.TYPE_HUMANIZED_MESSAGE);
+                        }
+                    }
+                });
+    }
 
-	@Override
-	protected void onGo(ComponentContainer container, ScreenData<?> data) {
-		if (data.getParams() instanceof Integer) {
-			ProblemService problemService = AppContext
-					.getSpringBean(ProblemService.class);
-			SimpleProblem problem = problemService
-					.findProblemById((Integer) data.getParams());
-			if (problem != null) {
-				ProblemContainer problemContainer = (ProblemContainer) container;
-				problemContainer.removeAllComponents();
-				problemContainer.addComponent(view.getWidget());
-				view.previewItem(problem);
-			} else {
+    @Override
+    protected void onGo(ComponentContainer container, ScreenData<?> data) {
+        if (data.getParams() instanceof Integer) {
+            ProblemService problemService = AppContext
+                    .getSpringBean(ProblemService.class);
+            SimpleProblem problem = problemService
+                    .findProblemById((Integer) data.getParams());
+            if (problem != null) {
+                ProblemContainer problemContainer = (ProblemContainer) container;
+                problemContainer.removeAllComponents();
+                problemContainer.addComponent(view.getWidget());
+                view.previewItem(problem);
+            } else {
                 AppContext.getApplication().getMainWindow().showNotification("Information", "The record is not existed", Window.Notification.TYPE_HUMANIZED_MESSAGE);
                 return;
             }
 
-		}
-	}
+        }
+    }
 }
