89c5f161116433e59948c72bed65bdaa74ad6fae
Nghi Tran <nghitran@esofthead.com>
2013-01-18 15:45:54 +0700

    apply new css styles in Depot, ActivityStreamPanel now extends from Depot

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/Depot.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/Depot.java
@@ -4,6 +4,7 @@ import com.vaadin.event.LayoutEvents.LayoutClickEvent;
 import com.vaadin.event.LayoutEvents.LayoutClickListener;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.CustomComponent;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
@@ -11,65 +12,68 @@ import com.vaadin.ui.VerticalLayout;
 
 public class Depot extends VerticalLayout {
 
-    private static final long serialVersionUID = 1L;
-    private boolean isOpenned = true;
-    protected HorizontalLayout header;
-    private final Label headerLbl;
-    protected ComponentContainer headerContent;
-    protected ComponentContainer bodyContent;
+	private static final long serialVersionUID = 1L;
+	private boolean isOpenned = true;
+	protected HorizontalLayout header;
+	private final Label headerLbl;
+	protected ComponentContainer headerContent;
+	protected ComponentContainer bodyContent;
 
-    public Depot(String title, ComponentContainer headerElement,
-            ComponentContainer component) {
-        this.setStyleName("depotComp");
-        header = new HorizontalLayout();
-        header.setStyleName("depotHeader");
-        header.setWidth("500px");
-        this.bodyContent = component;
-        this.addComponent(header);
+	public Depot(String title, ComponentContainer headerElement,
+			ComponentContainer component) {
+		this.setStyleName("depotComp");
+		header = new HorizontalLayout();
+		header.setStyleName("depotHeader");
+		header.setWidth("500px");
+		this.bodyContent = component;
+		this.addComponent(header);
 
-        headerLbl = new Label(title);
-        headerLbl.setStyleName("h2");
-        headerLbl.setSizeUndefined();
-        header.addComponent(headerLbl);
-        if (headerElement == null) {
-            this.headerContent = headerElement;
-            headerElement = new HorizontalLayout();
-            headerElement.setSizeFull();
-            headerElement.setStyleName("default-element");
-            ((HorizontalLayout) headerElement)
-                    .addListener(new LayoutClickListener() {
-                private static final long serialVersionUID = 1L;
+		CssLayout headerLeft = new CssLayout();
+		headerLbl = new Label(title);
+		headerLbl.setStyleName("h2");
+		headerLbl.setSizeUndefined();
+		headerLeft.addComponent(headerLbl);
+		headerLeft.setStyleName("depot-title");
+		headerLeft.addListener(new LayoutClickListener() {
+			private static final long serialVersionUID = 1L;
 
-                @Override
-                public void layoutClick(LayoutClickEvent event) {
-                    isOpenned = !isOpenned;
-                    if (isOpenned) {
-                        bodyContent.setHeight("100%");
-                        Depot.this.removeStyleName("collapsed");
-                    } else {
-                        bodyContent.setHeight("0px");
-                        Depot.this.addStyleName("collapsed");
-                    }
-                }
-            });
-        }
+			@Override
+			public void layoutClick(LayoutClickEvent event) {
+				isOpenned = !isOpenned;
+				if (isOpenned) {
+					bodyContent.setHeight("100%");
+					Depot.this.removeStyleName("collapsed");
+				} else {
+					bodyContent.setHeight("0px");
+					Depot.this.addStyleName("collapsed");
+				}
+			}
+		});
+		header.addComponent(headerLeft);
 
-        headerElement.addStyleName("header-elements");
-        header.addComponent(headerElement);
-        header.setExpandRatio(headerElement, 1.0f);
+		CssLayout headerRight = new CssLayout();
+		headerRight.setStyleName("header-elements");
+		headerRight.setSizeFull();
 
-        CustomComponent customComp = new CustomComponent(component);
-        customComp.setWidth("100%");
-        customComp.setStyleName("depotContent");
-        this.addComponent(customComp);
-        this.setComponentAlignment(customComp, Alignment.MIDDLE_LEFT);
-    }
+		if (headerElement != null) {
+			headerRight.addComponent(headerElement);
+		}
 
-    public Depot(String title, ComponentContainer component) {
-        this(title, null, component);
-    }
+		header.addComponent(headerRight);
+		header.setExpandRatio(headerRight, 1.0f);
 
-    public void setTitle(String title) {
-        headerLbl.setValue(title);
-    }
+		CustomComponent customComp = new CustomComponent(component);
+		customComp.setWidth("100%");
+		customComp.setStyleName("depotContent");
+		this.addComponent(customComp);
+		this.setComponentAlignment(customComp, Alignment.MIDDLE_LEFT);
+	}
+
+	public Depot(String title, ComponentContainer component) {
+		this(title, null, component);
+	}
+
+	public void setTitle(String title) {
+		headerLbl.setValue(title);
+	}
 }
