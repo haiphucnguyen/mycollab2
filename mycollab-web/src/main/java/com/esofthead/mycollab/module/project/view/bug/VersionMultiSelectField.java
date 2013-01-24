d24377270eaf31ee27e6b8cbb662441246ad432c
linh nguyen <linhnguyen@esofthead.com>
2013-01-24 10:56:33 +0700

    update multiple select item popup

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/VersionMultiSelectField.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/VersionMultiSelectField.java
@@ -5,6 +5,7 @@
 package com.esofthead.mycollab.module.project.view.bug;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import com.esofthead.mycollab.core.arguments.NumberSearchField;
@@ -22,11 +23,16 @@ import com.esofthead.mycollab.web.AppContext;
  */
 @SuppressWarnings("serial")
 public class VersionMultiSelectField extends MultiSelectComp {
+	
+	private HashMap<String, Version> hashMapVersion;
 
     public VersionMultiSelectField() {
         super();
+    }
 
-        VersionSearchCriteria searchCriteria = new VersionSearchCriteria();
+	@Override
+	void initData() {
+		VersionSearchCriteria searchCriteria = new VersionSearchCriteria();
 
         SimpleProject project = (SimpleProject) AppContext
                 .getVariable("project");
@@ -36,12 +42,27 @@ public class VersionMultiSelectField extends MultiSelectComp {
         VersionService versionService = AppContext.getSpringBean(VersionService.class);
         List<Version> lstVersion = versionService.findPagableListByCriteria(new SearchRequest<VersionSearchCriteria>(searchCriteria, 0, Integer.MAX_VALUE));
         List<String> lstVersionName = new ArrayList<String>();
+        hashMapVersion = new HashMap<String, Version>();
 
         for (int i = 0; i < lstVersion.size(); i++) {
             Version version = lstVersion.get(i);
             lstVersionName.add(version.getVersionname());
+            hashMapVersion.put(version.getVersionname(), version);
         }
 
         this.loadData(lstVersionName);
-    }
+	}
+	
+	public List<Version> getListSelectedVersion() {
+		List<String> lstStr =  getSelectedItem();
+		List<Version> lstValues = new ArrayList<Version>();
+		
+		for (int i = 0; i < lstStr.size(); i++) {
+			if (hashMapVersion.containsKey(lstStr.get(i))) {
+				lstValues.add(hashMapVersion.get(lstStr.get(i)));
+			}
+		}
+		
+		return lstValues;
+	}
 }
