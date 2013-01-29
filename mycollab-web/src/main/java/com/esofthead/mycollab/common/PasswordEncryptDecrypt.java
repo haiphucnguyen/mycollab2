7a54225a8cd0fdc099913f853f4030379822f12b
Nguyen Hai <hainguyen@esofthead.com>
2013-01-29 16:08:14 +0700

    fix issue of spring context file

--- mycollab-web/src/main/java/com/esofthead/mycollab/common/PasswordEncryptDecrypt.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/common/PasswordEncryptDecrypt.java
@@ -4,10 +4,20 @@
  */
 package com.esofthead.mycollab.common;
 
+import org.jasypt.util.text.BasicTextEncryptor;
+
 /**
  *
  * @author haiphucnguyen
  */
 public class PasswordEncryptDecrypt {
+    public static final String DECRYPT_PASS = "esofthead321";
     
+    public static String encryptText() {
+        return "";
+    }
+    
+    public static void main(String[] args) {
+        
+    }
 }
