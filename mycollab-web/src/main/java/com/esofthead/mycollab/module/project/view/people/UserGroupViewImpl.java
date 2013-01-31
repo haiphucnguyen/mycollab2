8f49bafc3489e8ee730ebfb7150d74f6abca91d5
Nguyen Hai <hainguyen@esofthead.com>
2013-01-31 14:11:30 +0700

    Update project view

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/people/UserGroupViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/people/UserGroupViewImpl.java
@@ -6,6 +6,9 @@ package com.esofthead.mycollab.module.project.view.people;
 
 import com.esofthead.mycollab.vaadin.mvp.AbstractView;
 import com.esofthead.mycollab.vaadin.ui.ViewComponent;
+import com.github.wolfie.detachedtabs.DetachedTabs;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.VerticalLayout;
 
 /**
  *
@@ -13,5 +16,35 @@ import com.esofthead.mycollab.vaadin.ui.ViewComponent;
  */
 @ViewComponent
 public class UserGroupViewImpl extends AbstractView implements UserGroupView {
+
+    private ProjectUserPresenter userPresenter;
+    private ProjectRolePresenter rolePresenter;
+    private final DetachedTabs myProjectTab;
+    private final CssLayout mySpaceArea = new CssLayout();
+
+    public UserGroupViewImpl() {
+
+        myProjectTab = new DetachedTabs.Horizontal(mySpaceArea);
+        myProjectTab.setSizeFull();
+        myProjectTab.setHeight(null);
+
+        CssLayout menu = new CssLayout();
+        menu.setHeight("40px");
+        menu.setStyleName("sidebar-menu");
+        menu.addComponent(myProjectTab);
+
+        this.addComponent(menu);
+        mySpaceArea.setStyleName("projectTabContent");
+        mySpaceArea.setWidth("100%");
+        mySpaceArea.setHeight(null);
+        this.addComponent(mySpaceArea);
+        this.setExpandRatio(mySpaceArea, 1.0f);
+        this.setWidth("100%");
+        buildComponents();
+    }
     
+    private void buildComponents() {
+        myProjectTab.addTab(new VerticalLayout(), "Users");
+        myProjectTab.addTab(new VerticalLayout(), "Roles");
+    }
 }
