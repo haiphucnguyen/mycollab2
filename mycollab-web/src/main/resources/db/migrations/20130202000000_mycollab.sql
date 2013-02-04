21da92890f3c94211de247d87d0fa69258725a54
Nguyen Hai <hainguyen@esofthead.com>
2013-02-04 10:48:16 +0700

    Update bug sql

---
+++ mycollab-web/src/main/resources/db/migrations/20130202000000_mycollab.sql
@@ -0,0 +1,7 @@
+ALTER TABLE `m_tracker_bug` ADD COLUMN `milestoneId` INT UNSIGNED NULL, 
+  ADD CONSTRAINT `FK_m_tracker_bug_6`
+  FOREIGN KEY (`milestoneId` )
+  REFERENCES `m_prj_project` (`id` )
+  ON DELETE SET NULL
+  ON UPDATE CASCADE
+, ADD INDEX `FK_m_tracker_bug_6_idx` (`milestoneId` ASC) ;
