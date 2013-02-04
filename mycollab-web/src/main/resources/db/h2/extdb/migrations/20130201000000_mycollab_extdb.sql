2ddc43ef6794f7d105944d25fe3b2c0420242a86
nobugz <nobugz@live.com>
2013-02-04 10:37:40 +0700

    fix error when processing h2 db edit mapper

--- mycollab-web/src/main/resources/db/h2/extdb/migrations/20130201000000_mycollab_extdb.sql
+++ mycollab-web/src/main/resources/db/h2/extdb/migrations/20130201000000_mycollab_extdb.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS `ip_country`;
+DROP TABLE IF EXISTS `ipcountry`;
 
-CREATE  TABLE `ip_country` (
+CREATE  TABLE `ipcountry` (
   `fromip` BIGINT NOT NULL ,
   `toip` BIGINT NOT NULL ,
   `country_name` VARCHAR(100) NOT NULL ,
