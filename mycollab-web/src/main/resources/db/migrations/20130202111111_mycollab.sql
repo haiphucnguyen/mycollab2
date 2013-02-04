383acbedef83e812fbe7952b8dd8730ce0a166d7
Nguyen Hai <hainguyen@esofthead.com>
2013-02-04 11:04:39 +0700

    Fix wrong sql script

--- mycollab-web/src/main/resources/db/migrations/20130202111111_mycollab.sql
+++ mycollab-web/src/main/resources/db/migrations/20130202111111_mycollab.sql
@@ -1,7 +1,7 @@
-ALTER TABLE `m_tracker_bug` ADD COLUMN `milestoneId` INT UNSIGNED NULL, 
+ALTER TABLE `m_tracker_bug` ADD COLUMN `milestoneId` INT NULL  AFTER `sAccountId` , 
   ADD CONSTRAINT `FK_m_tracker_bug_6`
   FOREIGN KEY (`milestoneId` )
-  REFERENCES `m_prj_project` (`id` )
+  REFERENCES `m_prj_milestone` (`id` )
   ON DELETE SET NULL
   ON UPDATE CASCADE
 , ADD INDEX `FK_m_tracker_bug_6_idx` (`milestoneId` ASC) ;
