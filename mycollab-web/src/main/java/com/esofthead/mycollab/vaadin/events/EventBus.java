3d5a89e3f8fb0f36ffaab9b16c1b83e8ab22ccf7
Nguyen Hai <hainguyen@esofthead.com>
2013-02-19 09:01:08 +0700

    Update event bus

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/events/EventBus.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/events/EventBus.java
@@ -1,6 +1,5 @@
 package com.esofthead.mycollab.vaadin.events;
 
-import com.esofthead.mycollab.web.AppContext;
 import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -8,8 +7,11 @@ import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.esofthead.mycollab.web.AppContext;
 
 @SuppressWarnings("serial")
 public class EventBus implements Serializable {
