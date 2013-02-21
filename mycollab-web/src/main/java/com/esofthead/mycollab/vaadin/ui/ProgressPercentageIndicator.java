0e6f90fa120e7a50a9c4da367157c8881548ce2e
Nguyen Hai <hainguyen@esofthead.com>
2013-02-21 22:50:42 +0700

    update progress

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/ProgressPercentageIndicator.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/ProgressPercentageIndicator.java
@@ -1,38 +1,12 @@
 package com.esofthead.mycollab.vaadin.ui;
 
-import org.vaadin.overlay.TextOverlay;
+import com.vaadin.ui.Label;
 
-import com.vaadin.ui.Alignment;
-import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.ProgressIndicator;
-
-public class ProgressPercentageIndicator extends HorizontalLayout {
+public class ProgressPercentageIndicator extends Label {
 	private static final long serialVersionUID = 1L;
-	
-	private ProgressIndicator progress;
-	private TextOverlay to;
 
 	public ProgressPercentageIndicator(double val) {
-		this.setWidth("100%");
-		progress = new ProgressIndicator(new Float(val));
-		progress.setPollingInterval(1000 * 60 * 60 * 24);
-		progress.setWidth("100%");
-		this.addComponent(progress);
-	}
-	
-	public void setValue(double val) {
-		progress.setValue(val);
-	}
-	
-	@Override
-	public void attach() {
-		if (this.getApplication() != null) {
-			to = new TextOverlay(this, progress.getValue() + "%");
-			to.setComponentAnchor(Alignment.MIDDLE_CENTER);
-			to.setOverlayAnchor(Alignment.MIDDLE_CENTER);
-			to.setXOffset(0);
-			to.setYOffset(0);
-			this.addComponent(to);
-		}
+		String perVal = val + "%";
+		this.setValue(perVal);
 	}
 }
