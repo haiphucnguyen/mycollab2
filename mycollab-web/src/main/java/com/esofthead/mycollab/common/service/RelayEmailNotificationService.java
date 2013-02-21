87ece3b261305ce33da8d466aac2705bc57b308e
Nguyen Hai <hainguyen@esofthead.com>
2013-02-21 16:58:50 +0700

    Add email relay notification service

---
+++ mycollab-web/src/main/java/com/esofthead/mycollab/common/service/RelayEmailNotificationService.java
@@ -0,0 +1,11 @@
+package com.esofthead.mycollab.common.service;
+
+import com.esofthead.mycollab.common.domain.RelayEmailNotification;
+import com.esofthead.mycollab.common.domain.criteria.RelayEmailNotificationSearchCriteria;
+import com.esofthead.mycollab.core.persistence.service.IDefaultService;
+
+public interface RelayEmailNotificationService
+		extends
+		IDefaultService<Integer, RelayEmailNotification, RelayEmailNotificationSearchCriteria> {
+
+}
