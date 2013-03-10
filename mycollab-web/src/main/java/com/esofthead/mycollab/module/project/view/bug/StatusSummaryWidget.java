d3b8bc5404dd8bf630f98a18393d9d7005706a5f
Nguyen Hai <hainguyen@esofthead.com>
2013-03-10 10:35:57 +0700

    Add link to project chart

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/StatusSummaryWidget.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/StatusSummaryWidget.java
@@ -5,9 +5,17 @@ import java.util.List;
 import org.jfree.data.general.DefaultPieDataset;
 
 import com.esofthead.mycollab.common.domain.GroupItem;
+import com.esofthead.mycollab.core.arguments.NumberSearchField;
+import com.esofthead.mycollab.core.arguments.SearchField;
+import com.esofthead.mycollab.core.arguments.SetSearchField;
+import com.esofthead.mycollab.module.project.CurrentProjectVariables;
 import com.esofthead.mycollab.module.project.ProjectDataTypeFactory;
+import com.esofthead.mycollab.module.project.events.BugEvent;
+import com.esofthead.mycollab.module.project.view.parameters.BugScreenData;
+import com.esofthead.mycollab.module.project.view.parameters.BugSearchParameter;
 import com.esofthead.mycollab.module.tracker.domain.criteria.BugSearchCriteria;
 import com.esofthead.mycollab.module.tracker.service.BugService;
+import com.esofthead.mycollab.vaadin.events.EventBus;
 import com.esofthead.mycollab.vaadin.ui.chart.PieChartDescriptionBox;
 import com.esofthead.mycollab.vaadin.ui.chart.PieChartWrapper;
 import com.esofthead.mycollab.web.AppContext;
@@ -56,7 +64,14 @@ public class StatusSummaryWidget extends PieChartWrapper<BugSearchCriteria> {
 
 	@Override
 	protected void onClickedDescription(String key) {
-		// TODO Auto-generated method stub
-		
+		BugSearchCriteria searchCriteria = new BugSearchCriteria();
+		searchCriteria.setStatuses(new SetSearchField<String>(SearchField.AND,
+				new String[] { key }));
+		searchCriteria.setProjectId(new NumberSearchField(
+				CurrentProjectVariables.getProjectId()));
+		BugSearchParameter param = new BugSearchParameter(key + " Bug List",
+				searchCriteria);
+		EventBus.getInstance().fireEvent(
+				new BugEvent.GotoList(this, new BugScreenData.Search(param)));
 	}
 }
