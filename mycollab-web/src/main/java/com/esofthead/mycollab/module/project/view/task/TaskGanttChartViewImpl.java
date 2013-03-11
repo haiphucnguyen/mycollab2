41b02b30ec1d15fbb67e227c3497e4bcbbc14a13
Nguyen Hai <hainguyen@esofthead.com>
2013-03-11 18:14:17 +0700

    Update gantt chart

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/task/TaskGanttChartViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/task/TaskGanttChartViewImpl.java
@@ -12,11 +12,13 @@ import org.joda.time.format.DateTimeFormatter;
 import ru.bazon.vaadin.ganttdiagram.GanttColumnFormatter;
 import ru.bazon.vaadin.ganttdiagram.GanttDiagram;
 import ru.bazon.vaadin.ganttdiagram.GanttDiagramCellStyleGeneratorAdapter;
+import ru.bazon.vaadin.ganttdiagram.GanttGeneratedColumn;
 import ru.bazon.vaadin.ganttdiagram.model.GANTTCOLUMN;
 import ru.bazon.vaadin.ganttdiagram.model.GANTTDIAGRAMSCALE;
 import ru.bazon.vaadin.ganttdiagram.model.GanttDiagramModel;
 import ru.bazon.vaadin.ganttdiagram.model.GanttTask;
-import ru.bazon.vaadin.ganttdiagram.model.GanttTaskParameterDescription;
+import ru.bazon.vaadin.ganttdiagram.model.GanttTaskGeneratedColumnDescription;
+import ru.bazon.vaadin.ganttdiagram.treetable.GanttTreeTable;
 
 import com.esofthead.mycollab.core.arguments.NumberSearchField;
 import com.esofthead.mycollab.core.arguments.SearchRequest;
@@ -24,12 +26,19 @@ import com.esofthead.mycollab.core.arguments.StringSearchField;
 import com.esofthead.mycollab.module.project.CurrentProjectVariables;
 import com.esofthead.mycollab.module.project.domain.SimpleTask;
 import com.esofthead.mycollab.module.project.domain.SimpleTaskList;
+import com.esofthead.mycollab.module.project.domain.Task;
+import com.esofthead.mycollab.module.project.domain.TaskList;
 import com.esofthead.mycollab.module.project.domain.criteria.TaskListSearchCriteria;
 import com.esofthead.mycollab.module.project.domain.criteria.TaskSearchCriteria;
+import com.esofthead.mycollab.module.project.events.TaskEvent;
+import com.esofthead.mycollab.module.project.events.TaskListEvent;
 import com.esofthead.mycollab.module.project.service.ProjectTaskListService;
+import com.esofthead.mycollab.vaadin.events.EventBus;
 import com.esofthead.mycollab.vaadin.mvp.AbstractView;
 import com.esofthead.mycollab.vaadin.ui.ViewComponent;
 import com.esofthead.mycollab.web.AppContext;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 
 @ViewComponent
 public class TaskGanttChartViewImpl extends AbstractView implements
@@ -37,7 +46,7 @@ public class TaskGanttChartViewImpl extends AbstractView implements
 	private static final long serialVersionUID = 1L;
 
 	private static DateTimeFormatter dateFormat = DateTimeFormat
-			.forPattern("yyyy-MM-dd");
+			.forPattern("MM-dd-yy");
 
 	private TaskSearchCriteria searchCriteria;
 
@@ -61,16 +70,53 @@ public class TaskGanttChartViewImpl extends AbstractView implements
 		GanttDiagramModel gdata = new GanttDiagramModel();
 		gdata.setGanttColumnVisible(GANTTCOLUMN.STARTTIME,
 				"Start         Date", null, null);
-		gdata.setGanttColumnVisible(GANTTCOLUMN.ENDTIME, "End           Date",
-				null, null);
-		gdata.setGanttColumnVisible(GANTTCOLUMN.COMPLETE, "Percentage", null,
+		gdata.setGanttColumnVisible(GANTTCOLUMN.ENDTIME, "End Date", null, null);
+		gdata.setGanttColumnVisible(GANTTCOLUMN.COMPLETE, "%Complete", null,
 				null);
-		gdata.addParametersDescription(new GanttTaskParameterDescription(
-				"name", String.class, "", "Task                     Name"));
+		// gdata.addParametersDescription(new GanttTaskParameterDescription(
+		// "name", String.class, "", "Name"));
+
+		gdata.addGanttGeneratedColumn(new GanttTaskGeneratedColumnDescription(
+				"name", new GanttGeneratedColumn() {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public Object generateCell(GanttTreeTable treeTable,
+							final GanttTask task, String columnId) {
+						Button btn = new Button((String) task
+								.getParameter("name"),
+								new Button.ClickListener() {
+									private static final long serialVersionUID = 1L;
+
+									@Override
+									public void buttonClick(ClickEvent event) {
+										Object myTask = task
+												.getParameter("task");
+										if (myTask instanceof TaskList) {
+											EventBus.getInstance()
+													.fireEvent(
+															new TaskListEvent.GotoRead(
+																	TaskGanttChartViewImpl.this,
+																	((TaskList) myTask)
+																			.getId()));
+										} else if (myTask instanceof Task) {
+											EventBus.getInstance()
+													.fireEvent(
+															new TaskEvent.GotoRead(
+																	TaskGanttChartViewImpl.this,
+																	((Task) myTask)
+																			.getId()));
+										}
+									}
+								});
+						btn.setStyleName("link");
+						btn.setWidth("200px");
+						return btn;
+					}
+				}, "Name"));
 
 		gdata.setColumnFormatter(GANTTCOLUMN.STARTTIME.getId(),
 				new GanttColumnFormatter<DateTime>() {
-
 					@Override
 					public String formatValue(String columnName, DateTime value) {
 						String sqlTimeString = dateFormat.print(value);
@@ -81,7 +127,6 @@ public class TaskGanttChartViewImpl extends AbstractView implements
 
 		gdata.setColumnFormatter(GANTTCOLUMN.ENDTIME.getId(),
 				new GanttColumnFormatter<DateTime>() {
-
 					@Override
 					public String formatValue(String columnName, DateTime value) {
 						String sqlTimeString = dateFormat.print(value);
@@ -90,10 +135,10 @@ public class TaskGanttChartViewImpl extends AbstractView implements
 
 				});
 		gdata.setColumnsOrder(Arrays.asList(new Object[] { "name",
-				GANTTCOLUMN.COMPLETE, GANTTCOLUMN.STARTTIME,
-				GANTTCOLUMN.ENDTIME }));
+				GANTTCOLUMN.STARTTIME, GANTTCOLUMN.ENDTIME,
+				GANTTCOLUMN.COMPLETE, }));
 
-		gdata.setTaskTooltipTemplate("<b>Name :</b> ${name}<br><b>Start time:</b> ${startTime}<br><b>End time:</b> ${endTime}");
+		// gdata.setTaskTooltipTemplate("<b>Name :</b> ${name}<br><b>Start time:</b> ${startTime}<br><b>End time:</b> ${endTime}");
 
 		for (SimpleTaskList taskList : tasks) {
 			GanttTask parentGanttTask = new GanttTask(
@@ -101,6 +146,7 @@ public class TaskGanttChartViewImpl extends AbstractView implements
 					convertDateTime(taskList.getEndDate()),
 					taskList.getPercentageComplete());
 			parentGanttTask.addParameter("name", taskList.getName());
+			parentGanttTask.addParameter("task", taskList);
 			List<SimpleTask> subTasks = taskList.getSubTasks();
 			for (SimpleTask subTask : subTasks) {
 				GanttTask ganttTask = new GanttTask(
@@ -108,7 +154,7 @@ public class TaskGanttChartViewImpl extends AbstractView implements
 						convertDateTime(subTask.getEnddate()),
 						subTask.getPercentagecomplete(), parentGanttTask);
 				ganttTask.addParameter("name", subTask.getTaskname());
-
+				ganttTask.addParameter("task", subTask);
 			}
 			gdata.addTask(parentGanttTask);
 		}
@@ -118,9 +164,8 @@ public class TaskGanttChartViewImpl extends AbstractView implements
 		diagram.addAvailableScale(GANTTDIAGRAMSCALE.DAY_MONTH, "Day of Month");
 		diagram.addAvailableScale(GANTTDIAGRAMSCALE.WEEK, "Week");
 		diagram.addAvailableScale(GANTTDIAGRAMSCALE.MONTH, "Month");
-		diagram.addAvailableScale(GANTTDIAGRAMSCALE.YEAR, "Year");
 
-		diagram.setScale(GANTTDIAGRAMSCALE.MONTH);
+		diagram.setScale(GANTTDIAGRAMSCALE.DAY);
 		this.addComponent(diagram);
 		this.setExpandRatio(diagram, 1.0f);
 	}
