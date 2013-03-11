8db05d938f16b6ca8c1384eaab4b6fd6a9e4eaf0
Nguyen Hai <hainguyen@esofthead.com>
2013-03-11 14:16:45 +0700

    Update gantt chart

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/domain/SimpleTaskList.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/domain/SimpleTaskList.java
@@ -5,84 +5,124 @@
 package com.esofthead.mycollab.module.project.domain;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
- *
+ * 
  * @author haiphucnguyen
  */
 public class SimpleTaskList extends TaskList {
+	private static final long serialVersionUID = 1L;
+	private String milestoneName;
+	private String ownerFullName;
+	private List<SimpleTask> subTasks = new ArrayList<SimpleTask>();
+	private double percentageComplete;
+	private int numOpenTasks;
+	private int numAllTasks;
 
-    private String milestoneName;
-    private String ownerFullName;
-    private List<SimpleTask> subTasks = new ArrayList<SimpleTask>();
-    private double percentageComplete;
-    private int numOpenTasks;
-    private int numAllTasks;
+	public String getMilestoneName() {
+		return milestoneName;
+	}
 
-    public String getMilestoneName() {
-        return milestoneName;
-    }
+	public void setMilestoneName(String milestoneName) {
+		this.milestoneName = milestoneName;
+	}
 
-    public void setMilestoneName(String milestoneName) {
-        this.milestoneName = milestoneName;
-    }
+	public String getOwnerFullName() {
+		return ownerFullName;
+	}
 
-    public String getOwnerFullName() {
-        return ownerFullName;
-    }
+	public void setOwnerFullName(String ownerFullName) {
+		this.ownerFullName = ownerFullName;
+	}
 
-    public void setOwnerFullName(String ownerFullName) {
-        this.ownerFullName = ownerFullName;
-    }
+	public List<SimpleTask> getSubTasks() {
+		return subTasks;
+	}
 
-    public List<SimpleTask> getSubTasks() {
-        return subTasks;
-    }
+	public void setSubTasks(List<SimpleTask> subTasks) {
+		this.subTasks = subTasks;
+	}
 
-    public void setSubTasks(List<SimpleTask> subTasks) {
-        this.subTasks = subTasks;
-    }
+	public Date getStartDate() {
+		Date result = null;
+		for (SimpleTask task : subTasks) {
+			if (task.getStartdate() != null) {
+				if (result == null) {
+					result = task.getStartdate();
+				} else {
+					if (result.after(task.getStartdate())) {
+						result = task.getStartdate();
+					}
+				}
+			}
+		}
+		return result;
+	}
 
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof SimpleTaskList)) {
-            return false;
-        } else {
-            SimpleTaskList taskList = (SimpleTaskList) o;
-            return (taskList.getId() == this.getId() && taskList.getGroupindex() == this.getGroupindex());
-        }
-    }
+	public Date getEndDate() {
+		Date result = null;
+		for (SimpleTask task : subTasks) {
+			if (task.getEnddate() != null) {
+				if (result == null) {
+					result = task.getEnddate();
+				} else {
+					if (result.before(task.getEnddate())) {
+						result = task.getEnddate();
+					}
+				}
+			}
+		}
+		return result;
+	}
 
-    @Override
-    public int hashCode() {
-        int hash = 5;
-        hash = 47 * hash + (this.milestoneName != null ? this.milestoneName.hashCode() : 0);
-        hash = 47 * hash + (this.ownerFullName != null ? this.ownerFullName.hashCode() : 0);
-        return hash;
-    }
+	@Override
+	public boolean equals(Object o) {
+		if (!(o instanceof SimpleTaskList)) {
+			return false;
+		} else {
+			SimpleTaskList taskList = (SimpleTaskList) o;
+			return (taskList.getId() == this.getId() && taskList
+					.getGroupindex() == this.getGroupindex());
+		}
+	}
 
-    public double getPercentageComplete() {
-        return percentageComplete;
-    }
+	@Override
+	public int hashCode() {
+		int hash = 5;
+		hash = 47
+				* hash
+				+ (this.milestoneName != null ? this.milestoneName.hashCode()
+						: 0);
+		hash = 47
+				* hash
+				+ (this.ownerFullName != null ? this.ownerFullName.hashCode()
+						: 0);
+		return hash;
+	}
 
-    public void setPercentageComplete(double percentageComplete) {
-        this.percentageComplete = percentageComplete;
-    }
+	public double getPercentageComplete() {
+		return percentageComplete;
+	}
 
-    public int getNumOpenTasks() {
-        return numOpenTasks;
-    }
+	public void setPercentageComplete(double percentageComplete) {
+		this.percentageComplete = percentageComplete;
+	}
 
-    public void setNumOpenTasks(int numOpenTasks) {
-        this.numOpenTasks = numOpenTasks;
-    }
+	public int getNumOpenTasks() {
+		return numOpenTasks;
+	}
 
-    public int getNumAllTasks() {
-        return numAllTasks;
-    }
+	public void setNumOpenTasks(int numOpenTasks) {
+		this.numOpenTasks = numOpenTasks;
+	}
 
-    public void setNumAllTasks(int numAllTasks) {
-        this.numAllTasks = numAllTasks;
-    }
+	public int getNumAllTasks() {
+		return numAllTasks;
+	}
+
+	public void setNumAllTasks(int numAllTasks) {
+		this.numAllTasks = numAllTasks;
+	}
 }
--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/task/TaskGanttChartViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/task/TaskGanttChartViewImpl.java
@@ -1,30 +1,43 @@
 package com.esofthead.mycollab.module.project.view.task;
 
-import java.util.Calendar;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
-import org.jfree.chart.ChartFactory;
-import org.jfree.chart.JFreeChart;
-import org.jfree.data.gantt.Task;
-import org.jfree.data.gantt.TaskSeries;
-import org.jfree.data.gantt.TaskSeriesCollection;
-import org.jfree.data.time.SimpleTimePeriod;
+import org.joda.time.DateTime;
+import org.joda.time.DateTimeZone;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
+import ru.bazon.vaadin.ganttdiagram.GanttColumnFormatter;
+import ru.bazon.vaadin.ganttdiagram.GanttDiagram;
+import ru.bazon.vaadin.ganttdiagram.GanttDiagramCellStyleGeneratorAdapter;
+import ru.bazon.vaadin.ganttdiagram.model.GANTTCOLUMN;
+import ru.bazon.vaadin.ganttdiagram.model.GANTTDIAGRAMSCALE;
+import ru.bazon.vaadin.ganttdiagram.model.GanttDiagramModel;
+import ru.bazon.vaadin.ganttdiagram.model.GanttTask;
+import ru.bazon.vaadin.ganttdiagram.model.GanttTaskParameterDescription;
+
+import com.esofthead.mycollab.core.arguments.NumberSearchField;
 import com.esofthead.mycollab.core.arguments.SearchRequest;
+import com.esofthead.mycollab.core.arguments.StringSearchField;
+import com.esofthead.mycollab.module.project.CurrentProjectVariables;
 import com.esofthead.mycollab.module.project.domain.SimpleTask;
+import com.esofthead.mycollab.module.project.domain.SimpleTaskList;
+import com.esofthead.mycollab.module.project.domain.criteria.TaskListSearchCriteria;
 import com.esofthead.mycollab.module.project.domain.criteria.TaskSearchCriteria;
-import com.esofthead.mycollab.module.project.service.ProjectTaskService;
+import com.esofthead.mycollab.module.project.service.ProjectTaskListService;
 import com.esofthead.mycollab.vaadin.mvp.AbstractView;
-import com.esofthead.mycollab.vaadin.ui.JFreeChartWrapper;
 import com.esofthead.mycollab.vaadin.ui.ViewComponent;
 import com.esofthead.mycollab.web.AppContext;
-import com.vaadin.ui.Alignment;
 
 @ViewComponent
 public class TaskGanttChartViewImpl extends AbstractView implements
 		TaskGanttChartView {
 	private static final long serialVersionUID = 1L;
+
+	private static DateTimeFormatter dateFormat = DateTimeFormat
+			.forPattern("yyyy-MM-dd");
 
 	private TaskSearchCriteria searchCriteria;
 
@@ -33,57 +46,120 @@ public class TaskGanttChartViewImpl extends AbstractView implements
 		this.searchCriteria = searchCriteria;
 		this.removeAllComponents();
 		this.setSizeFull();
-		JFreeChart chart = createChart();
-		JFreeChartWrapper chartWrapper = new JFreeChartWrapper(chart);
 
-		this.addComponent(chartWrapper);
-		this.setComponentAlignment(chartWrapper, Alignment.MIDDLE_CENTER);
+		TaskListSearchCriteria criteria = new TaskListSearchCriteria();
+		criteria.setStatus(new StringSearchField("Open"));
+		criteria.setProjectId(new NumberSearchField(CurrentProjectVariables
+				.getProjectId()));
+
+		ProjectTaskListService taskService = AppContext
+				.getSpringBean(ProjectTaskListService.class);
+		List<SimpleTaskList> tasks = taskService
+				.findPagableListByCriteria(new SearchRequest<TaskListSearchCriteria>(
+						criteria, 0, Integer.MAX_VALUE));
+
+		GanttDiagramModel gdata = new GanttDiagramModel();
+		gdata.setGanttColumnVisible(GANTTCOLUMN.STARTTIME,
+				"Start         Date", null, null);
+		gdata.setGanttColumnVisible(GANTTCOLUMN.ENDTIME, "End           Date",
+				null, null);
+		gdata.setGanttColumnVisible(GANTTCOLUMN.COMPLETE, "Percentage", null,
+				null);
+		gdata.addParametersDescription(new GanttTaskParameterDescription(
+				"name", String.class, "", "Task                     Name"));
+
+		gdata.setColumnFormatter(GANTTCOLUMN.STARTTIME.getId(),
+				new GanttColumnFormatter<DateTime>() {
+
+					@Override
+					public String formatValue(String columnName, DateTime value) {
+						String sqlTimeString = dateFormat.print(value);
+						return sqlTimeString;
+					}
+
+				});
+
+		gdata.setColumnFormatter(GANTTCOLUMN.ENDTIME.getId(),
+				new GanttColumnFormatter<DateTime>() {
+
+					@Override
+					public String formatValue(String columnName, DateTime value) {
+						String sqlTimeString = dateFormat.print(value);
+						return sqlTimeString;
+					}
+
+				});
+		gdata.setColumnsOrder(Arrays.asList(new Object[] { "name",
+				GANTTCOLUMN.COMPLETE, GANTTCOLUMN.STARTTIME,
+				GANTTCOLUMN.ENDTIME }));
+
+		gdata.setTaskTooltipTemplate("<b>Name :</b> ${name}<br><b>Start time:</b> ${startTime}<br><b>End time:</b> ${endTime}");
+
+		for (SimpleTaskList taskList : tasks) {
+			GanttTask parentGanttTask = new GanttTask(
+					convertDateTime(taskList.getStartDate()),
+					convertDateTime(taskList.getEndDate()),
+					taskList.getPercentageComplete());
+			parentGanttTask.addParameter("name", taskList.getName());
+			List<SimpleTask> subTasks = taskList.getSubTasks();
+			for (SimpleTask subTask : subTasks) {
+				GanttTask ganttTask = new GanttTask(
+						convertDateTime(subTask.getStartdate()),
+						convertDateTime(subTask.getEnddate()),
+						subTask.getPercentagecomplete(), parentGanttTask);
+				ganttTask.addParameter("name", subTask.getTaskname());
+
+			}
+			gdata.addTask(parentGanttTask);
+		}
+		GanttDiagram diagram = new GanttDiagram(gdata);
+		diagram.setGanttDiagramCellStyleGenerator(new GanttDiagramDemoCellStyleGenerator());
+		diagram.addAvailableScale(GANTTDIAGRAMSCALE.DAY, "Day");
+		diagram.addAvailableScale(GANTTDIAGRAMSCALE.DAY_MONTH, "Day of Month");
+		diagram.addAvailableScale(GANTTDIAGRAMSCALE.WEEK, "Week");
+		diagram.addAvailableScale(GANTTDIAGRAMSCALE.MONTH, "Month");
+		diagram.addAvailableScale(GANTTDIAGRAMSCALE.YEAR, "Year");
+
+		diagram.setScale(GANTTDIAGRAMSCALE.MONTH);
+		this.addComponent(diagram);
+		this.setExpandRatio(diagram, 1.0f);
 	}
 
-	private JFreeChart createChart() {
-		ProjectTaskService taskService = AppContext
-				.getSpringBean(ProjectTaskService.class);
-		List<SimpleTask> tasks = taskService
-				.findPagableListByCriteria(new SearchRequest<TaskSearchCriteria>(
-						searchCriteria, 0, Integer.MAX_VALUE));
+	private static DateTime convertDateTime(Date date) {
+		if (date == null) {
+			return new DateTime();
+		}
+		return new DateTime(date.getTime(), DateTimeZone.UTC);
+	}
 
-		/**
-		 * Creating a task series And adding planned tasks dates on the series.
-		 */
-		TaskSeries seriesOne = new TaskSeries("Planned Implementation");
+	public class GanttDiagramDemoCellStyleGenerator extends
+			GanttDiagramCellStyleGeneratorAdapter {
 
-		final TaskSeriesCollection collection = new TaskSeriesCollection();
+		@Override
+		public String getRowStyle(GanttDiagram diagram, GanttTask task) {
 
-		for (SimpleTask task : tasks) {
-			if (task.getStartdate() != null && task.getEnddate() != null) {
-				seriesOne.add(new Task(task.getTaskname(),
-						new SimpleTimePeriod(task.getStartdate(), task
-								.getEnddate())));
-			}
+			return super.getRowStyle(diagram, task);
 		}
 
-		/**
-		 * Adding the series to the collection Holds actual Dates.
-		 */
-		collection.add(seriesOne);
-		JFreeChart chart = ChartFactory.createGanttChart("", // chart
-																// title
-				"Task", // domain axis label
-				"Date", // range axis label
-				collection, // data
-				true, // include legend
-				true, // tooltips
-				false // urls
-				);
-		return chart;
-	}
+		@Override
+		public String getCellStyle(GanttDiagram diagram, GanttTask task,
+				GANTTCOLUMN column) {
+			if (column == GANTTCOLUMN.COMPLETE) {
+				return "background-red v-table-cell-content-font-bold";
+			}
 
-	private static Date makeDate(final int day, final int month, final int year) {
+			return super.getCellStyle(diagram, task, column);
+		}
 
-		final Calendar calendar = Calendar.getInstance();
-		calendar.set(year, month, day);
-		final Date result = calendar.getTime();
-		return result;
+		@Override
+		public String getCellStyle(GanttDiagram diagram, GanttTask task,
+				String column) {
+			if (column.equals("overflow") && task.getCompleteState() == 100) {
+				return "font-bold";
+			}
+
+			return super.getCellStyle(diagram, task, column);
+		}
 
 	}
 }
