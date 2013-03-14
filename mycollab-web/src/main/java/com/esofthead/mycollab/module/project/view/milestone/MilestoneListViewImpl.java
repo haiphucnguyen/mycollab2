e61497919fe951c2100edbe1d66c95fa6a89f155
Nghi Tran <nghitran@esofthead.com>
2013-03-14 09:50:22 +0700

    word wrap milestone's name

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/milestone/MilestoneListViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/milestone/MilestoneListViewImpl.java
@@ -141,6 +141,7 @@ public class MilestoneListViewImpl extends AbstractView implements
 		milestoneLink.setStyleName("link");
 		milestoneLink.addStyleName("medium-text");
 		milestoneLink.addStyleName("bold");
+		milestoneLink.addStyleName(UIConstants.WORD_WRAP);
 		milestone.setDescription(milestone.getDescription());
 		layout.addComponent(milestoneLink);
 
