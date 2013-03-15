be273d67f1000dee2196f299b766bb92dc37bd4f
Nguyen Hai <hainguyen@esofthead.com>
2013-03-15 10:24:54 +0700

    Update project message widget

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/user/ProjectMessageListComponent.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/user/ProjectMessageListComponent.java
@@ -3,13 +3,22 @@ package com.esofthead.mycollab.module.project.view.user;
 import com.esofthead.mycollab.core.arguments.SetSearchField;
 import com.esofthead.mycollab.core.utils.DateTimeUtils;
 import com.esofthead.mycollab.module.project.CurrentProjectVariables;
+import com.esofthead.mycollab.module.project.ProjectContants;
+import com.esofthead.mycollab.module.project.ProjectResources;
 import com.esofthead.mycollab.module.project.domain.SimpleMessage;
 import com.esofthead.mycollab.module.project.domain.criteria.MessageSearchCriteria;
+import com.esofthead.mycollab.module.project.events.MessageEvent;
 import com.esofthead.mycollab.module.project.service.MessageService;
+import com.esofthead.mycollab.module.project.view.people.component.ProjectUserLink;
+import com.esofthead.mycollab.shell.view.ScreenSize;
+import com.esofthead.mycollab.vaadin.events.EventBus;
 import com.esofthead.mycollab.vaadin.ui.DefaultBeanPagedList;
 import com.esofthead.mycollab.vaadin.ui.Depot;
 import com.esofthead.mycollab.web.AppContext;
 import com.vaadin.lazyloadwrapper.LazyLoadWrapper;
+import com.vaadin.terminal.Sizeable;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
@@ -43,14 +52,37 @@ public class ProjectMessageListComponent extends Depot {
 			DefaultBeanPagedList.RowDisplayHandler<SimpleMessage> {
 
 		@Override
-		public Component generateRow(SimpleMessage obj, int rowIndex) {
+		public Component generateRow(final SimpleMessage obj, int rowIndex) {
 			CssLayout layout = new CssLayout();
 			layout.setWidth("100%");
 			layout.setStyleName("activity-stream");
 
 			CssLayout header = new CssLayout();
 			header.setStyleName("stream-content");
-			header.addComponent(new Label(obj.getTitle()));
+			header.addComponent(new ProjectUserLink(obj.getPosteduser(), obj
+					.getFullPostedUserName(), true));
+			
+			Label actionLbl = new Label(" added news ");
+			actionLbl.setWidth(Sizeable.SIZE_UNDEFINED, 0);
+			header.addComponent(actionLbl);
+
+			Button messageLink = new Button(handleText(obj.getTitle()),
+					new Button.ClickListener() {
+						private static final long serialVersionUID = 1L;
+
+						@Override
+						public void buttonClick(ClickEvent event) {
+							EventBus.getInstance().fireEvent(
+									new MessageEvent.GotoRead(
+											MessageRowDisplayHandler.this, obj
+													.getId()));
+						}
+					});
+			messageLink.setStyleName("link");
+			messageLink.setIcon(ProjectResources
+					.getIconResource16size(ProjectContants.MESSAGE));
+			header.addComponent(messageLink);
+			
 			layout.addComponent(header);
 
 			CssLayout body = new CssLayout();
@@ -63,5 +95,16 @@ public class ProjectMessageListComponent extends Depot {
 			return layout;
 		}
 
+	}
+
+	private static String handleText(String value) {
+		int limitValue = 45;
+		if (ScreenSize.hasSupport1024Pixels()) {
+			limitValue = 35;
+		}
+		if (value.length() > limitValue) {
+			return value.substring(0, limitValue) + "...";
+		}
+		return value;
 	}
 }
