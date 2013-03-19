de83637850ce1f33b6266cd3a493f928a955e046
nobugz <nobugz@live.com>
2013-03-19 15:34:56 +0700

    add source migrate db

--- mycollab_db_migrate/src/main/java/com/esofthead/db/sqldump/DbConfiguration.java
+++ mycollab_db_migrate/src/main/java/com/esofthead/db/sqldump/DbConfiguration.java
@@ -7,34 +7,15 @@ public class DbConfiguration {
 	public static final String USER_NAME = "db.username";
 	public static final String PASSWORD = "db.password";
 	public static final String URL = "db.url";
-	public static final String SCHEMA = "db.schema";
 
 	public static final String H2_DRIVER = "h2.driverClassName";
 	public static final String H2_URL = "h2.url";
 
 	private static final String RESOURCE_PROPERTIES = "config.properties";
-//	private static Properties properties;
-//	static {
-//		properties = new Properties();
-//		try {
-//			properties.load(Thread.currentThread().getContextClassLoader()
-//					.getResourceAsStream(RESOURCE_PROPERTIES));
-//		} catch (Exception e) {
-//			properties = null;
-//			e.printStackTrace();
-//		}
-//	}
-
-//	public static String getProperty(String key) {
-//		if (null != properties)
-//			return properties.getProperty(key);
-//		return null;
-//	}
 	
 	private String userName;
 	private String password;
 	private String url;
-	private String schema;
 
 	public static DbConfiguration loadDefault() {
 		Properties properties = new Properties();
@@ -47,7 +28,6 @@ public class DbConfiguration {
 			config.setPassword(properties.getProperty(PASSWORD));
 			config.setUserName(properties.getProperty(USER_NAME));
 			config.setUrl(properties.getProperty(URL));
-			config.setSchema(properties.getProperty(SCHEMA));
 			
 			return config;
 		} catch (Exception e) {
@@ -95,19 +75,5 @@ public class DbConfiguration {
 	 */
 	public void setUrl(String url) {
 		this.url = url;
-	}
-
-	/**
-	 * @return the schema
-	 */
-	public String getSchema() {
-		return schema;
-	}
-
-	/**
-	 * @param schema the schema to set
-	 */
-	public void setSchema(String schema) {
-		this.schema = schema;
 	}
 }
