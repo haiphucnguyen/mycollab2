47b1ef0905eb0b19647e8057a9487ffc97932b45
Nguyen Hai <hainguyen@esofthead.com>
2013-03-22 11:52:30 +0700

    Update bug, version and component navigation

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/ComponentUrlResolver.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/ComponentUrlResolver.java
@@ -5,14 +5,20 @@ import com.esofthead.mycollab.core.arguments.NumberSearchField;
 import com.esofthead.mycollab.module.project.events.ProjectEvent;
 import com.esofthead.mycollab.module.project.view.parameters.ComponentScreenData;
 import com.esofthead.mycollab.module.project.view.parameters.ProjectScreenData;
+import com.esofthead.mycollab.module.tracker.domain.Component;
+import com.esofthead.mycollab.module.tracker.domain.SimpleComponent;
 import com.esofthead.mycollab.module.tracker.domain.criteria.ComponentSearchCriteria;
+import com.esofthead.mycollab.module.tracker.service.ComponentService;
 import com.esofthead.mycollab.vaadin.events.EventBus;
 import com.esofthead.mycollab.vaadin.mvp.PageActionChain;
 import com.esofthead.mycollab.vaadin.mvp.UrlResolver;
+import com.esofthead.mycollab.web.AppContext;
 
 public class ComponentUrlResolver extends UrlResolver {
 	public ComponentUrlResolver() {
 		this.addSubResolver("list", new ListUrlResolver());
+		this.addSubResolver("add", new AddUrlResolver());
+		this.addSubResolver("edit", new EditUrlResolver());
 		this.addSubResolver("preview", new PreviewUrlResolver());
 	}
 
@@ -34,6 +40,20 @@ public class ComponentUrlResolver extends UrlResolver {
 		}
 	}
 
+	private static class AddUrlResolver extends UrlResolver {
+		@Override
+		protected void handlePage(String... params) {
+			String decodeUrl = UrlEncodeDecoder.decode(params[0]);
+			int projectId = Integer.parseInt(decodeUrl);
+
+			PageActionChain chain = new PageActionChain(
+					new ProjectScreenData.Goto(projectId),
+					new ComponentScreenData.Add(new Component()));
+			EventBus.getInstance().fireEvent(
+					new ProjectEvent.GotoMyProject(this, chain));
+		}
+	}
+
 	private static class PreviewUrlResolver extends UrlResolver {
 		@Override
 		protected void handlePage(String... params) {
@@ -45,6 +65,27 @@ public class ComponentUrlResolver extends UrlResolver {
 			PageActionChain chain = new PageActionChain(
 					new ProjectScreenData.Goto(projectId),
 					new ComponentScreenData.Read(componentId));
+			EventBus.getInstance().fireEvent(
+					new ProjectEvent.GotoMyProject(this, chain));
+		}
+	}
+
+	private static class EditUrlResolver extends UrlResolver {
+		@Override
+		protected void handlePage(String... params) {
+			String decodeUrl = UrlEncodeDecoder.decode(params[0]);
+			String[] tokens = decodeUrl.split("/");
+
+			int projectId = Integer.parseInt(tokens[0]);
+			int componentId = Integer.parseInt(tokens[1]);
+
+			ComponentService componentService = AppContext
+					.getSpringBean(ComponentService.class);
+			SimpleComponent component = componentService
+					.findComponentById(componentId);
+			PageActionChain chain = new PageActionChain(
+					new ProjectScreenData.Goto(projectId),
+					new ComponentScreenData.Edit(component));
 			EventBus.getInstance().fireEvent(
 					new ProjectEvent.GotoMyProject(this, chain));
 		}
