5979d797c988debe727556c02e08406be750f67a
Nguyen Hai <hainguyen@esofthead.com>
2013-03-22 16:32:24 +0700

    clean code

--- mycollab_db_migrate/src/main/java/com/esofthead/db/sqldump/DbExport.java
+++ mycollab_db_migrate/src/main/java/com/esofthead/db/sqldump/DbExport.java
@@ -1,8 +1,14 @@
 package com.esofthead.db.sqldump;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
 import java.util.zip.GZIPOutputStream;
 
 import com.esofthead.util.sqldump.data.Schema;
@@ -23,53 +29,39 @@ public class DbExport {
 		} else {
 			writer = new OutputStreamWriter(out);
 		}
-		
-		 exportDb(configuration, writer);
-		
-		 writer.flush();
-		 writer.close();
+
+		exportDb(configuration, writer);
+
+		writer.flush();
+		writer.close();
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 
-		
-		
 		/*
 		 * Code migrate h2 database
 		 */
-//		File file = new File("D:/out/data");
-//		FileInputStream fin = new FileInputStream(file);
-//		ByteArrayOutputStream sout = new ByteArrayOutputStream();
-//		byte[] buffer = new byte[4096];
-//		int byteRead;
-//		while ((byteRead = fin.read(buffer)) != -1) {
-//			sout.write(buffer, 0, byteRead);
-//		}
-//		fin.close();
-//		sout.flush();
-//		sout.close();
-//
-//		executeNonQuery(new String(sout.toByteArray()));
+		File file = new File("D:/out/data");
+		FileInputStream fin = new FileInputStream(file);
+		ByteArrayOutputStream sout = new ByteArrayOutputStream();
+		byte[] buffer = new byte[4096];
+		int byteRead;
+		while ((byteRead = fin.read(buffer)) != -1) {
+			sout.write(buffer, 0, byteRead);
+		}
+		fin.close();
+		sout.flush();
+		sout.close();
+
+		executeNonQuery(new String(sout.toByteArray()));
 	}
 
-//	public static final String processingStringValue(String data) {
-//		String result = data.replace("\\", "\\\\");
-//		result = result.replace("\0", "\\0");
-//		result = result.replace("'", "\\'");
-//		result = result.replace("\"", "\\\"");
-//		result = result.replace("\b", "\\b");
-//		result = result.replace("\n", "\\n");
-//		result = result.replace("\r", "\\r");
-//		result = result.replace("\t", "\\t");
-//		return result;
-//	}
-//
-//	public static void executeNonQuery(String query) throws Exception {
-//		Class.forName("org.h2.Driver");
-//		Connection con = (Connection) DriverManager
-//				.getConnection("jdbc:h2:~/mycollab_extdb;MODE=MySQL");
-//		Statement stmt = (Statement) con.createStatement();
-//		stmt.execute(query);
-//		con.close();
-//	}
+	public static void executeNonQuery(String query) throws Exception {
+		Class.forName("org.h2.Driver");
+		Connection con = (Connection) DriverManager
+				.getConnection("jdbc:h2:~/mycollab_extdb;MODE=MySQL");
+		Statement stmt = (Statement) con.createStatement();
+		stmt.execute(query);
+		con.close();
+	}
 }
