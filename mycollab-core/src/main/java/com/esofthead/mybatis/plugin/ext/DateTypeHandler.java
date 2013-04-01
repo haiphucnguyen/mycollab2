5b2e47fca6cf8884fcc3f5d1cc2458a36da9693c
Nguyen Hai <hainguyen@esofthead.com>
2013-04-01 16:00:43 +0700

    Update pom file

--- mycollab-core/src/main/java/com/esofthead/mybatis/plugin/ext/DateTypeHandler.java
+++ mycollab-core/src/main/java/com/esofthead/mybatis/plugin/ext/DateTypeHandler.java
@@ -5,10 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.text.ParseException;
 import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.TimeZone;
 
 import org.apache.ibatis.type.BaseTypeHandler;
 import org.apache.ibatis.type.JdbcType;
@@ -17,9 +14,9 @@ import org.apache.ibatis.type.MappedJdbcTypes;
 @MappedJdbcTypes(JdbcType.TIMESTAMP)
 public class DateTypeHandler extends BaseTypeHandler<Date> {
 
-	static {
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-	}
+	// static {
+	// TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	// }
 
 	@Override
 	public void setNonNullParameter(PreparedStatement ps, int i,
@@ -55,18 +52,5 @@ public class DateTypeHandler extends BaseTypeHandler<Date> {
 			return new Date(sqlTimestamp.getTime());
 		}
 		return null;
-	}
-
-	public static void main(String[] args) throws ParseException {
-		Date d = new GregorianCalendar().getTime();
-		System.out.println(d + "---" + d.getTime());
-
-		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-		Date d1 = new GregorianCalendar(TimeZone.getTimeZone("UTC")).getTime();
-		// String[] availableIDs = TimeZone.getAvailableIDs();
-		// for (String id:availableIDs) {
-		// System.out.println(id + " " +
-		// TimeZone.getTimeZone(id).getRawOffset()/(1000*60*60));
-		// }
 	}
 }
