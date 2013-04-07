ca077ed93b537b5e81faab43afccdd49330765eb
Nguyen Hai <hainguyen@esofthead.com>
2013-04-07 16:01:47 +0700

    Add number of bugs and tasks to project member domain

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/domain/SimpleProjectMember.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/domain/SimpleProjectMember.java
@@ -7,39 +7,37 @@ package com.esofthead.mycollab.module.project.domain;
 import com.esofthead.mycollab.common.domain.PermissionMap;
 
 /**
- *
+ * 
  * @author haiphucnguyen
  */
 public class SimpleProjectMember extends ProjectMember {
 	private static final long serialVersionUID = 1L;
 
 	private String memberFullName;
-    
-    private String roleName;
-    
-    private PermissionMap permissionMaps;
-    
-    private int numOpenTasks;
-	private int numTasks;
-	
+
+	private String roleName;
+
+	private PermissionMap permissionMaps;
+
+	private int numOpenTasks;
+
 	private int numOpenBugs;
-	private int numBugs;
 
-    public String getMemberFullName() {
-        return memberFullName;
-    }
+	public String getMemberFullName() {
+		return memberFullName;
+	}
 
-    public void setMemberFullName(String memberFullName) {
-        this.memberFullName = memberFullName;
-    }
+	public void setMemberFullName(String memberFullName) {
+		this.memberFullName = memberFullName;
+	}
 
-    public String getRoleName() {
-        return roleName;
-    }
+	public String getRoleName() {
+		return roleName;
+	}
 
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
+	public void setRoleName(String roleName) {
+		this.roleName = roleName;
+	}
 
 	public PermissionMap getPermissionMaps() {
 		return permissionMaps;
@@ -57,27 +55,11 @@ public class SimpleProjectMember extends ProjectMember {
 		this.numOpenTasks = numOpenTasks;
 	}
 
-	public int getNumTasks() {
-		return numTasks;
-	}
-
-	public void setNumTasks(int numTasks) {
-		this.numTasks = numTasks;
-	}
-
 	public int getNumOpenBugs() {
 		return numOpenBugs;
 	}
 
 	public void setNumOpenBugs(int numOpenBugs) {
 		this.numOpenBugs = numOpenBugs;
-	}
-
-	public int getNumBugs() {
-		return numBugs;
-	}
-
-	public void setNumBugs(int numBugs) {
-		this.numBugs = numBugs;
 	}
 }
