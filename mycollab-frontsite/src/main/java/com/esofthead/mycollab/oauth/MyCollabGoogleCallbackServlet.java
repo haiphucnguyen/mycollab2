1b21e9f26f80c972bd5a3ee422dc12d5a0ad629a
Hai Nguyen <hainguyen@esofthead.com>
2013-04-29 09:24:01 +0700

    Update google authenticate

---
+++ mycollab-frontsite/src/main/java/com/esofthead/mycollab/oauth/MyCollabGoogleCallbackServlet.java
@@ -0,0 +1,41 @@
+package com.esofthead.mycollab.oauth;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+public class MyCollabGoogleCallbackServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		GoogleOauthHelper helper = new GoogleOauthHelper();
+		if (request.getParameter("code") != null
+				&& request.getParameter("state").equals("google")) {
+
+			/*
+			 * Executes after google redirects to the callback url. Please note
+			 * that the state request parameter is for convenience to
+			 * differentiate between authentication methods (ex. facebook oauth,
+			 * google oauth, twitter, in-house).
+			 * 
+			 * GoogleAuthHelper()#getUserInfoJson(String) method returns a
+			 * String containing the json representation of the authenticated
+			 * user's information. At this point you should parse and persist
+			 * the info.
+			 */
+			Gson gSon = new Gson();
+			GoogleUser user = gSon.fromJson(
+					helper.getUserInfoJson(request.getParameter("code")),
+					GoogleUser.class);
+			System.out.println(user.getEmail() + " " + user.getId() + " "
+					+ user.getName());
+		}
+	}
+
+}
