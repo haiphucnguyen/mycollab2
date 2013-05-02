8e45c1f13c0001d0d58b20544f5dc2e3960aa1d5
linh nguyen <linhnguyen@esofthead.com>
2013-05-02 14:40:10 +0700

    create bug ads

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/BugAdvertisementViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/BugAdvertisementViewImpl.java
@@ -1,15 +1,39 @@
 package com.esofthead.mycollab.module.project.view.bug;
 
 import com.esofthead.mycollab.vaadin.mvp.AbstractView;
+import com.esofthead.mycollab.vaadin.ui.GenericForm;
+import com.esofthead.mycollab.vaadin.ui.IFormLayoutFactory;
 import com.esofthead.mycollab.vaadin.ui.ViewComponent;
-import com.vaadin.ui.Label;
+import com.vaadin.ui.CustomLayout;
+import com.vaadin.ui.Field;
+import com.vaadin.ui.Layout;
 
 @ViewComponent
 public class BugAdvertisementViewImpl extends AbstractView implements
 		BugAdvertisementView {
 	private static final long serialVersionUID = 1L;
+	private final GenericForm form;
 
 	public BugAdvertisementViewImpl() {
-		this.addComponent(new Label("Please upgrade"));
+		form = new GenericForm();
+		form.setFormLayoutFactory(new BugFormLayoutFactory());
+		this.addComponent(form);
+	}
+	
+	private static class BugFormLayoutFactory implements IFormLayoutFactory {
+		private static final long serialVersionUID = 1L;
+		private CustomLayout bugFormLayout;
+
+		@Override
+		public Layout getLayout() {
+			bugFormLayout = new CustomLayout("ads/bug/bugAds");
+			return bugFormLayout;
+		}
+
+		@Override
+		public void attachField(Object propertyId, Field field) {
+			
+		}
+
 	}
 }
