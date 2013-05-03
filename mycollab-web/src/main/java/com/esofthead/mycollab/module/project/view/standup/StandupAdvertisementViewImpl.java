98b96c44545c96b7708ac4cb342d4ae46cbe203a
linh nguyen <linhnguyen@esofthead.com>
2013-05-03 10:08:51 +0700

    create standup ads

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/standup/StandupAdvertisementViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/standup/StandupAdvertisementViewImpl.java
@@ -1,8 +1,13 @@
 package com.esofthead.mycollab.module.project.view.standup;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.esofthead.mycollab.common.ApplicationProperties;
 import com.esofthead.mycollab.vaadin.mvp.AbstractView;
 import com.esofthead.mycollab.vaadin.ui.ViewComponent;
-import com.vaadin.ui.Label;
+import com.vaadin.terminal.ExternalResource;
+import com.vaadin.ui.Embedded;
 
 @ViewComponent
 public class StandupAdvertisementViewImpl extends AbstractView implements
@@ -10,7 +15,22 @@ public class StandupAdvertisementViewImpl extends AbstractView implements
 	private static final long serialVersionUID = 1L;
 	
 	public StandupAdvertisementViewImpl() {
-		this.addComponent(new Label("Please upgrade"));
+		this.setWidth("100%");
+		this.setHeight("1024px");
+		URL url = null;
+		try {
+			url = new URL(
+					ApplicationProperties
+							.getProperty(ApplicationProperties.APP_URL)
+							+ "assets/ads/standup/standupAds.html");
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		}
+		Embedded browser = new Embedded("", new ExternalResource(url));
+		browser.setWidth("100%");
+		browser.setHeight("100%");
+		browser.setType(Embedded.TYPE_BROWSER);
+		this.addComponent(browser);
 	}
 
 }
