4dd20155101e78d794242babb9ced09fba08290b
linh nguyen <linhnguyen@esofthead.com>
2013-05-09 15:52:35 +0700

    create total hour time tracking

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/localization/TimeTrackingI18nEnum.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/localization/TimeTrackingI18nEnum.java
@@ -7,5 +7,5 @@ import ch.qos.cal10n.LocaleData;
 @BaseName("localization/project/timetracking")
 @LocaleData({ @Locale("en_US") })
 public enum TimeTrackingI18nEnum {
-	TIME_RECORD_HEADER, TASK_LIST_RANGE, SEARCH_TIME_TITLE
+	TIME_RECORD_HEADER, TASK_LIST_RANGE, TASK_LIST_RANGE_WITH_TOTAL_HOUR, SEARCH_TIME_TITLE
 }
--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/time/TimeTrackingListViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/time/TimeTrackingListViewImpl.java
@@ -1,9 +1,11 @@
 package com.esofthead.mycollab.module.project.view.time;
 
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import com.esofthead.mycollab.common.MonitorTypeConstants;
 import com.esofthead.mycollab.core.arguments.RangeDateSearchField;
+import com.esofthead.mycollab.core.arguments.SearchRequest;
 import com.esofthead.mycollab.module.file.ExportTimeLoggingStreamResource;
 import com.esofthead.mycollab.module.file.FieldExportColumn;
 import com.esofthead.mycollab.module.project.CurrentProjectVariables;
@@ -49,6 +51,7 @@ public class TimeTrackingListViewImpl extends AbstractView implements
 	private ItemTimeLoggingSearchPanel itemTimeLoggingPanel;
 	private ItemTimeLoggingSearchCriteria itemTimeLogginSearchCriteria;
 	private Button exportBtn;
+	private ItemTimeLoggingService itemTimeLoggingService;
 	private static final FieldExportColumn[] EXPORT_COLUMNS = new FieldExportColumn[] {
 		new FieldExportColumn("logUserFullName", "User"),
 		new FieldExportColumn("type", "Type"),
@@ -62,6 +65,8 @@ public class TimeTrackingListViewImpl extends AbstractView implements
 
 		this.setSpacing(true);
 		this.setMargin(false, true, true, true);
+		
+		itemTimeLoggingService = AppContext.getSpringBean(ItemTimeLoggingService.class);
 
 		Label titleLbl = new Label(
 				LocalizationHelper
@@ -113,15 +118,27 @@ public class TimeTrackingListViewImpl extends AbstractView implements
 		initUI();
 	}
 
+	@SuppressWarnings("unchecked")
 	private void setTimeRange() {
 		RangeDateSearchField rangeField = itemTimeLogginSearchCriteria
 				.getRangeDate();
 
 		String fromDate = AppContext.formatDate(rangeField.getFrom());
 		String toDate = AppContext.formatDate(rangeField.getTo());
-
-		lbTimeRange.setValue(LocalizationHelper.getMessage(
-				TimeTrackingI18nEnum.TASK_LIST_RANGE, fromDate, toDate));
+		
+		List<SimpleItemTimeLogging> lstItemLogging = itemTimeLoggingService.findPagableListByCriteria(new SearchRequest<ItemTimeLoggingSearchCriteria>(itemTimeLogginSearchCriteria, 0, Integer.MAX_VALUE));
+		double totalHour = 0;
+		for (SimpleItemTimeLogging simpleItemTimeLogging : lstItemLogging) {
+			totalHour += simpleItemTimeLogging.getLogvalue();
+		}
+		
+		if (totalHour > 0) {
+			lbTimeRange.setValue(LocalizationHelper.getMessage(
+					TimeTrackingI18nEnum.TASK_LIST_RANGE_WITH_TOTAL_HOUR, fromDate, toDate, totalHour));
+		} else {
+			lbTimeRange.setValue(LocalizationHelper.getMessage(
+					TimeTrackingI18nEnum.TASK_LIST_RANGE, fromDate, toDate));
+		}
 	}
 
 	private void initUI() {
