0915f075fdc1ee883cb903f3587298b09fa13eeb
Hai Nguyen <hainguyen@esofthead.com>
2013-05-15 21:04:44 +0700

    Update user url handler

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/user/accountsettings/view/AccountUrlResolver.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/user/accountsettings/view/AccountUrlResolver.java
@@ -3,7 +3,9 @@ package com.esofthead.mycollab.module.user.accountsettings.view;
 import com.esofthead.mycollab.module.user.accountsettings.team.view.RoleUrlResolver;
 import com.esofthead.mycollab.module.user.accountsettings.team.view.UserUrlResolver;
 import com.esofthead.mycollab.module.user.accountsettings.view.events.ProfileEvent;
+import com.esofthead.mycollab.shell.events.ShellEvent;
 import com.esofthead.mycollab.vaadin.events.EventBus;
+import com.esofthead.mycollab.vaadin.mvp.ModuleHelper;
 import com.esofthead.mycollab.vaadin.mvp.UrlResolver;
 
 public class AccountUrlResolver extends UrlResolver {
@@ -11,6 +13,16 @@ public class AccountUrlResolver extends UrlResolver {
 		this.addSubResolver("preview", new ReadUrlResolver());
 		this.addSubResolver("user", new UserUrlResolver());
 		this.addSubResolver("role", new RoleUrlResolver());
+	}
+
+	@Override
+	public void handle(String... params) {
+		if (!ModuleHelper.isCurrentAccountModule()) {
+			EventBus.getInstance().fireEvent(
+					new ShellEvent.GotoUserAccountModule(this, params));
+		} else {
+			super.handle(params);
+		}
 	}
 
 	private static class ReadUrlResolver extends UrlResolver {
