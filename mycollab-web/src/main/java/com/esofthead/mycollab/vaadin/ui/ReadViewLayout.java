73e515497114d2e50442a648b4c88b6603f6412b
nghitran <nghitran@esofthead.com>
2013-05-16 17:16:02 +0700

    update style for ReadViewLayout

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/ReadViewLayout.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/ReadViewLayout.java
@@ -2,40 +2,48 @@ package com.esofthead.mycollab.vaadin.ui;
 
 import com.github.wolfie.detachedtabs.DetachedTabs;
 import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
-import com.vaadin.ui.Embedded;
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Label;
-import com.vaadin.ui.VerticalLayout;
 
-public class ReadViewLayout extends VerticalLayout {
+public class ReadViewLayout extends CssLayout {
 	private static final long serialVersionUID = 1L;
 
-	private HorizontalLayout header;
-	private final Embedded iconEmbed;
-	private Label titleLbl;
-	private DetachedTabs viewTab;
-	private CssLayout body;
+	private final HorizontalLayout header;
+	// private final Embedded iconEmbed;
+	private final Label titleLbl;
+	private final DetachedTabs viewTab;
+	private final CssLayout body;
 
 	public ReadViewLayout(ThemeResource icon) {
 		this.setSizeFull();
+		this.setStyleName("readview-layout");
+
 		header = new HorizontalLayout();
+		header.setWidth("100%");
+		header.setMargin(true, true, false, true);
+		header.setStyleName("readview-layout-header");
 		this.addComponent(header);
 
-		iconEmbed = new Embedded();
-		iconEmbed.setSource(icon);
-		header.addComponent(iconEmbed);
+		// iconEmbed = new Embedded();
+		// iconEmbed.setSource(icon);
+		// header.addComponent(iconEmbed);
 
 		titleLbl = new Label();
+		titleLbl.setStyleName("h1");
 		header.addComponent(titleLbl);
+		header.setExpandRatio(titleLbl, 1.0f);
 
 		body = new CssLayout();
+		body.setStyleName("readview-layout-body");
 		body.setSizeFull();
 		this.addComponent(body);
-		this.setExpandRatio(body, 1.0f);
+		// this.setExpandRatio(body, 1.0f);
 
 		viewTab = new DetachedTabs.Horizontal(body);
+		viewTab.setSizeUndefined();
 		header.addComponent(viewTab);
 	}
 
@@ -44,11 +52,11 @@ public class ReadViewLayout extends VerticalLayout {
 	}
 
 	public void setTitle(String title) {
-		titleLbl.setCaption(title);
+		titleLbl.setValue(title);
 	}
 
 	public void addTab(Component content, String button) {
-		viewTab.addTab(content, button);
+		viewTab.addTab(content, new Button(button));
 	}
 
 	public void selectTab(String viewName) {
