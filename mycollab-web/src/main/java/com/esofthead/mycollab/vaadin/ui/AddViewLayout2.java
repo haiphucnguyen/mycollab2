1fc6000b24c154a9fb8c962ed7c29de2ef35171f
nghitran <nghitran@esofthead.com>
2013-05-21 17:24:39 +0700

    update AddViewLayout

---
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/AddViewLayout2.java
@@ -0,0 +1,57 @@
+package com.esofthead.mycollab.vaadin.ui;
+
+import com.vaadin.terminal.ThemeResource;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.CssLayout;
+import com.vaadin.ui.Embedded;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
+
+public class AddViewLayout2 extends CssLayout {
+	private static final long serialVersionUID = 1L;
+
+	private final HorizontalLayout header;
+	private final Embedded iconEmbed;
+	private final Label titleLbl;
+	private final VerticalLayout body;
+
+	public AddViewLayout2(String title, ThemeResource icon) {
+		// this.setSizeFull();
+		setStyleName("addview-layout");
+
+		header = new HorizontalLayout();
+		header.setWidth("100%");
+		header.setMargin(true, true, false, true);
+		header.setStyleName("addview-layout-header");
+		this.addComponent(header);
+
+		iconEmbed = new Embedded();
+		iconEmbed.setSource(icon);
+		header.addComponent(iconEmbed);
+
+		titleLbl = new Label(title);
+		titleLbl.setStyleName("h1");
+		header.addComponent(titleLbl);
+		header.setExpandRatio(titleLbl, 1.0f);
+
+		body = new VerticalLayout();
+		body.setStyleName("addview-layout-body");
+		this.addComponent(body);
+	}
+
+	public void addBody(ComponentContainer body) {
+		this.body.addComponent(body);
+		this.body.setExpandRatio(body, 1.0f);
+	}
+
+	public void addControlButtons(Component controlsBtn) {
+		controlsBtn.addStyleName("control-buttons");
+		body.addComponent(controlsBtn);
+	}
+
+	public void setTitle(String title) {
+		titleLbl.setValue(title);
+	}
+}
