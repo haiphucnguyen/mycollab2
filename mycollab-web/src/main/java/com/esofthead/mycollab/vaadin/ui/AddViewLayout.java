5631da0c671ae7ae5679fb6b94039dd8f4543054
robertduy <cuongnguyen@esofthead.com>
2013-05-31 09:48:37 +0700

    no change

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/AddViewLayout.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/AddViewLayout.java
@@ -1,6 +1,9 @@
 package com.esofthead.mycollab.vaadin.ui;
 
+import com.github.wolfie.detachedtabs.DetachedTabs;
 import com.vaadin.terminal.Resource;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Component;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.CustomLayout;
 import com.vaadin.ui.Embedded;
@@ -12,7 +15,7 @@ public class AddViewLayout extends CustomLayout {
 
 	private final Label titleLbl;
 	private final Embedded icon;
-
+	
 	public AddViewLayout(String title, Resource resource) {
 		super("addView");
 
@@ -56,4 +59,5 @@ public class AddViewLayout extends CustomLayout {
 	public void addBody(ComponentContainer body) {
 		this.addComponent(body, "addViewBody");
 	}
+	
 }
