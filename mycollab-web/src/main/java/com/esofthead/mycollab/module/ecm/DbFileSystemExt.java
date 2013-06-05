fc78fd6041be646c58f1128da0ee12ae2dd2de76
Hai Nguyen <hainguyen@esofthead.com>
2013-06-05 16:09:59 +0700

    Fix compile in test package

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/ecm/DbFileSystemExt.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/ecm/DbFileSystemExt.java
@@ -1,12 +1,11 @@
 package com.esofthead.mycollab.module.ecm;
 
 import org.apache.jackrabbit.core.fs.db.DbFileSystem;
-import org.apache.jackrabbit.core.fs.db.DerbyFileSystem;
 
 import com.esofthead.mycollab.common.ApplicationProperties;
 
 public class DbFileSystemExt extends DbFileSystem {
-	DerbyFileSystem a;
+	
 	public DbFileSystemExt() {
 		this.schema = "mysql";
 		this.url = ApplicationProperties
