9ad4faa18d62836ad55e231dbae7081f38708e57
robertduy <cuongnguyen@esofthead.com>
2013-06-05 19:24:51 +0700

    My first look to pass(quick) update multi-selected Account problems.

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/dao/AccountMapperExt.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/dao/AccountMapperExt.java
@@ -1,5 +1,7 @@
 package com.esofthead.mycollab.module.crm.dao;
 
+import java.util.HashMap;
+
 import com.esofthead.mycollab.core.persistence.ISearchableDAO;
 import com.esofthead.mycollab.module.crm.domain.SimpleAccount;
 import com.esofthead.mycollab.module.crm.domain.criteria.AccountSearchCriteria;
@@ -7,4 +9,6 @@ import com.esofthead.mycollab.module.crm.domain.criteria.AccountSearchCriteria;
 public interface AccountMapperExt extends ISearchableDAO<AccountSearchCriteria> {
 
 	public abstract SimpleAccount findAccountById(int accountId);
+	
+	public abstract void updateListAccount(HashMap<String,AccountSearchCriteria> map);
 }
