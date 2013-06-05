1563263ce85211ee45dc82a6fccfcd47c0eb4688
robertduy <cuongnguyen@esofthead.com>
2013-06-05 13:39:33 +0700

    Iteam Time Logging edit

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/domain/SimpleItemTimeLogging.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/domain/SimpleItemTimeLogging.java
@@ -4,6 +4,12 @@ public class SimpleItemTimeLogging extends ItemTimeLogging {
 	private static final long serialVersionUID = 1L;
 
 	private String logUserFullName;
+	
+	private String projectName;
+	
+	private String projectShortName;
+	
+	private String summary;
 
 	public String getLogUserFullName() {
 		return logUserFullName;
@@ -13,4 +19,27 @@ public class SimpleItemTimeLogging extends ItemTimeLogging {
 		this.logUserFullName = logUserFullName;
 	}
 
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+	public String getProjectShortName() {
+		return projectShortName;
+	}
+
+	public void setProjectShortName(String projectShortName) {
+		this.projectShortName = projectShortName;
+	}
+
+	public String getSummary() {
+		return summary;
+	}
+
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
 }
