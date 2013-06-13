01a2b0bc6ccceb4dd23c2f1ef0412eecd640fd86
robertduy <cuongnguyen@esofthead.com>
2013-06-13 11:34:00 +0700

    Finish Mass Update All screen.

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/dao/CaseMapperExt.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/dao/CaseMapperExt.java
@@ -1,16 +1,12 @@
 package com.esofthead.mycollab.module.crm.dao;
 
-import org.apache.ibatis.annotations.Param;
-
-import com.esofthead.mycollab.core.arguments.SearchCriteria;
+import com.esofthead.mycollab.core.persistence.IMassUpdateDAO;
 import com.esofthead.mycollab.core.persistence.ISearchableDAO;
-import com.esofthead.mycollab.module.crm.domain.Account;
 import com.esofthead.mycollab.module.crm.domain.CaseWithBLOBs;
 import com.esofthead.mycollab.module.crm.domain.SimpleCase;
 import com.esofthead.mycollab.module.crm.domain.criteria.CaseSearchCriteria;
 
-public interface CaseMapperExt extends ISearchableDAO<CaseSearchCriteria> {
+public interface CaseMapperExt extends ISearchableDAO<CaseSearchCriteria> ,
+	IMassUpdateDAO<CaseWithBLOBs, CaseSearchCriteria>{
 	SimpleCase findCaseById(int caseId);
-	
-	public abstract void upateCaseBySearchCriteria(@Param("record") CaseWithBLOBs record, @Param("searchCriteria")SearchCriteria searchCriteria);
 }
--- mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/dao/OpportunityMapperExt.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/dao/OpportunityMapperExt.java
@@ -5,14 +5,15 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 import com.esofthead.mycollab.common.domain.GroupItem;
-import com.esofthead.mycollab.core.arguments.SearchCriteria;
+import com.esofthead.mycollab.core.persistence.IMassUpdateDAO;
 import com.esofthead.mycollab.core.persistence.ISearchableDAO;
 import com.esofthead.mycollab.module.crm.domain.Opportunity;
 import com.esofthead.mycollab.module.crm.domain.SimpleOpportunity;
 import com.esofthead.mycollab.module.crm.domain.criteria.OpportunitySearchCriteria;
 
 public interface OpportunityMapperExt extends
-		ISearchableDAO<OpportunitySearchCriteria> {
+		ISearchableDAO<OpportunitySearchCriteria>,
+		IMassUpdateDAO<Opportunity, OpportunitySearchCriteria>{
 
 	SimpleOpportunity findOpportunityById(int opportunityId);
 
@@ -22,7 +23,4 @@ public interface OpportunityMapperExt extends
 	List<GroupItem> getLeadSourcesSummary(
 			@Param("searchCriteria") OpportunitySearchCriteria criteria);
 
-	public abstract void upateOpportunityBySearchCriteria(
-			@Param("record") Opportunity record,
-			@Param("searchCriteria") SearchCriteria searchCriteria);
 }
