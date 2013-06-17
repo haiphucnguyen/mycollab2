a175190d57d349471ff7d18da95c97bdb7a8284a
Hai Nguyen <hainguyen@esofthead.com>
2013-06-17 16:26:25 +0700

    Update string check null or empty value

--- mycollab-core/src/main/java/com/esofthead/mycollab/core/utils/StringUtil.java
+++ mycollab-core/src/main/java/com/esofthead/mycollab/core/utils/StringUtil.java
@@ -4,4 +4,12 @@ public class StringUtil {
 	public static boolean isNotNullOrEmpty(String str) {
 		return (str != null) && (!str.trim().equals(""));
 	}
+
+	public static String preStringFormat(String value) {
+		if (value == null || "".equals(value)) {
+			return " ";
+		} else {
+			return value;
+		}
+	}
 }
