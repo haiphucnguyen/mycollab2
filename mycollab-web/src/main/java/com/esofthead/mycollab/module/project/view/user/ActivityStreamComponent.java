374432848e0d483d39558570ec80a926de801713
Hai Nguyen <hainguyen@esofthead.com>
2013-06-18 23:17:10 +0700

    Update activity stream

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/user/ActivityStreamComponent.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/user/ActivityStreamComponent.java
@@ -4,7 +4,11 @@
  */
 package com.esofthead.mycollab.module.project.view.user;
 
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
+
+import org.apache.commons.lang3.time.DateUtils;
 
 import com.esofthead.mycollab.common.ActivityStreamConstants;
 import com.esofthead.mycollab.common.ModuleNameConstants;
@@ -12,7 +16,6 @@ import com.esofthead.mycollab.common.domain.criteria.ActivityStreamSearchCriteri
 import com.esofthead.mycollab.core.MyCollabException;
 import com.esofthead.mycollab.core.arguments.SearchField;
 import com.esofthead.mycollab.core.arguments.SetSearchField;
-import com.esofthead.mycollab.core.utils.DateTimeUtils;
 import com.esofthead.mycollab.module.project.ProjectResources;
 import com.esofthead.mycollab.module.project.domain.ProjectActivityStream;
 import com.esofthead.mycollab.module.project.localization.ProjectCommonI18nEnum;
@@ -20,15 +23,12 @@ import com.esofthead.mycollab.module.project.localization.ProjectLocalizationTyp
 import com.esofthead.mycollab.module.project.service.ProjectService;
 import com.esofthead.mycollab.module.project.view.ProjectLinkBuilder;
 import com.esofthead.mycollab.vaadin.ui.AbstractBeanPagedList;
-import com.esofthead.mycollab.vaadin.ui.DefaultBeanPagedList;
 import com.esofthead.mycollab.vaadin.ui.Depot;
 import com.esofthead.mycollab.vaadin.ui.ResourceResolver;
 import com.esofthead.mycollab.vaadin.ui.UserAvatarControlFactory;
 import com.esofthead.mycollab.web.AppContext;
 import com.esofthead.mycollab.web.LocalizationHelper;
 import com.vaadin.lazyloadwrapper.LazyLoadWrapper;
-import com.vaadin.ui.Component;
-import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 
@@ -37,98 +37,6 @@ import com.vaadin.ui.VerticalLayout;
  * @author haiphucnguyen
  */
 public class ActivityStreamComponent extends Depot {
-	public static class ActivityStreamRowDisplayHandler implements
-			DefaultBeanPagedList.RowDisplayHandler<ProjectActivityStream> {
-
-		@Override
-		public Component generateRow(
-				final ProjectActivityStream activityStream, final int rowIndex) {
-			final CssLayout layout = new CssLayout();
-			layout.setWidth("100%");
-			layout.setStyleName("activity-stream");
-
-			if ((rowIndex + 1) % 2 != 0) {
-				layout.addStyleName("odd");
-			}
-
-			final CssLayout header = new CssLayout();
-			header.setStyleName("stream-content");
-
-			String content = "";
-			if (ActivityStreamConstants.ACTION_CREATE.equals(activityStream
-					.getAction())) {
-				content = LocalizationHelper
-						.getMessage(
-								ProjectCommonI18nEnum.FEED_PROJECT_USER_ACTIVITY_CREATE_ACTION_TITLE,
-								UserAvatarControlFactory.getLink(
-										activityStream.getCreateduser(), 16),
-								ProjectLinkBuilder.WebLinkGenerator.generateProjectMemberFullLink(
-										activityStream.getExtratypeid(),
-										activityStream.getCreateduser()),
-								activityStream.getCreatedUserFullName(),
-								LocalizationHelper
-										.getMessage(ProjectLocalizationTypeMap
-												.getType(activityStream
-														.getType())),
-								ProjectResources.getResourceLink(activityStream
-										.getType()),
-								ProjectLinkBuilder.WebLinkGenerator.generateProjectItemLink(
-										activityStream.getExtratypeid(),
-										activityStream.getType(),
-										activityStream.getTypeid()),
-								activityStream.getNamefield(),
-								ResourceResolver
-										.getResourceLink("icons/16/project/project.png"),
-								ProjectLinkBuilder.WebLinkGenerator.generateProjectFullLink(
-										activityStream.getProjectId(),
-										ProjectLinkBuilder.DEFAULT_PREFIX_PARAM),
-								activityStream.getProjectName());
-			} else if (ActivityStreamConstants.ACTION_UPDATE
-					.equals(activityStream.getAction())) {
-				content = LocalizationHelper
-						.getMessage(
-								ProjectCommonI18nEnum.FEED_PROJECT_USER_ACTIVITY_UPDATE_ACTION_TITLE,
-								UserAvatarControlFactory.getLink(
-										activityStream.getCreateduser(), 16),
-								ProjectLinkBuilder.WebLinkGenerator.generateProjectMemberFullLink(
-										activityStream.getExtratypeid(),
-										activityStream.getCreateduser()),
-								activityStream.getCreatedUserFullName(),
-								LocalizationHelper
-										.getMessage(ProjectLocalizationTypeMap
-												.getType(activityStream
-														.getType())),
-								ProjectResources.getResourceLink(activityStream
-										.getType()),
-								ProjectLinkBuilder.WebLinkGenerator.generateProjectItemLink(
-										activityStream.getExtratypeid(),
-										activityStream.getType(),
-										activityStream.getTypeid()),
-								activityStream.getNamefield(),
-								ResourceResolver
-										.getResourceLink("icons/16/project/project.png"),
-								ProjectLinkBuilder.WebLinkGenerator.generateProjectFullLink(
-										activityStream.getProjectId(),
-										ProjectLinkBuilder.DEFAULT_PREFIX_PARAM),
-								activityStream.getProjectName());
-			}
-
-			final Label activityLink = new Label(content, Label.CONTENT_XHTML);
-			header.addComponent(activityLink);
-
-			layout.addComponent(header);
-
-			final CssLayout body = new CssLayout();
-			body.setStyleName("activity-date");
-			final Label dateLbl = new Label(
-					DateTimeUtils.getStringDateFromNow(activityStream
-							.getCreatedtime()));
-			body.addComponent(dateLbl);
-
-			layout.addComponent(body);
-			return layout;
-		}
-	}
 
 	static class ProjectActivityStreamPagedList
 			extends
@@ -137,9 +45,7 @@ public class ActivityStreamComponent extends Depot {
 		private final ProjectService projectService;
 
 		public ProjectActivityStreamPagedList() {
-			super(
-					ActivityStreamComponent.ActivityStreamRowDisplayHandler.class,
-					10);
+			super(null, 20);
 
 			projectService = AppContext.getSpringBean(ProjectService.class);
 		}
@@ -159,14 +65,85 @@ public class ActivityStreamComponent extends Depot {
 			final List<ProjectActivityStream> currentListData = projectService
 					.getProjectActivityStreams(searchRequest);
 			listContainer.removeAllComponents();
-			int i = 0;
+
+			Date currentDate = new GregorianCalendar(2100, 1, 1).getTime();
+
 			try {
-				for (final ProjectActivityStream item : currentListData) {
-					final AbstractBeanPagedList.RowDisplayHandler<ProjectActivityStream> rowHandler = rowDisplayHandler
-							.newInstance();
-					final Component row = rowHandler.generateRow(item, i);
-					listContainer.addComponent(row);
-					i++;
+				for (final ProjectActivityStream activityStream : currentListData) {
+					Date itemCreatedDate = activityStream.getCreatedtime();
+					if (!DateUtils.isSameDay(currentDate, itemCreatedDate)) {
+						listContainer.addComponent(new Label(AppContext
+								.formatDate(itemCreatedDate)));
+						currentDate = itemCreatedDate;
+					}
+
+					String content = "";
+
+					if (ActivityStreamConstants.ACTION_CREATE
+							.equals(activityStream.getAction())) {
+						content = LocalizationHelper
+								.getMessage(
+										ProjectCommonI18nEnum.FEED_PROJECT_USER_ACTIVITY_CREATE_ACTION_TITLE,
+										UserAvatarControlFactory.getLink(
+												activityStream.getCreateduser(),
+												16),
+										ProjectLinkBuilder.WebLinkGenerator.generateProjectMemberFullLink(
+												activityStream.getExtratypeid(),
+												activityStream.getCreateduser()),
+										activityStream.getCreatedUserFullName(),
+										LocalizationHelper
+												.getMessage(ProjectLocalizationTypeMap
+														.getType(activityStream
+																.getType())),
+										ProjectResources
+												.getResourceLink(activityStream
+														.getType()),
+										ProjectLinkBuilder.WebLinkGenerator.generateProjectItemLink(
+												activityStream.getExtratypeid(),
+												activityStream.getType(),
+												activityStream.getTypeid()),
+										activityStream.getNamefield(),
+										ResourceResolver
+												.getResourceLink("icons/16/project/project.png"),
+										ProjectLinkBuilder.WebLinkGenerator.generateProjectFullLink(
+												activityStream.getProjectId(),
+												ProjectLinkBuilder.DEFAULT_PREFIX_PARAM),
+										activityStream.getProjectName());
+					} else if (ActivityStreamConstants.ACTION_UPDATE
+							.equals(activityStream.getAction())) {
+						content = LocalizationHelper
+								.getMessage(
+										ProjectCommonI18nEnum.FEED_PROJECT_USER_ACTIVITY_UPDATE_ACTION_TITLE,
+										UserAvatarControlFactory.getLink(
+												activityStream.getCreateduser(),
+												16),
+										ProjectLinkBuilder.WebLinkGenerator.generateProjectMemberFullLink(
+												activityStream.getExtratypeid(),
+												activityStream.getCreateduser()),
+										activityStream.getCreatedUserFullName(),
+										LocalizationHelper
+												.getMessage(ProjectLocalizationTypeMap
+														.getType(activityStream
+																.getType())),
+										ProjectResources
+												.getResourceLink(activityStream
+														.getType()),
+										ProjectLinkBuilder.WebLinkGenerator.generateProjectItemLink(
+												activityStream.getExtratypeid(),
+												activityStream.getType(),
+												activityStream.getTypeid()),
+										activityStream.getNamefield(),
+										ResourceResolver
+												.getResourceLink("icons/16/project/project.png"),
+										ProjectLinkBuilder.WebLinkGenerator.generateProjectFullLink(
+												activityStream.getProjectId(),
+												ProjectLinkBuilder.DEFAULT_PREFIX_PARAM),
+										activityStream.getProjectName());
+					}
+
+					final Label activityLink = new Label(content,
+							Label.CONTENT_XHTML);
+					listContainer.addComponent(activityLink);
 				}
 			} catch (final Exception e) {
 				throw new MyCollabException(e);
