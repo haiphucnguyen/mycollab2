e2876984f3ab1eb5f30fa76fcbe29819804eef31
Van Truc <trucnguyen@esofthead.com>
2013-07-01 15:59:23 +0700

    Layout page tour

--- mycollab-frontsite/src/main/java/com/esofthead/mycollab/base/MainNavigationMenu.java
+++ mycollab-frontsite/src/main/java/com/esofthead/mycollab/base/MainNavigationMenu.java
@@ -1,14 +1,7 @@
 package com.esofthead.mycollab.base;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.wicket.MarkupContainer;
-import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.link.ExternalLink;
-import org.apache.wicket.markup.html.list.ListItem;
-import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 
 import com.esofthead.mycollab.pages.HomePage;
@@ -23,23 +16,17 @@ public class MainNavigationMenu extends Panel {
 	public MainNavigationMenu(String id) {
 		super(id);
 
-		List<MarkupContainer> menus = createMenu();
-
+		/*List<MarkupContainer> menus = createMenu();
 		ListView<MarkupContainer> listView = new ListView<MarkupContainer>(
 				"mainNav", menus) {
-
 			private static final long serialVersionUID = 1L;
-
 			@Override
 			protected void populateItem(final ListItem<MarkupContainer> listItem) {
 				final MarkupContainer link = listItem.getModelObject();
 				listItem.add(link);
 			}
-			
-			
 		};
-
-		add(listView);
+		add(listView);*/
 
 		BookmarkablePageLink<Void> homePageLink = new BookmarkablePageLink<Void>(
 				"backHome", HomePage.class);
@@ -48,14 +35,20 @@ public class MainNavigationMenu extends Panel {
 		ExternalLink signInLink = new ExternalLink("signIn",
 				"http://app.mycollab.com", "Sign In");
 		add(signInLink);
-		
-		//ExternalLink support = new ExternalLink("support", "mailto:sales@esofthead.com");
-		//add(support);
+
+		BookmarkablePageLink<Void> tour = new BookmarkablePageLink<Void>("tour", TourPage.class);
+		add(tour);
+		ExternalLink support = new ExternalLink("support", "mailto:sales@esofthead.com");
+		add(support);
+		BookmarkablePageLink<Void> pricing = new BookmarkablePageLink<Void>("pricing_signup", PricingPage.class);
+		add(pricing);
+		BookmarkablePageLink<Void> privacy = new BookmarkablePageLink<Void>("privacy", PrivacyPage.class);
+		add(privacy);
 
 		this.setRenderBodyOnly(true);
 	}
 
-	private List<MarkupContainer> createMenu() {
+	/*private List<MarkupContainer> createMenu() {
 		List<MarkupContainer> menus = new ArrayList<MarkupContainer>();
 
 		menus.add(new BookmarkablePageLink<Void>("mainNavItem", TourPage.class)
@@ -69,5 +62,5 @@ public class MainNavigationMenu extends Panel {
 				PrivacyPage.class).add(new Label("itemLabel", "Privacy")));
 
 		return menus;
-	}
+	}*/
 }
---
+++ mycollab-frontsite/src/main/webapp/js/tour.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+	$('.tour-items').hide();
+	$('.content-1').show(0);
+	$('.items-1').addClass('item-active');
+	
+	// Event click menu left
+	$('.tour-menu').click(function(){
+		$('.tour-menu').removeClass('item-active');
+		$(this).addClass('item-active');
+		//TourSlide(this);
+		var className = $(this).attr('class');
+		var classArray = className.split(" ");
+		if(classArray != null) {
+			items = classArray[0].split("-");
+			TourSlide(items[1]);
+		}
+	})	
+});
+
+//Fixed scroll
+$(window).scroll(function(){
+	var $free_trial = $('.free-trial');
+	var $menu_left = $('.tour-mid-left');
+	// Fixed scroll free trial
+	if ($(window).scrollTop() > 80) {
+		$free_trial.css({'position': 'fixed', 'top': '0px'}); 
+	} else {
+		$free_trial.css({'position': 'absolute', 'top': 'auto'});
+	}
+	// Fixed scroll menu left
+	if ($(window).scrollTop() > 220) {
+		$menu_left.css({'position': 'fixed', 'top': '0px'}); 
+	} else {
+		$menu_left.css({'position': 'absolute', 'top': 'auto'});
+	}
+});
+
+// Slide content
+function TourSlide($obj){
+	$('.tour-items').hide("slide", { direction: "left" }, 700);
+	setTimeout(function(){
+		$('.content-' + $obj).show("slide", { direction: "right" }, 800);
+	}, 500);	
+	
+}
