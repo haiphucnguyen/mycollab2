473138145e1ae1e3bcb2dddec546408ae6bced44
Hai Nguyen <hainguyen@esofthead.com>
2013-07-03 12:48:14 +0700

    Update weak map

--- mycollab-core/src/main/java/com/esofthead/mycollab/core/WeakHashmapStack.java
+++ mycollab-core/src/main/java/com/esofthead/mycollab/core/WeakHashmapStack.java
@@ -5,21 +5,19 @@ import java.util.WeakHashMap;
 
 public class WeakHashmapStack<K, V> extends WeakHashMap<K, V> {
 
-	private static int MAX_SIZE = 5;
-
 	@Override
 	public V put(K key, V value) {
-		if (this.size() >= MAX_SIZE) {
-			Iterator<K> iterator = this.keySet().iterator();
-			while (iterator.hasNext()) {
-				K keyVal = iterator.next();
-				V val = this.get(keyVal);
-				if (!NotDisposeClass.class.isAssignableFrom(val.getClass())) {
-					this.remove(keyVal);
-					break;
-				}
-			}
-		}
+		// if (this.size() >= MAX_SIZE) {
+		// Iterator<K> iterator = this.keySet().iterator();
+		// while (iterator.hasNext()) {
+		// K keyVal = iterator.next();
+		// V val = this.get(keyVal);
+		// if (!NotDisposeClass.class.isAssignableFrom(val.getClass())) {
+		// this.remove(keyVal);
+		// break;
+		// }
+		// }
+		// }
 		return super.put(key, value);
 	}
 
