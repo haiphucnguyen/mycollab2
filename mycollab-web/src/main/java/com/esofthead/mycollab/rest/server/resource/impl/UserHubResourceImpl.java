28685fe664a9cb66202accfffe40800dcf4acf0f
Hai Nguyen <hainguyen@esofthead.com>
2013-07-04 13:32:02 +0700

    Update rest api to list subdomain of users

--- mycollab-web/src/main/java/com/esofthead/mycollab/rest/server/resource/impl/UserHubResourceImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/rest/server/resource/impl/UserHubResourceImpl.java
@@ -1,5 +1,7 @@
 package com.esofthead.mycollab.rest.server.resource.impl;
 
+import java.util.List;
+
 import org.restlet.Server;
 import org.restlet.data.Form;
 import org.restlet.data.Protocol;
@@ -10,7 +12,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.esofthead.mycollab.core.MyCollabException;
 import com.esofthead.mycollab.module.billing.service.BillingService;
 import com.esofthead.mycollab.rest.server.resource.UserHubResource;
 
@@ -32,9 +33,9 @@ public class UserHubResourceImpl extends ServerResource implements
 		String email = form.getFirstValue("email");
 		String timezoneId = form.getFirstValue("timezone");
 		int planId = Integer.parseInt(form.getFirstValue("planId"));
-		// billingService.registerAccount(subdomain, planId, username, password,
-		// email, timezoneId);
-		throw new MyCollabException("aaa");
+		billingService.registerAccount(subdomain, planId, username, password,
+				email, timezoneId);
+		return "ok";
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -42,5 +43,10 @@ public class UserHubResourceImpl extends ServerResource implements
 		Server server = new Server(Protocol.HTTP, 8182,
 				UserHubResourceImpl.class);
 		server.start();
+	}
+
+	@Override
+	public List<String> getSubdomainsOfUser(String username) {
+		return billingService.getSubdomainsOfUser(username);
 	}
 }
