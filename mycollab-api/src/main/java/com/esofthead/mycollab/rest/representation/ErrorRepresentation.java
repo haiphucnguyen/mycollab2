b508ee365a71b8c30c8b821c078092c57471167a
Hai Nguyen <hainguyen@esofthead.com>
2013-07-09 16:56:13 +0700

    Update rest issue

---
+++ mycollab-api/src/main/java/com/esofthead/mycollab/rest/representation/ErrorRepresentation.java
@@ -0,0 +1,11 @@
+package com.esofthead.mycollab.rest.representation;
+
+import org.restlet.representation.StringRepresentation;
+
+public class ErrorRepresentation extends StringRepresentation {
+
+	public ErrorRepresentation(char[] chars) {
+		super(chars);
+	}
+
+}
--- mycollab-api/src/main/java/com/esofthead/mycollab/rest/server/resource/UserHubResource.java
+++ mycollab-api/src/main/java/com/esofthead/mycollab/rest/server/resource/UserHubResource.java
@@ -1,6 +1,7 @@
 package com.esofthead.mycollab.rest.server.resource;
 
 import org.restlet.data.Form;
+import org.restlet.representation.Representation;
 import org.restlet.resource.Post;
 
 import com.esofthead.mycollab.rest.server.signup.ExistingEmailRegisterException;
@@ -9,6 +10,6 @@ import com.esofthead.mycollab.rest.server.signup.SubdomainExistedException;
 
 public interface UserHubResource {
 	@Post("form")
-	public String doPost(Form form) throws SubdomainExistedException,
+	public Representation doPost(Form entity) throws SubdomainExistedException,
 			ExistingEmailRegisterException, ExistingUserRegisterException;
 }
