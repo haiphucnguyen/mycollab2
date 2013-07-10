75a8afd371d45da3ca819a1e111490e193ae713f
Van Truc <trucnguyen@esofthead.com>
2013-07-10 14:05:55 +0700

    Fixlayout page home

---
+++ mycollab-frontsite/src/main/webapp/js/home.js
@@ -0,0 +1,10 @@
+$(window).load(function() {
+	$('#slider').nivoSlider({
+        pauseOnHover: false,
+        pauseTime: 3000,
+        onImageLoad: function(img) {
+        },
+        onImageChange: function(img) {
+        }
+      });
+});
