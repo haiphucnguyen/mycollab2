0ebb43b1aaa43a443604fd69b4d6266143885edc
robertduy <cuongnguyen@esofthead.com>
2013-07-12 09:59:01 +0700

    Update CSVimportEnityProcess

--- mycollab-web/src/main/java/com/esofthead/mycollab/iexporter/CSVImportEntityProcess.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/iexporter/CSVImportEntityProcess.java
@@ -3,6 +3,12 @@ package com.esofthead.mycollab.iexporter;
 import java.io.File;
 import java.io.FileReader;
 import java.util.List;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 import au.com.bytecode.opencsv.CSVReader;
 
@@ -13,6 +19,12 @@ import com.esofthead.mycollab.iexporter.CSVObjectEntityConverter.ImportFieldDef;
 import com.esofthead.mycollab.web.AppContext;
 
 public class CSVImportEntityProcess<S extends ICrudService, E> {
+	private Validator validation;
+
+	public CSVImportEntityProcess() {
+		validation = AppContext.getSpringBean(LocalValidatorFactoryBean.class);
+	}
+
 	public void doImport(File file, S service, Class<E> beanCls,
 			List<ImportFieldDef> fieldDef) {
 		try {
@@ -25,12 +37,38 @@ public class CSVImportEntityProcess<S extends ICrudService, E> {
 						beanCls,
 						new CSVItemMapperDef(rowData, fieldDef
 								.toArray(new ImportFieldDef[fieldDef.size()])));
-				service.saveWithSession(bean, AppContext.getUsername());
-				rowData = csvReader.readNext();
+				try {
+					validate(bean);
+					service.saveWithSession(bean, AppContext.getUsername());
+				} catch (IllegalArgumentException e1) {
+					throw new IllegalAccessError(e1.getMessage());
+				} finally {
+					rowData = csvReader.readNext();
+				}
 			}
 			csvReader.close();
 		} catch (Exception e) {
 			throw new MyCollabException(e);
+		}
+	}
+
+	/**
+	 * 
+	 * @param bean
+	 * @throws InValidDataException
+	 */
+	private void validate(E bean) throws IllegalArgumentException {
+		Set<ConstraintViolation<E>> violations = validation.validate(bean);
+		if (violations.size() > 0) {
+			StringBuilder errorMsg = new StringBuilder();
+
+			for (@SuppressWarnings("rawtypes")
+			ConstraintViolation violation : violations) {
+				errorMsg.append(violation.getPropertyPath().toString()).append(
+						": ");
+				errorMsg.append(violation.getMessage()).append(".");
+			}
+			throw new IllegalArgumentException(errorMsg.toString());
 		}
 	}
 }
