a1e3b4737cb21e094e9774ebca6a4dac1cf6df64
Hai Nguyen <hainguyen@esofthead.com>
2013-07-13 21:30:25 +0700

    Update window

--- mycollab-web/src/main/java/com/esofthead/mycollab/shell/view/NoSubDomainExistedWindow.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/shell/view/NoSubDomainExistedWindow.java
@@ -3,6 +3,8 @@ package com.esofthead.mycollab.shell.view;
 import com.esofthead.mycollab.vaadin.ui.ButtonLink;
 import com.esofthead.mycollab.web.CustomLayoutExt;
 import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Label;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
@@ -11,8 +13,6 @@ public class NoSubDomainExistedWindow extends Window {
 	private static final long serialVersionUID = 1L;
 
 	public NoSubDomainExistedWindow(final String domain) {
-		// this.addComponent(new Label("There is no sudomain: " + domain
-		// + ". Do you forgot your domain?"));
 		final NoSubDomainLayout contentLayout = new NoSubDomainLayout(domain);
 		contentLayout.setWidth("700px");
 		this.addComponent(contentLayout);
@@ -32,7 +32,15 @@ public class NoSubDomainExistedWindow extends Window {
 					+ domain + ". Do you forgot your domain?");
 			warningContent.addComponent(warningMsg);
 
-			final ButtonLink backToHome = new ButtonLink("Go back to Home Page");
+			final ButtonLink backToHome = new ButtonLink(
+					"Go back to Home Page", new Button.ClickListener() {
+						private static final long serialVersionUID = 1L;
+
+						@Override
+						public void buttonClick(ClickEvent event) {
+							
+						}
+					});
 			warningContent.addComponent(backToHome);
 			this.addComponent(warningContent, "warning-message");
 		}
