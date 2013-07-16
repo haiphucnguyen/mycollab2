1756ce064b47b9df7634b2b51e7156e679699d74
Hai Nguyen <hainguyen@esofthead.com>
2013-07-16 14:26:39 +0700

    Create new file module

---
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/file/view/FileModule.java
@@ -0,0 +1,14 @@
+package com.esofthead.mycollab.module.file.view;
+
+import com.esofthead.mycollab.vaadin.mvp.AbstractView;
+import com.esofthead.mycollab.vaadin.mvp.IModule;
+import com.esofthead.mycollab.vaadin.ui.ViewComponent;
+
+@ViewComponent
+public class FileModule extends AbstractView implements IModule {
+	private static final long serialVersionUID = 1L;
+
+	public void gotoFileDashboard() {
+		
+	}
+}
