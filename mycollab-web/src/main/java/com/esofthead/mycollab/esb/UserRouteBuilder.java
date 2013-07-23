247c72f23511fc5524212254c8c9f6580693e2f7
Hai Nguyen <hainguyen@esofthead.com>
2013-07-23 21:38:22 +0700

    Update namespace

---
+++ mycollab-web/src/main/java/com/esofthead/mycollab/esb/UserRouteBuilder.java
@@ -0,0 +1,13 @@
+package com.esofthead.mycollab.esb;
+
+import org.apache.camel.spring.SpringRouteBuilder;
+
+public class UserRouteBuilder extends SpringRouteBuilder {
+
+	@Override
+	public void configure() throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
