2dc8bdcf11c9f593b7f88fa3ea5abf3065d7eb96
Nghi Tran <nghitran@esofthead.com>
2013-08-01 17:47:41 +0700

    update MessageBox and Notification ui

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/EmailTokenField.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/EmailTokenField.java
@@ -6,98 +6,103 @@ import java.util.List;
 import org.vaadin.tokenfield.TokenField;
 
 import com.esofthead.mycollab.common.domain.MailRecipientField;
+import com.esofthead.mycollab.common.localization.GenericI18Enum;
 import com.esofthead.mycollab.core.utils.EmailValidator;
 import com.esofthead.mycollab.utils.ParsingUtils;
 import com.esofthead.mycollab.utils.ParsingUtils.InvalidEmailException;
 import com.esofthead.mycollab.web.AppContext;
+import com.esofthead.mycollab.web.LocalizationHelper;
 import com.vaadin.ui.Window;
 
 @SuppressWarnings("serial")
 public class EmailTokenField extends TokenField {
 
-	private final EmailValidator emailValidate = new EmailValidator();
+    private final EmailValidator emailValidate = new EmailValidator();
 
-	private List<MailRecipientField> lstMailToken = new ArrayList<MailRecipientField>();
+    private final List<MailRecipientField> lstMailToken = new ArrayList<MailRecipientField>();
 
-	public EmailTokenField() {
-		super();
+    public EmailTokenField() {
+        super();
 
-		this.setStyleName(TokenField.STYLE_TOKENFIELD);
-		this.setInputWidth("544px");
-		this.setRememberNewTokens(false);
-	}
+        this.setStyleName(TokenField.STYLE_TOKENFIELD);
+        this.setInputWidth("544px");
+        this.setRememberNewTokens(false);
+    }
 
-	@Override
-	protected void onTokenClick(Object tokenId) {
-		super.onTokenClick(tokenId);
-		int index = -1;
-		try {
-			index = getItemIndexInListToEmail(ParsingUtils
-					.getMailRecipient(tokenId.toString()));
-		} catch (InvalidEmailException e) {
-			e.printStackTrace();
-		}
-		if (index > -1) {
-			lstMailToken.remove(index);
-		}
-	}
+    @Override
+    protected void onTokenClick(Object tokenId) {
+        super.onTokenClick(tokenId);
+        int index = -1;
+        try {
+            index = getItemIndexInListToEmail(ParsingUtils
+                    .getMailRecipient(tokenId.toString()));
+        } catch (InvalidEmailException e) {
+            e.printStackTrace();
+        }
+        if (index > -1) {
+            lstMailToken.remove(index);
+        }
+    }
 
-	public void addToken(Object tokenId) {
-		super.addToken(tokenId);
-		try {
-			lstMailToken.add(ParsingUtils.getMailRecipient(tokenId.toString()));
-		} catch (InvalidEmailException e) {
-			e.printStackTrace();
-		}
-	}
+    @Override
+    public void addToken(Object tokenId) {
+        super.addToken(tokenId);
+        try {
+            lstMailToken.add(ParsingUtils.getMailRecipient(tokenId.toString()));
+        } catch (InvalidEmailException e) {
+            e.printStackTrace();
+        }
+    }
 
-	private int getItemIndexInListToEmail(MailRecipientField item) {
-		for (int i = 0; i < lstMailToken.size(); i++) {
-			MailRecipientField recipient = lstMailToken.get(i);
-			if (item.getEmail().equals(recipient.getEmail())
-					&& item.getName().equals(recipient.getName())) {
-				return i;
-			}
-		}
-		return -1;
-	}
+    private int getItemIndexInListToEmail(MailRecipientField item) {
+        for (int i = 0; i < lstMailToken.size(); i++) {
+            MailRecipientField recipient = lstMailToken.get(i);
+            if (item.getEmail().equals(recipient.getEmail())
+                    && item.getName().equals(recipient.getName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	@Override
-	protected void onTokenInput(Object tokenId) {
-		String[] tokens = ((String) tokenId).split(",");
-		for (int i = 0; i < tokens.length; i++) {
-			String token = tokens[i].trim();
-			if (token.length() > 0) {
-				if (emailValidate.validate(token)) {
-					super.onTokenInput(token);
-				} else {
-					AppContext
-							.getApplication()
-							.getMainWindow()
-							.showNotification(
-									"Warming",
-									"The email is not valid, pleas check again!",
-									Window.Notification.TYPE_HUMANIZED_MESSAGE);
-				}
-			}
-		}
-	}
+    @Override
+    protected void onTokenInput(Object tokenId) {
+        String[] tokens = ((String) tokenId).split(",");
+        for (int i = 0; i < tokens.length; i++) {
+            String token = tokens[i].trim();
+            if (token.length() > 0) {
+                if (emailValidate.validate(token)) {
+                    super.onTokenInput(token);
+                } else {
+                    AppContext
+                            .getApplication()
+                            .getMainWindow()
+                            .showNotification(
+                                    LocalizationHelper
+                                            .getMessage(GenericI18Enum.WARNING_WINDOW_TITLE),
+                                    LocalizationHelper
+                                            .getMessage(GenericI18Enum.WARNING_NOT_VALID_EMAIL),
+                                    Window.Notification.TYPE_HUMANIZED_MESSAGE);
+                }
+            }
+        }
+    }
 
-	public void setInputStyle(String styleName) {
-		cb.setStyleName(styleName);
-	}
-	
-	public void removeAllRecipients() {
-		
-		for (MailRecipientField recipient : lstMailToken) {
-			removeToken(recipient.getEmail());
-		}
-		
-		lstMailToken.removeAll(lstMailToken);
-	}
+    public void setInputStyle(String styleName) {
+        cb.setStyleName(styleName);
+    }
 
-	public List<MailRecipientField> getListRecipient() {
-		return lstMailToken;
-	}
+    public void removeAllRecipients() {
+
+        for (MailRecipientField recipient : lstMailToken) {
+            removeToken(recipient.getEmail());
+        }
+
+        lstMailToken.removeAll(lstMailToken);
+    }
+
+    public List<MailRecipientField> getListRecipient() {
+        return lstMailToken;
+    }
 
 }
