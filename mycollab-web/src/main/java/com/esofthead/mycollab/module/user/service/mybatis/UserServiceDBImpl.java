53b3bc9caad95e52c1f845c3dc19bab3faaac57e
Hai Nguyen <hainguyen@esofthead.com>
2013-08-06 11:04:54 +0700

    Integrate mycollab with blazeds

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/user/service/mybatis/UserServiceDBImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/user/service/mybatis/UserServiceDBImpl.java
@@ -30,6 +30,7 @@ import org.apache.ibatis.session.RowBounds;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.flex.remoting.RemotingDestination;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -68,6 +69,7 @@ import com.thoughtworks.xstream.io.xml.StaxDriver;
 
 @Service
 @Transactional
+@RemotingDestination(channels={"mycollab-amf","mycollab-secure-amf"})
 public class UserServiceDBImpl extends
 		DefaultService<String, User, UserSearchCriteria> implements UserService {
 
