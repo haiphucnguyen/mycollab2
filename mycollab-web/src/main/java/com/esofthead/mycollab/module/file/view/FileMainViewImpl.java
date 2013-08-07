967ea85fa52548ef5bc8a998a025a0b17d37a0b7
robertduy <cuongnguyen@esofthead.com>
2013-08-07 09:32:51 +0700

    Update dropbox integration window

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/file/view/FileMainViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/file/view/FileMainViewImpl.java
@@ -7,22 +7,14 @@ import java.math.RoundingMode;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import javax.servlet.http.HttpSession;
 
 import org.vaadin.dialogs.ConfirmDialog;
 import org.vaadin.easyuploads.MultiFileUploadExt;
 import org.vaadin.hene.popupbutton.PopupButton;
 import org.vaadin.peter.buttongroup.ButtonGroup;
 
-import com.dropbox.core.DbxAppInfo;
-import com.dropbox.core.DbxRequestConfig;
-import com.dropbox.core.DbxSessionStore;
-import com.dropbox.core.DbxStandardSessionStore;
-import com.dropbox.core.DbxWebAuth;
 import com.esofthead.mycollab.common.ApplicationProperties;
 import com.esofthead.mycollab.common.localization.GenericI18Enum;
 import com.esofthead.mycollab.core.MyCollabException;
@@ -57,9 +49,7 @@ import com.vaadin.data.Property.ValueChangeListener;
 import com.vaadin.event.ItemClickEvent;
 import com.vaadin.event.LayoutEvents.LayoutClickEvent;
 import com.vaadin.event.LayoutEvents.LayoutClickListener;
-import com.vaadin.terminal.ExternalResource;
 import com.vaadin.terminal.Sizeable;
-import com.vaadin.terminal.gwt.server.WebApplicationContext;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
@@ -171,7 +161,7 @@ public class FileMainViewImpl extends AbstractView implements FileMainView {
 					@Override
 					public void buttonClick(ClickEvent event) {
 						FileMainViewImpl.this.getWindow().addWindow(
-								new IntegrateAnotherStoreWindow());
+								new DropBoxOAuthWindow());
 					}
 				});
 		uploadDropboxBtn.addStyleName("link");
