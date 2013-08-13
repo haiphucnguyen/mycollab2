c7d89405f03953b9510b88941ab5c3d20262706c
robertduy <cuongnguyen@esofthead.com>
2013-08-13 15:49:06 +0700

    Update cache heriachy again

--- mycollab-services/src/main/java/com/esofthead/mycollab/module/mail/service/MailRelayService.java
+++ mycollab-services/src/main/java/com/esofthead/mycollab/module/mail/service/MailRelayService.java
@@ -3,12 +3,13 @@ package com.esofthead.mycollab.module.mail.service;
 import java.util.List;
 
 import com.esofthead.mycollab.common.domain.RelayEmailWithBLOBs;
+import com.esofthead.mycollab.core.persistence.service.IService;
 
-public interface MailRelayService {
+public interface MailRelayService extends IService {
 	void saveRelayEmail(int sAccountId, String[] toNames, String[] toEmails,
 			String subject, String bodyContent);
 
 	List<RelayEmailWithBLOBs> getRelayEmails();
-	
+
 	void cleanEmails();
 }
