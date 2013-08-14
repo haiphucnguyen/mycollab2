6b8769b61cdb510739fb4b7de78e37ef651abe1a
robertduy <cuongnguyen@esofthead.com>
2013-08-14 14:27:46 +0700

    update ProjectMemberInviteNoticationActionImpl

--- mycollab-services/src/main/java/com/esofthead/mycollab/schedule/email/project/ProjectMemberInviteNotificationActionImpl.java
+++ mycollab-services/src/main/java/com/esofthead/mycollab/schedule/email/project/ProjectMemberInviteNotificationActionImpl.java
@@ -1,7 +1,6 @@
 package com.esofthead.mycollab.schedule.email.project;
 
 import java.util.Arrays;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,20 +9,19 @@ import com.esofthead.mycollab.common.UrlEncodeDecoder;
 import com.esofthead.mycollab.common.domain.MailRecipientField;
 import com.esofthead.mycollab.common.domain.SimpleRelayEmailNotification;
 import com.esofthead.mycollab.configuration.SiteConfiguration;
-import com.esofthead.mycollab.core.arguments.SearchRequest;
-import com.esofthead.mycollab.core.arguments.StringSearchField;
 import com.esofthead.mycollab.module.billing.RegisterStatusConstants;
 import com.esofthead.mycollab.module.mail.TemplateGenerator;
 import com.esofthead.mycollab.module.mail.service.ExtMailService;
 import com.esofthead.mycollab.module.project.dao.ProjectMemberMapper;
 import com.esofthead.mycollab.module.project.domain.SimpleProjectMember;
-import com.esofthead.mycollab.module.project.domain.criteria.ProjectMemberSearchCriteria;
 import com.esofthead.mycollab.module.project.service.ProjectMemberService;
 import com.esofthead.mycollab.module.project.service.ProjectService;
 import com.esofthead.mycollab.module.user.service.UserService;
+import com.esofthead.mycollab.schedule.email.DefaultSendingRelayEmailNotificationAction;
 
 @Service
-public class ProjectMemberInviteNotificationActionImpl implements
+public class ProjectMemberInviteNotificationActionImpl extends
+		DefaultSendingRelayEmailNotificationAction implements
 		ProjectMemberInviteNotificationAction {
 
 	@Autowired
@@ -42,65 +40,58 @@ public class ProjectMemberInviteNotificationActionImpl implements
 	private UserService userService;
 
 	@Override
-	public void sendNotificationForCreateAction(
-			SimpleRelayEmailNotification notification) {
-		ProjectMemberSearchCriteria searchCriteria = new ProjectMemberSearchCriteria();
-		searchCriteria.setStatus(new StringSearchField(
-				RegisterStatusConstants.VERIFICATING));
+	protected TemplateGenerator templateGeneratorForCreateAction(
+			SimpleRelayEmailNotification emailNotification) {
+		int project_member_id = emailNotification.getTypeid();
+		SimpleProjectMember member = projectMemberService.findById(
+				project_member_id, emailNotification.getSaccountid());
+		String subdomain = projectService.getSubdomainOfProject(member
+				.getProjectid());
 
-		List<SimpleProjectMember> members = projectMemberService
-				.findPagableListByCriteria(new SearchRequest<ProjectMemberSearchCriteria>(
-						searchCriteria, 0, Integer.MAX_VALUE));
+		TemplateGenerator templateGenerator = new TemplateGenerator(
+				"$inviteUser has invited you to join the team for project \" $member.projectName\"",
+				"templates/email/project/memberInvitation/memberInvitationNotifier.mt");
+		templateGenerator.putVariable("member", member);
+		templateGenerator.putVariable("inviteUser", member.getMemberFullName());
+		templateGenerator.putVariable(
+				"urlAccept",
+				SiteConfiguration.getSiteUrl(subdomain)
+						+ "project/member/invitation/confirm_invite/"
+						+ UrlEncodeDecoder.encode(member.getsAccountId() + "/"
+								+ member.getId()));
+		templateGenerator.putVariable(
+				"urlDeny",
+				SiteConfiguration.getSiteUrl(subdomain)
+						+ "project/member/invitation/deny_invite/"
+						+ UrlEncodeDecoder.encode(member.getsAccountId() + "/"
+								+ member.getId()));
 
-		for (SimpleProjectMember member : members) {
-			String subdomain = projectService.getSubdomainOfProject(member
-					.getProjectid());
+		extMailService.sendHTMLMail("mail@esofthead.com", "No-reply", Arrays
+				.asList(new MailRecipientField(member.getEmail(), member
+						.getMemberFullName())), null, null, templateGenerator
+				.generateSubjectContent(), templateGenerator
+				.generateBodyContent(), null);
 
-			TemplateGenerator templateGenerator = new TemplateGenerator(
-					"$inviteUser has invited you to join the team for project \" $member.projectName\"",
-					"templates/email/project/memberInvitation/memberInvitationNotifier.mt");
-			templateGenerator.putVariable("member", member);
-			templateGenerator.putVariable("inviteUser",
-					member.getMemberFullName());
-			templateGenerator.putVariable(
-					"urlAccept",
-					SiteConfiguration.getSiteUrl(subdomain)
-							+ "project/member/invitation/confirm_invite/"
-							+ UrlEncodeDecoder.encode(member.getsAccountId()
-									+ "/" + member.getId()));
-			templateGenerator.putVariable(
-					"urlDeny",
-					SiteConfiguration.getSiteUrl(subdomain)
-							+ "project/member/invitation/deny_invite/"
-							+ UrlEncodeDecoder.encode(member.getsAccountId()
-									+ "/" + member.getId()));
-
-			extMailService.sendHTMLMail("mail@esofthead.com", "No-reply",
-					Arrays.asList(new MailRecipientField(member.getEmail(),
-							member.getMemberFullName())), null, null,
-					templateGenerator.generateSubjectContent(),
-					templateGenerator.generateBodyContent(), null);
-
-			// Send email and change register status of user to
-			// RegisterStatusConstants.SENT_VERIFICATION_EMAIL
-			member.setStatus(RegisterStatusConstants.SENT_VERIFICATION_EMAIL);
-			projectMemberMapper.updateByPrimaryKeySelective(member);
-		}
-
+		// Send email and change register status of user to
+		// RegisterStatusConstants.SENT_VERIFICATION_EMAIL
+		member.setStatus(RegisterStatusConstants.SENT_VERIFICATION_EMAIL);
+		projectMemberService.updateWithSession(member,
+				emailNotification.getChangeByUserFullName());
+		return templateGenerator;
 	}
 
 	@Override
-	public void sendNotificationForUpdateAction(
-			SimpleRelayEmailNotification notification) {
+	protected TemplateGenerator templateGeneratorForUpdateAction(
+			SimpleRelayEmailNotification emailNotification) {
 		// do nothing
-
+		return null;
 	}
 
 	@Override
-	public void sendNotificationForCommentAction(
-			SimpleRelayEmailNotification notification) {
+	protected TemplateGenerator templateGeneratorForCommentAction(
+			SimpleRelayEmailNotification emailNotification) {
 		// do nothing
-
+		return null;
 	}
 
 }
