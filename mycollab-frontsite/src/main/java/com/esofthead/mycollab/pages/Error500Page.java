acc38c9d17169c8a1fc134fc4363faee278129e7
Hai Nguyen <hainguyen@esofthead.com>
2013-08-27 21:48:02 +0700

    Update error reporting

--- mycollab-frontsite/src/main/java/com/esofthead/mycollab/pages/Error500Page.java
+++ mycollab-frontsite/src/main/java/com/esofthead/mycollab/pages/Error500Page.java
@@ -1,6 +1,7 @@
 package com.esofthead.mycollab.pages;
 
 import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
 
 import com.esofthead.mycollab.ErrorReportingUtils;
 import com.esofthead.mycollab.base.BaseErrorPage;
@@ -8,6 +9,10 @@ import com.esofthead.mycollab.base.BaseErrorPage;
 public class Error500Page extends BaseErrorPage {
 
 	private static final long serialVersionUID = 1L;
+
+	public Error500Page(PageParameters parameters) {
+		super(parameters);
+	}
 
 	public Error500Page(Exception e) {
 		super(e);
