e3f5ac342004e892192f31bdf9cfd9689d9c314f
Hai Nguyen <hainguyen@esofthead.com>
2013-08-29 16:03:56 +0700

    Update registration service

--- mycollab-services/src/main/java/com/esofthead/mycollab/module/billing/service/ibatis/BillingServiceImpl.java
+++ mycollab-services/src/main/java/com/esofthead/mycollab/module/billing/service/ibatis/BillingServiceImpl.java
@@ -27,6 +27,7 @@ import com.esofthead.mycollab.module.user.dao.AccountSettingsMapper;
 import com.esofthead.mycollab.module.user.dao.BillingAccountMapper;
 import com.esofthead.mycollab.module.user.dao.BillingAccountMapperExt;
 import com.esofthead.mycollab.module.user.dao.BillingPlanMapper;
+import com.esofthead.mycollab.module.user.dao.UserAccountMapper;
 import com.esofthead.mycollab.module.user.dao.UserMapper;
 import com.esofthead.mycollab.module.user.domain.AccountSettings;
 import com.esofthead.mycollab.module.user.domain.BillingAccount;
@@ -58,6 +59,9 @@ public class BillingServiceImpl implements BillingService {
 
 	@Autowired
 	private AccountSettingsMapper accountSettingMapper;
+
+	@Autowired
+	private UserAccountMapper userAccountMapper;
 
 	@Autowired
 	private UserMapper userMapper;
@@ -162,6 +166,8 @@ public class BillingServiceImpl implements BillingService {
 		userAccount.setRegistrationsource(RegisterSourceConstants.WEB);
 		userAccount.setRoleid(null);
 		userAccount.setUsername(username);
+
+		userAccountMapper.insert(userAccount);
 
 		// save default roles
 		log.debug("Save default roles for account of subdomain {}", subdomain);
