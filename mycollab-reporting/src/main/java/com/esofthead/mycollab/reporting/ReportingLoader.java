3fc996d881485313c6df8cd46601e129c9dbf3f0
Hai Nguyen <hainguyen@esofthead.com>
2013-08-30 09:56:37 +0700

    Fix 	query member service

--- mycollab-reporting/src/main/java/com/esofthead/mycollab/reporting/ReportingLoader.java
+++ mycollab-reporting/src/main/java/com/esofthead/mycollab/reporting/ReportingLoader.java
@@ -5,18 +5,21 @@ import net.sf.dynamicreports.report.builder.DynamicReports;
 import net.sf.dynamicreports.report.builder.column.Columns;
 import net.sf.dynamicreports.report.builder.component.Components;
 import net.sf.dynamicreports.report.builder.datatype.DataTypes;
+import net.sf.dynamicreports.report.datasource.DRDataSource;
 import net.sf.dynamicreports.report.exception.DRException;
 
 public class ReportingLoader {
 	public static void main(String[] args) throws DRException {
+		DRDataSource ds = new DRDataSource("id", "name");
+		ds.add("1", "Nguyen Hai");
+		ds.add("2", "Duong Linh");
+
 		JasperReportBuilder report = DynamicReports.report();
 		report.columns(
-				Columns.column("Customer Id", "id", DataTypes.integerType()),
-				Columns.column("First Name", "first_name",
-						DataTypes.stringType()),
-				Columns.column("Last Name", "last_name", DataTypes.stringType()),
-				Columns.column("Date", "date", DataTypes.dateType())).title(
-				Components.text("Hello world"));
+				Columns.column("Customer Id", "id", DataTypes.stringType()),
+				Columns.column("Name", "name", DataTypes.stringType()))
+				.title(Components.text("Bug Report")).setDataSource(ds);
+
 		report.show();
 	}
 }
