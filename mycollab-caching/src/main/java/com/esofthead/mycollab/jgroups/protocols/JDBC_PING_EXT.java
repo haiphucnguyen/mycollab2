5015a7cc51dc0e81e465cbec792dbf88c21b5581
Hai Nguyen <hainguyen@esofthead.com>
2013-10-01 14:01:45 +0700

    Update distributed lock

--- mycollab-caching/src/main/java/com/esofthead/mycollab/jgroups/protocols/JDBC_PING_EXT.java
+++ mycollab-caching/src/main/java/com/esofthead/mycollab/jgroups/protocols/JDBC_PING_EXT.java
@@ -1,11 +1,16 @@
 package com.esofthead.mycollab.jgroups.protocols;
 
+import org.jgroups.conf.ClassConfigurator;
 import org.jgroups.protocols.JDBC_PING;
 
 import com.esofthead.mycollab.configuration.DatabaseConfiguration;
 import com.esofthead.mycollab.configuration.SiteConfiguration;
 
 public class JDBC_PING_EXT extends JDBC_PING {
+
+	static {
+		ClassConfigurator.add((short) 2048, JDBC_PING_EXT.class);
+	}
 
 	@Override
 	public void init() throws Exception {
@@ -15,11 +20,11 @@ public class JDBC_PING_EXT extends JDBC_PING {
 		this.connection_url = dbConfiguration.getDbUrl();
 		this.connection_username = dbConfiguration.getUser();
 		this.connection_password = dbConfiguration.getPassword();
-		this.initialize_sql = " CREATE TABLE IF NOT EXISTS JGROUPSPING (own_addr varchar(200) NOT NULL, " +
-				"cluster_name varchar(200) NOT NULL," +
-				"ping_data varbinary(5000) DEFAULT NULL, " +
-				"PRIMARY KEY (own_addr, cluster_name) ) " +
-				"ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+		this.initialize_sql = " CREATE TABLE IF NOT EXISTS JGROUPSPING (own_addr varchar(200) NOT NULL, "
+				+ "cluster_name varchar(200) NOT NULL,"
+				+ "ping_data varbinary(5000) DEFAULT NULL, "
+				+ "PRIMARY KEY (own_addr, cluster_name) ) "
+				+ "ENGINE=InnoDB DEFAULT CHARSET=latin1;";
 		super.init();
 	}
 }
