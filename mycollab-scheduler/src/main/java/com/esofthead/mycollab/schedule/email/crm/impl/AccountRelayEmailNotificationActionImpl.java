012259655b99198c79129e7f39bdb4be35691d33
robertduy <cuongnguyen@esofthead.com>
2013-10-02 19:53:54 +0700

    update email crm

--- mycollab-scheduler/src/main/java/com/esofthead/mycollab/schedule/email/crm/impl/AccountRelayEmailNotificationActionImpl.java
+++ mycollab-scheduler/src/main/java/com/esofthead/mycollab/schedule/email/crm/impl/AccountRelayEmailNotificationActionImpl.java
@@ -1,45 +1,163 @@
 package com.esofthead.mycollab.schedule.email.crm.impl;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.esofthead.mycollab.common.domain.SimpleAuditLog;
 import com.esofthead.mycollab.common.domain.SimpleRelayEmailNotification;
+import com.esofthead.mycollab.common.service.AuditLogService;
+import com.esofthead.mycollab.core.utils.StringUtils;
+import com.esofthead.mycollab.module.crm.domain.CrmNotificationSetting;
+import com.esofthead.mycollab.module.crm.domain.SimpleAccount;
+import com.esofthead.mycollab.module.crm.service.AccountService;
+import com.esofthead.mycollab.module.crm.service.CrmNotificationSettingService;
 import com.esofthead.mycollab.module.mail.TemplateGenerator;
 import com.esofthead.mycollab.module.user.domain.SimpleUser;
 import com.esofthead.mycollab.schedule.email.crm.AccountRelayEmailNotificationAction;
+
+//import com.esofthead.mycollab.module.crm.view.CrmLinkGenerator;
 
 @Component
 public class AccountRelayEmailNotificationActionImpl extends
 		CrmDefaultSendingRelayEmailAction implements
 		AccountRelayEmailNotificationAction {
 
+	@Autowired
+	private AuditLogService auditLogService;
+	@Autowired
+	private AccountService accountService;
+
+	@Autowired
+	private CrmNotificationSettingService notificationService;
+
+	private final AccountFieldNameMapper mapper;
+
+	public AccountRelayEmailNotificationActionImpl() {
+		mapper = new AccountFieldNameMapper();
+	}
+
 	@Override
 	protected TemplateGenerator templateGeneratorForCreateAction(
 			SimpleRelayEmailNotification emailNotification) {
-		// TODO Auto-generated method stub
-		return null;
+		int recordAccountId = emailNotification.getTypeid();
+		SimpleAccount simpleAccount = accountService.findById(recordAccountId,
+				emailNotification.getSaccountid());
+		if (simpleAccount != null) {
+			String subject = StringUtils.subString(
+					simpleAccount.getAccountname(), 150);
+
+			TemplateGenerator templateGenerator = new TemplateGenerator(
+					"Account: \"" + subject + "\" has been created",
+					"templates/email/crm/accountCreatedNotifier.mt");
+
+			templateGenerator.putVariable("simpleAccount", simpleAccount);
+			templateGenerator.putVariable("hyperLinks",
+					constructHyperLinks(simpleAccount));
+			return templateGenerator;
+		} else {
+			return null;
+		}
+	}
+
+	private Map<String, String> constructHyperLinks(SimpleAccount simpleAccount) {
+		Map<String, String> hyperLinks = new HashMap<String, String>();
+		// hyperLinks.put("accountURL", CrmLinkGenerator.generateCrmItemLink(
+		// CrmTypeConstants.ACCOUNT, simpleAccount.getId()));
+
+		return hyperLinks;
 	}
 
 	@Override
 	protected TemplateGenerator templateGeneratorForUpdateAction(
 			SimpleRelayEmailNotification emailNotification) {
-		// TODO Auto-generated method stub
-		return null;
+		SimpleAccount simpleAccount = accountService.findById(
+				emailNotification.getTypeid(),
+				emailNotification.getSaccountid());
+
+		String subject = StringUtils.subString(simpleAccount.getAccountname(),
+				150);
+
+		TemplateGenerator templateGenerator = new TemplateGenerator(
+				"Account: \"" + subject + "...\" has been updated",
+				"templates/email/crm/accountUpdatedNotifier.mt");
+		templateGenerator.putVariable("simpleAccount", simpleAccount);
+		templateGenerator.putVariable("hyperLinks",
+				constructHyperLinks(simpleAccount));
+
+		if (emailNotification.getTypeid() != null) {
+			SimpleAuditLog auditLog = auditLogService.findLatestLog(
+					emailNotification.getTypeid(),
+					emailNotification.getSaccountid());
+			templateGenerator.putVariable("historyLog", auditLog);
+
+			templateGenerator.putVariable("mapper", mapper);
+		}
+		return templateGenerator;
 	}
 
 	@Override
 	protected TemplateGenerator templateGeneratorForCommentAction(
 			SimpleRelayEmailNotification emailNotification) {
-		// TODO Auto-generated method stub
-		return null;
+		int accountRecordId = emailNotification.getTypeid();
+		SimpleAccount simpleAccount = accountService.findById(accountRecordId,
+				emailNotification.getSaccountid());
+
+		TemplateGenerator templateGenerator = new TemplateGenerator("[Account]"
+				+ emailNotification.getChangeByUserFullName()
+				+ " has commented on "
+				+ StringUtils.subString(simpleAccount.getAccountname(), 100)
+				+ "\"", "templates/email/project/bugCommentNotifier.mt");
+		templateGenerator.putVariable("comment", emailNotification);
+		// templateGenerator.putVariable("userComment", linkGenerator
+		// .generateUserPreviewFullLink(emailNotification.getChangeby()));
+		templateGenerator.putVariable("bug", simpleAccount);
+		templateGenerator.putVariable("hyperLinks",
+				constructHyperLinks(simpleAccount));
+
+		return templateGenerator;
 	}
 
 	@Override
 	protected List<SimpleUser> getListNotififyUserWithFilter(
 			SimpleRelayEmailNotification notification) {
-		// TODO Auto-generated method stub
-		return null;
+		List<CrmNotificationSetting> notificationSettings = notificationService
+				.findNotifications(notification.getChangeby(),
+						notification.getSaccountid());
+
+		List<SimpleUser> inListUsers = notification.getNotifyUsers();
+
+		// TODO: MORE CODE FILTER
+		return inListUsers;
+	}
+
+	public class AccountFieldNameMapper {
+		private final Map<String, String> fieldNameMap;
+
+		AccountFieldNameMapper() {
+			fieldNameMap = new HashMap<String, String>();
+
+			fieldNameMap.put("summary", "Bug Summary");
+			fieldNameMap.put("description", "Description");
+			fieldNameMap.put("status", "Status");
+			fieldNameMap.put("assignuser", "Assigned to");
+			fieldNameMap.put("resolution", "Resolution");
+			fieldNameMap.put("severity", "Serverity");
+			fieldNameMap.put("environment", "Environment");
+			fieldNameMap.put("priority", "Priority");
+			fieldNameMap.put("duedate", "Due Date");
+		}
+
+		public boolean hasField(String fieldName) {
+			return fieldNameMap.containsKey(fieldName);
+		}
+
+		public String getFieldLabel(String fieldName) {
+			return fieldNameMap.get(fieldName);
+		}
 	}
 
 }
---
+++ mycollab-scheduler/src/main/resources/templates/email/crm/accountCreatedNotifier.mt
@@ -0,0 +1,93 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>New account created</title>
+</head>
+<body>
+	<table width="650" cellpadding="0" cellspacing="0" border="0" style="margin: 0px auto;">
+		<tr>
+			<td style="background: url('${defaultUrls.cdn_url}border_large_top_new.png') no-repeat 0 0 transparent; font-size: 11px; line-height: 11px;" height="11">&nbsp;</td>
+		</tr>
+		<tr>
+			<td style="background: url('${defaultUrls.cdn_url}border_large_center_orange.png') repeat-y 0 0 transparent; text-align: center; padding-bottom: 10px;"><div style="width: 440px; display: inline-block; vertical-align: middle; text-align: left;"><span style="font: bold 18px Tahoma, Geneva, sans-serif; color: white;">New Account</span></div><div style="width: 150px; display: inline-block; vertical-align: middle;"><img src="${defaultUrls.cdn_url}logo_new.png" alt="esofthead-logo" width="150" height="45" style="margin: 0px; padding: 0px;"></div>
+			</td>
+		</tr>
+		<tr>
+			<td style="background: url('${defaultUrls.cdn_url}border_large_center_new.png') repeat-y 0 0 transparent; color: #4e4e4e; font: 13px 'Lucida Sans Unicode', 'Lucida Grande', sans-serif; padding: 10px 30px 0px;">
+				<div style="font-weight: bold; display: block; border-bottom: 1px solid rgb(212, 212, 212); padding-bottom: 5px; margin-bottom: 10px;">Hi $!userName,</div>
+				<div style="display: block; padding: 8px; background-color: rgb(247, 228, 221);">Just wanna let you know that a new account has been created. Here're details about it:</div>
+				<table width="588" cellpadding="0" cellspacing="0" border="0" style="margin: 0 auto 25px;">
+					<tr>
+						<td style="color: #5a5a5a; font: 10px 'Lucida Sans Unicode', 'Lucida Grande', sans-serif; padding: 3px 10px;">
+							<table cellpadding="0" cellspacing="5" border="0" style="font-size: 10px; width: 100%;">
+								<tr>
+									<td style="width: 60px; max-width: 90px; vertical-align: top; text-align: right;">Account Name:&nbsp;</td>
+									<td style="font-weight: bold; font-size: 11px;" colspan="3"><a href="$!hyperLinks.accountURL" style="color: rgb(216, 121, 55); text-decoration: none;">$!simpleAccount.accountname</a></td>
+								</tr>
+								<tr>
+									<td style="text-align: right;">Website:&nbsp;</td>
+									<td>$!simpleAccount.website</td>
+									<td style="text-align: right; min-width: 90px;">Fax:&nbsp;</td>
+									<td>$!simpleAccount.fax</td>		
+								</tr>
+								<tr>
+									<td style="text-align: right;">Employees:&nbsp;</td>
+									<td></td>
+									<td style="text-align: right;">Other Phone:&nbsp;</td>
+									<td>$!simpleAccount.alternatephone</td>		
+								</tr>
+								<tr>
+									<td style="text-align: right;">Industry:&nbsp;</td>
+									<td>$!simpleAccount.industry</td>
+									<td style="text-align: right;">Email:&nbsp;</td>
+									<td>$!simpleAccount.email</td>		
+								</tr>
+								<tr>
+									<td style="text-align: right;">Type:&nbsp;</td>
+									<td>$!simpleAccount.type</td>
+									<td style="text-align: right;">Ownership:&nbsp;</td>
+									<td>$!simpleAccount.ownership</td>		
+								</tr>
+								<tr>
+									<td style="text-align: right; vertical-align: top;">Due date:&nbsp;</td>
+									<td style="vertical-align: top;">$!date.format('short_date', $!simpleAccount.Duedate)</td>
+									<td style="text-align: right; vertical-align: top;">Milestone:&nbsp;</td>
+									<td><a href="$!hyperLinks.milestoneUrl" style="color: rgb(216, 121, 55); text-decoration: none;">$!simpleAccount.milestoneName</td>		
+								</tr>
+								<tr>
+									<td style="text-align: right; vertical-align: top;">Logged by:&nbsp;</td>
+									<td><a href="${hyperLinks.loggedUserUrl}" style="color: rgb(216, 121, 55); text-decoration: none;">$!simpleAccount.loguserFullName</a></td>
+									<td style="text-align: right; vertical-align: top;">Assigned to:&nbsp;</td>
+									<td>
+										<a href="${hyperLinks.assignUserUrl}" style="color: rgb(216, 121, 55); text-decoration: none;">$!simpleAccount.assignuserFullName</a>
+									</td>				
+								</tr>
+								<tr>
+									<td style="text-align: right; min-width: 90px; vertical-align: top;">Description:&nbsp;</td>
+									<td colspan="3" style="word-wrap: break-word; white-space: normal; word-break: break-all;">$!simpleAccount.description</td>
+								</tr>
+							</table>
+						</td>
+					</tr>
+				</table>
+			</td>
+		</tr>
+		<tr>
+			<td style="background: url('${defaultUrls.cdn_url}border_large_center_footer.png') repeat-y 0 0 transparent; border-top: 1px solid rgb(212, 212, 212);">
+				<div style="margin-top: 10px; padding-left: 30px; color: #4e4e4e; font: 10px 'Lucida Sans Unicode', 'Lucida Grande', sans-serif; display: inline-block; width: 295px;">Copyright by <a href="http://www.esofthead.com" style="color: rgb(216, 121, 55); text-decoration: none;">eSoftHead</a><br>&copy; 2013 MyCollab, LLC. All rights reserved.</div>
+				<div style="text-align: right; font-size: 10px; display: inline-block; width: 295px;">
+					<span style="display: inline-block; vertical-align: top; margin-top: 10px;">Connect with us:&nbsp;</span>
+					<a href="${defaultUrls.facebook_url}"><img src="${defaultUrls.cdn_url}fb_social_icon.png" height="25" width="25"></a>
+					<a href="${defaultUrls.google_url}"><img src="${defaultUrls.cdn_url}google_social_icon.png" height="25" width="25"></a>
+					<a href="${defaultUrls.linkedin_url}"><img src="${defaultUrls.cdn_url}linkedin_social_icon.png" height="25" width="25"></a>
+					<a href="${defaultUrls.twitter_url}"><img src="${defaultUrls.cdn_url}twitter_social_icon.png" height="25" width="25"></a>
+				</div>
+			</td>
+		</tr>
+		<tr>
+			<td style="background: url('${defaultUrls.cdn_url}border_large_bottom_new.png') no-repeat 0 0 transparent; line-height: 7px; font-size: 7px;" height="7">&nbsp;</td>
+		</tr>
+	</table>
+</body>
+</html>
