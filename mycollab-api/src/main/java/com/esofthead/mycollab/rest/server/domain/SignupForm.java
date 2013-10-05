6939ec2fe146b25998ecde77f46bc1ba0eddf310
Hai Nguyen <hainguyen@esofthead.com>
2013-10-05 11:14:45 +0700

    Update signup form

--- mycollab-api/src/main/java/com/esofthead/mycollab/rest/server/domain/SignupForm.java
+++ mycollab-api/src/main/java/com/esofthead/mycollab/rest/server/domain/SignupForm.java
@@ -1,15 +1,22 @@
 package com.esofthead.mycollab.rest.server.domain;
 
+import javax.ws.rs.FormParam;
+
 public class SignupForm {
 
+	@FormParam("subdomain")
 	private String subdomain;
 
+	@FormParam("planId")
 	private Integer planId;
 
+	@FormParam("password")
 	private String password;
 
+	@FormParam("email")
 	private String email;
 
+	@FormParam("timezoneId")
 	private String timezoneId;
 
 	public String getSubdomain() {
--- mycollab-api/src/main/java/com/esofthead/mycollab/rest/server/resource/AccountResource.java
+++ mycollab-api/src/main/java/com/esofthead/mycollab/rest/server/resource/AccountResource.java
@@ -1,12 +1,20 @@
 package com.esofthead.mycollab.rest.server.resource;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.jboss.resteasy.annotations.Form;
 
 import com.esofthead.mycollab.rest.server.domain.SignupForm;
 
+@Path("/account")
 public interface AccountResource {
 	@POST
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/signup")
 	String signup(@Form SignupForm entity);
 }
