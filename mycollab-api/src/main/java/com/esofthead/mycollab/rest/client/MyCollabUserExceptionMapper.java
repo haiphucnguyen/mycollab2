ecbaa231976591f1b4d4cd2a1bab453d2ed3f249
Hai Nguyen <hainguyen@esofthead.com>
2013-10-07 11:37:49 +0700

    Register error handling for resteasy

--- mycollab-api/src/main/java/com/esofthead/mycollab/rest/client/MyCollabUserExceptionMapper.java
+++ mycollab-api/src/main/java/com/esofthead/mycollab/rest/client/MyCollabUserExceptionMapper.java
@@ -2,15 +2,20 @@ package com.esofthead.mycollab.rest.client;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.springframework.stereotype.Component;
 
 import com.esofthead.mycollab.core.UserInvalidInputException;
 
+@Provider
+@Component
 public class MyCollabUserExceptionMapper implements
 		ExceptionMapper<UserInvalidInputException> {
 
 	@Override
 	public Response toResponse(UserInvalidInputException exception) {
-		return Response.status(400).build();
+		return Response.status(400).entity(exception.getMessage()).build();
 	}
 
 }
