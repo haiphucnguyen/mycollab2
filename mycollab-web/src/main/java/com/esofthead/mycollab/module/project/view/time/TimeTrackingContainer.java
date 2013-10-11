4f4d87ab94322fa636c55fe5961e0b658a067cdd
robertduy <cuongnguyen@esofthead.com>
2013-10-11 16:04:20 +0700

    timeTrackingContainer

---
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/time/TimeTrackingContainer.java
@@ -0,0 +1,10 @@
+package com.esofthead.mycollab.module.project.view.time;
+
+import com.esofthead.mycollab.vaadin.mvp.AbstractView;
+import com.esofthead.mycollab.vaadin.ui.ViewComponent;
+
+@ViewComponent
+public class TimeTrackingContainer extends AbstractView {
+	private static final long serialVersionUID = 1L;
+
+}
