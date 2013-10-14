cf0f33a718e9d3831840928213a9c27de266c8fb
Hai Nguyen <hainguyen@esofthead.com>
2013-10-14 11:25:53 +0700

    Update maven configuration

--- mycollab-web/src/main/java/com/esofthead/mycollab/jetty/ServerRunner.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/jetty/ServerRunner.java
@@ -8,8 +8,6 @@ import javax.servlet.SessionTrackingMode;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
 
-import com.esofthead.mycollab.vaadin.mvp.ViewManager;
-
 public class ServerRunner {
 	public static void main(String[] args) throws Exception {
 		Server server = new Server(8080);
