1e4e0aa6115a40513600fe8b167f43d1d991e2bb
Hai Nguyen <hainguyen@esofthead.com>
2013-10-17 15:38:22 +0700

    Update flex service generator

---
+++ mycollab-web/src/main/java/com/esofthead/mycollab/form/view/FormBuilder.java
@@ -0,0 +1,14 @@
+package com.esofthead.mycollab.form.view;
+
+import com.esofthead.mycollab.form.view.type.DynaForm;
+
+public class FormBuilder {
+
+	public void sections() {
+
+	}
+
+	public DynaForm build() {
+		return null;
+	}
+}
---
+++ mycollab-web/src/main/java/com/esofthead/mycollab/form/view/type/DynaField.java
@@ -0,0 +1,14 @@
+package com.esofthead.mycollab.form.view.type;
+
+import com.esofthead.mycollab.form.domain.CustomFieldFormat;
+
+public class DynaField {
+	private int fieldIndex;
+
+	private CustomFieldFormat fieldFormat;
+
+	public int getFieldIndex() {
+		return fieldIndex;
+	}
+
+}
