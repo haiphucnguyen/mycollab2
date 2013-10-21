7be0f55344a3b541b85d1cd40f831d5e2a416615
Hai Nguyen <hainguyen@esofthead.com>
2013-10-21 14:55:52 +0700

    Update custom field impl

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/view/setting/CrmCustomView.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/view/setting/CrmCustomView.java
@@ -3,5 +3,5 @@ package com.esofthead.mycollab.module.crm.view.setting;
 import com.esofthead.mycollab.vaadin.mvp.View;
 
 public interface CrmCustomView extends View {
-
+	void display();
 }
--- mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/view/setting/CrmCustomViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/view/setting/CrmCustomViewImpl.java
@@ -1,9 +1,77 @@
 package com.esofthead.mycollab.module.crm.view.setting;
 
+import com.esofthead.mycollab.module.crm.CrmTypeConstants;
 import com.esofthead.mycollab.vaadin.mvp.AbstractView;
+import com.esofthead.mycollab.vaadin.ui.ValueComboBox;
 import com.esofthead.mycollab.vaadin.ui.ViewComponent;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 
 @ViewComponent
 public class CrmCustomViewImpl extends AbstractView implements CrmCustomView {
+	private static final long serialVersionUID = 1L;
+
+	private Label headerLbl;
+	private ModuleSelectionComboBox moduleComboBox;
+
+	public CrmCustomViewImpl() {
+		headerLbl = new Label();
+		this.addComponent(headerLbl);
+
+		Label descLbl = new Label(
+				"Customize the page layout by changing the order of the columns and fields, marking fields as mandatory, adding or removing the fields and sections. You can drag and drop the section header to reorder the sections. You need to drag and drop the fields to move them to the List of Removed Fields");
+		this.addComponent(descLbl);
+
+		HorizontalLayout controlLayout = new HorizontalLayout();
+		moduleComboBox = new ModuleSelectionComboBox();
+
+		controlLayout.addComponent(new Label("Module"));
+		controlLayout.addComponent(moduleComboBox);
+
+		Button createCustomFieldBtn = new Button("New Custom Field",
+				new Button.ClickListener() {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public void buttonClick(ClickEvent event) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+		controlLayout.addComponent(createCustomFieldBtn);
+
+		Button createSectionBtn = new Button("Create Section",
+				new Button.ClickListener() {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public void buttonClick(ClickEvent event) {
+						// TODO Auto-generated method stub
+
+					}
+				});
+		controlLayout.addComponent(createSectionBtn);
+		this.addComponent(controlLayout);
+	}
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private class ModuleSelectionComboBox extends ValueComboBox {
+		private static final long serialVersionUID = 1L;
+
+		public ModuleSelectionComboBox() {
+			super(false, CrmTypeConstants.ACCOUNT, CrmTypeConstants.CONTACT,
+					CrmTypeConstants.CAMPAIGN, CrmTypeConstants.LEAD,
+					CrmTypeConstants.OPPORTUNITY, CrmTypeConstants.CASE,
+					CrmTypeConstants.TASK, CrmTypeConstants.CALL,
+					CrmTypeConstants.MEETING);
+		}
+	}
 
 }
