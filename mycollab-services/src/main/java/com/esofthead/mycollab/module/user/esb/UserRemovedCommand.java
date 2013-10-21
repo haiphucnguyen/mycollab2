04c993cc317eacfff34abe51fc1d5dc205c6f4a8
Hai Nguyen <hainguyen@esofthead.com>
2013-10-21 10:53:09 +0700

    Fix issue #189

--- mycollab-services/src/main/java/com/esofthead/mycollab/module/user/esb/UserRemovedCommand.java
+++ mycollab-services/src/main/java/com/esofthead/mycollab/module/user/esb/UserRemovedCommand.java
@@ -1,5 +1,5 @@
 package com.esofthead.mycollab.module.user.esb;
 
 public interface UserRemovedCommand {
-	void userRemoved(String username, int accountid);
+	void userRemoved(String username, Integer accountid);
 }
