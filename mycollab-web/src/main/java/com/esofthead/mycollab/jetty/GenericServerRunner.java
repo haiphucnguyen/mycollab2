1086d8da9850c96876110c1e33178827be36ebbc
Hai Nguyen <hainguyen@esofthead.com>
2013-11-05 14:40:09 +0700

    Update scripts

--- mycollab-web/src/main/java/com/esofthead/mycollab/jetty/GenericServerRunner.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/jetty/GenericServerRunner.java
@@ -1,14 +1,24 @@
 package com.esofthead.mycollab.jetty;
 
+import java.io.File;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.webapp.WebAppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.esofthead.mycollab.core.MyCollabException;
 
 public abstract class GenericServerRunner {
+	private static Logger log = LoggerFactory
+			.getLogger(GenericServerRunner.class);
+
 	private Server server;
 
 	public GenericServerRunner() {
 		server = new Server(8080);
-		String webappDirLocation = "src/main/webapp/";
+		log.debug("Detect root folder webapp");
+		String webappDirLocation = detectBasedir();
 		server.setHandler(buildContext(webappDirLocation));
 	}
 
@@ -21,5 +31,36 @@ public abstract class GenericServerRunner {
 
 	public void stop() throws Exception {
 		server.stop();
+	}
+
+	private String detectBasedir() {
+		File searchFile = searchFile(new File(System.getProperty("user.dir")),
+				"webapp");
+		if (searchFile == null) {
+			throw new MyCollabException("Can not detect webapp base folder");
+		} else {
+			log.debug("Webapp base dir is {}", searchFile.getAbsolutePath());
+			return searchFile.getAbsolutePath();
+		}
+	}
+
+	private File searchFile(File homeDir, String foldername) {
+		File[] listFiles = homeDir.listFiles();
+		for (File file : listFiles) {
+			if (file.isDirectory()) {
+				if (file.getName().equals(foldername)) {
+					return file;
+				} else {
+					File resultFile = searchFile(file, foldername);
+					if (resultFile != null) {
+						return resultFile;
+					}
+				}
+			} else {
+				continue;
+			}
+		}
+
+		return null;
 	}
 }
