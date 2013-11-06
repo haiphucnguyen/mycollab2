da0ad3bb23baaa6564f1f7b00d2cd4f39e6c3869
Hai Nguyen <hainguyen@esofthead.com>
2013-11-06 07:15:23 +0700

    More flexibility in loading resources

---
+++ mycollab-services/src/main/java/com/esofthead/mycollab/spring/AppServiceLoader.java
@@ -0,0 +1,20 @@
+package com.esofthead.mycollab.spring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+@Configuration
+public class AppServiceLoader {
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer properties() {
+		PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+		Resource[] resources = new ClassPathResource[] { new ClassPathResource(
+				"resources.properties") };
+		pspc.setLocations(resources);
+		pspc.setIgnoreUnresolvablePlaceholders(true);
+		return pspc;
+	}
+}
