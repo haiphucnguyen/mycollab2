d0beefe4b49d262807922b4d3c77c682edbc5b31
Nguyen Phuc Hai <hainguyen@esofthead.com>
2013-11-20 14:34:04 +0700

    Fix start windows script

--- mycollab-app-community/src/main/scripts/mycollab.bat
+++ mycollab-app-community/src/main/scripts/mycollab.bat
@@ -21,7 +21,8 @@ rem   JAVA_HOME       Must point at your Java Development Kit installation.
 rem                   Required to run the with the "debug" argument.
 rem ---------------------------------------------------------------------------
 
-set MYCOLLAB_OPTS="-Xms1536m -Xmx1536m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=256m -XX:+DisableExplicitGC"
+set MYCOLLAB_OPTS=-Xms1024m -Xmx1024m -XX:NewSize=256m -XX:MaxNewSize=256m -XX:PermSize=256m -XX:MaxPermSize=256m -XX:+DisableExplicitGC
+set _RUNJAVA=java
 
 rem Suppress Terminate batch job on CTRL+C
 if not ""%1"" == ""run"" goto mainEntry
@@ -42,152 +43,33 @@ rem Guess MYCOLLAB_HOME if not defined
 set "CURRENT_DIR=%cd%"
 if not "%MYCOLLAB_HOME%" == "" goto gotHome
 set "MYCOLLAB_HOME=%CURRENT_DIR%"
-if exist "%MYCOLLAB_HOME%\bin\catalina.bat" goto okHome
+if exist "%MYCOLLAB_HOME%\mycollab.bat" goto okHome
 cd ..
 set "MYCOLLAB_HOME=%cd%"
 cd "%CURRENT_DIR%"
 :gotHome
 
-if exist "%MYCOLLAB_HOME%\bin\catalina.bat" goto okHome
+if exist "%MYCOLLAB_HOME%\mycollab.bat" goto okHome
 echo The MYCOLLAB_HOME environment variable is not defined correctly
 echo This environment variable is needed to run this program
 goto end
 :okHome
 
-rem Copy CATALINA_BASE from MYCOLLAB_HOME if not defined
-if not "%CATALINA_BASE%" == "" goto gotBase
-set "CATALINA_BASE=%MYCOLLAB_HOME%"
-:gotBase
-
-rem Ensure that any user defined CLASSPATH variables are not used on startup,
-rem but allow them to be specified in setenv.bat, in rare case when it is needed.
-set CLASSPATH=
-
-rem Get standard environment variables
-if not exist "%CATALINA_BASE%\bin\setenv.bat" goto checkSetenvHome
-call "%CATALINA_BASE%\bin\setenv.bat"
-goto setenvDone
-:checkSetenvHome
-if exist "%MYCOLLAB_HOME%\bin\setenv.bat" call "%MYCOLLAB_HOME%\bin\setenv.bat"
-:setenvDone
-
-rem Get standard Java environment variables
-if exist "%MYCOLLAB_HOME%\bin\setclasspath.bat" goto okSetclasspath
-echo Cannot find "%MYCOLLAB_HOME%\bin\setclasspath.bat"
-echo This file is needed to run this program
-goto end
-:okSetclasspath
-call "%MYCOLLAB_HOME%\bin\setclasspath.bat" %1
-if errorlevel 1 goto end
-
-rem Add on extra jar file to CLASSPATH
-rem Note that there are no quotes as we do not want to introduce random
-rem quotes into the CLASSPATH
-if "%CLASSPATH%" == "" goto emptyClasspath
-set "CLASSPATH=%CLASSPATH%;"
-:emptyClasspath
-set "CLASSPATH=%CLASSPATH%%MYCOLLAB_HOME%\bin\bootstrap.jar"
-
-if not "%CATALINA_TMPDIR%" == "" goto gotTmpdir
-set "CATALINA_TMPDIR=%CATALINA_BASE%\temp"
-:gotTmpdir
-
-rem Add tomcat-juli.jar to classpath
-rem tomcat-juli.jar can be over-ridden per instance
-if not exist "%CATALINA_BASE%\bin\tomcat-juli.jar" goto juliClasspathHome
-set "CLASSPATH=%CLASSPATH%;%CATALINA_BASE%\bin\tomcat-juli.jar"
-goto juliClasspathDone
-:juliClasspathHome
-set "CLASSPATH=%CLASSPATH%;%MYCOLLAB_HOME%\bin\tomcat-juli.jar"
-:juliClasspathDone
-
-if not "%LOGGING_CONFIG%" == "" goto noJuliConfig
-set LOGGING_CONFIG=-Dnop
-if not exist "%CATALINA_BASE%\conf\logging.properties" goto noJuliConfig
-set LOGGING_CONFIG=-Djava.util.logging.config.file="%CATALINA_BASE%\conf\logging.properties"
-:noJuliConfig
-set JAVA_OPTS=%JAVA_OPTS% %LOGGING_CONFIG%
-
-if not "%LOGGING_MANAGER%" == "" goto noJuliManager
-set LOGGING_MANAGER=-Djava.util.logging.manager=org.apache.juli.ClassLoaderLogManager
-:noJuliManager
-set JAVA_OPTS=%JAVA_OPTS% %LOGGING_MANAGER%
-
 rem ----- Execute The Requested Command ---------------------------------------
 
-echo Using CATALINA_BASE:   "%CATALINA_BASE%"
 echo Using MYCOLLAB_HOME:   "%MYCOLLAB_HOME%"
-echo Using CATALINA_TMPDIR: "%CATALINA_TMPDIR%"
-if ""%1"" == ""debug"" goto use_jdk
-echo Using JRE_HOME:        "%JRE_HOME%"
-goto java_dir_displayed
-:use_jdk
-echo Using JAVA_HOME:       "%JAVA_HOME%"
-:java_dir_displayed
-echo Using CLASSPATH:       "%CLASSPATH%"
 
 set _EXECJAVA=%_RUNJAVA%
-set MAINCLASS=org.apache.catalina.startup.Bootstrap
-set ACTION=start
-set SECURITY_POLICY_FILE=
-set DEBUG_OPTS=
-set JPDA=
 
-if not ""%1"" == ""jpda"" goto noJpda
-set JPDA=jpda
-if not "%JPDA_TRANSPORT%" == "" goto gotJpdaTransport
-set JPDA_TRANSPORT=dt_socket
-:gotJpdaTransport
-if not "%JPDA_ADDRESS%" == "" goto gotJpdaAddress
-set JPDA_ADDRESS=8000
-:gotJpdaAddress
-if not "%JPDA_SUSPEND%" == "" goto gotJpdaSuspend
-set JPDA_SUSPEND=n
-:gotJpdaSuspend
-if not "%JPDA_OPTS%" == "" goto gotJpdaOpts
-set JPDA_OPTS=-agentlib:jdwp=transport=%JPDA_TRANSPORT%,address=%JPDA_ADDRESS%,server=y,suspend=%JPDA_SUSPEND%
-:gotJpdaOpts
-shift
-:noJpda
 
-if ""%1"" == ""debug"" goto doDebug
-if ""%1"" == ""run"" goto doRun
 if ""%1"" == ""start"" goto doStart
 if ""%1"" == ""stop"" goto doStop
-if ""%1"" == ""configtest"" goto doConfigTest
-if ""%1"" == ""version"" goto doVersion
 
-echo Usage:  catalina ( commands ... )
+echo Usage:  mycollab ( commands ... )
 echo commands:
-echo   debug             Start Catalina in a debugger
-echo   debug -security   Debug Catalina with a security manager
-echo   jpda start        Start Catalina under JPDA debugger
-echo   run               Start Catalina in the current window
-echo   run -security     Start in the current window with security manager
-echo   start             Start Catalina in a separate window
-echo   start -security   Start in a separate window with security manager
-echo   stop              Stop Catalina
-echo   configtest        Run a basic syntax check on server.xml
-echo   version           What version of tomcat are you running?
+echo   start             Start MyCollab in a separate window
+echo   stop              Stop MyCollab
 goto end
-
-:doDebug
-shift
-set _EXECJAVA=%_RUNJDB%
-set DEBUG_OPTS=-sourcepath "%MYCOLLAB_HOME%\..\..\java"
-if not ""%1"" == ""-security"" goto execCmd
-shift
-echo Using Security Manager
-set "SECURITY_POLICY_FILE=%CATALINA_BASE%\conf\catalina.policy"
-goto execCmd
-
-:doRun
-shift
-if not ""%1"" == ""-security"" goto execCmd
-shift
-echo Using Security Manager
-set "SECURITY_POLICY_FILE=%CATALINA_BASE%\conf\catalina.policy"
-goto execCmd
 
 :doStart
 shift
@@ -198,27 +80,13 @@ goto gotTitle
 :noTitle
 set _EXECJAVA=start %_RUNJAVA%
 :gotTitle
-if not ""%1"" == ""-security"" goto execCmd
 shift
-echo Using Security Manager
-set "SECURITY_POLICY_FILE=%CATALINA_BASE%\conf\catalina.policy"
 goto execCmd
 
 :doStop
 shift
 set ACTION=stop
-set CATALINA_OPTS=
 goto execCmd
-
-:doConfigTest
-shift
-set ACTION=configtest
-set CATALINA_OPTS=
-goto execCmd
-
-:doVersion
-%_EXECJAVA% -classpath "%MYCOLLAB_HOME%\lib\catalina.jar" org.apache.catalina.util.ServerInfo
-goto end
 
 
 :execCmd
@@ -232,19 +100,7 @@ goto setArgs
 :doneSetArgs
 
 rem Execute Java with the applicable properties
-if not "%JPDA%" == "" goto doJpda
-if not "%SECURITY_POLICY_FILE%" == "" goto doSecurity
-%_EXECJAVA% %JAVA_OPTS% %CATALINA_OPTS% %DEBUG_OPTS% -Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%" -classpath "%CLASSPATH%" -Dcatalina.base="%CATALINA_BASE%" -Dcatalina.home="%MYCOLLAB_HOME%" -Djava.io.tmpdir="%CATALINA_TMPDIR%" %MAINCLASS% %CMD_LINE_ARGS% %ACTION%
-goto end
-:doSecurity
-%_EXECJAVA% %JAVA_OPTS% %CATALINA_OPTS% %DEBUG_OPTS% -Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%" -classpath "%CLASSPATH%" -Djava.security.manager -Djava.security.policy=="%SECURITY_POLICY_FILE%" -Dcatalina.base="%CATALINA_BASE%" -Dcatalina.home="%MYCOLLAB_HOME%" -Djava.io.tmpdir="%CATALINA_TMPDIR%" %MAINCLASS% %CMD_LINE_ARGS% %ACTION%
-goto end
-:doJpda
-if not "%SECURITY_POLICY_FILE%" == "" goto doSecurityJpda
-%_EXECJAVA% %JAVA_OPTS% %CATALINA_OPTS% %JPDA_OPTS% %DEBUG_OPTS% -Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%" -classpath "%CLASSPATH%" -Dcatalina.base="%CATALINA_BASE%" -Dcatalina.home="%MYCOLLAB_HOME%" -Djava.io.tmpdir="%CATALINA_TMPDIR%" %MAINCLASS% %CMD_LINE_ARGS% %ACTION%
-goto end
-:doSecurityJpda
-%_EXECJAVA% %JAVA_OPTS% %CATALINA_OPTS% %JPDA_OPTS% %DEBUG_OPTS% -Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%" -classpath "%CLASSPATH%" -Djava.security.manager -Djava.security.policy=="%SECURITY_POLICY_FILE%" -Dcatalina.base="%CATALINA_BASE%" -Dcatalina.home="%MYCOLLAB_HOME%" -Djava.io.tmpdir="%CATALINA_TMPDIR%" %MAINCLASS% %CMD_LINE_ARGS% %ACTION%
+%_EXECJAVA% %MYCOLLAB_OPTS% -jar runner.jar  %ACTION%
 goto end
 
 :end
