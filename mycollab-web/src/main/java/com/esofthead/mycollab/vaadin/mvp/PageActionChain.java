fc14dd574a7d357f60a8fedbe99113d628b427cf
Hai Nguyen <hainguyen@esofthead.com>
2013-11-26 21:34:04 +0700

    Adjust javadoc

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/mvp/PageActionChain.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/mvp/PageActionChain.java
@@ -14,10 +14,6 @@
  * You should have received a copy of the GNU General Public License
  * along with mycollab-web.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.esofthead.mycollab.vaadin.mvp;
 
 import java.util.ArrayList;
@@ -28,43 +24,43 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- *
+ * 
  * @author haiphucnguyen
  */
 public class PageActionChain {
 	private static Logger log = LoggerFactory.getLogger(PageActionChain.class);
-	
-    private List<ScreenData> chains = new ArrayList<ScreenData>();
-    
-    public PageActionChain(ScreenData... pageActionArr) {
-        chains.addAll(Arrays.asList(pageActionArr));
-        log.debug("Chain size: " + this + "---" + chains.size());
-    }
-    
-    public PageActionChain add(ScreenData pageAction) {
-        chains.add(pageAction);
-        log.debug("Chain size: " + this + "---" + chains.size());
-        return this;
-    }
-    
-    public ScreenData pop() {
-    	log.debug("Pop pageActionChain " + this + "---" + chains.size());
-    	if (chains.size() > 0) {
-    		ScreenData pageAction = chains.get(0);
-            chains.remove(0);
-            return pageAction;
-    	} else {
-    		return null;
-    	}
-    	
-    }
-    
-    public ScreenData peek() {
-    	ScreenData pageAction = chains.get(0);
-        return pageAction;
-    }
-    
-    public boolean hasNext() {
-        return chains.size() > 0;
-    }
+
+	private List<ScreenData> chains = new ArrayList<ScreenData>();
+
+	public PageActionChain(ScreenData... pageActionArr) {
+		chains.addAll(Arrays.asList(pageActionArr));
+		log.debug("Chain size: " + this + "---" + chains.size());
+	}
+
+	public PageActionChain add(ScreenData pageAction) {
+		chains.add(pageAction);
+		log.debug("Chain size: " + this + "---" + chains.size());
+		return this;
+	}
+
+	public ScreenData pop() {
+		log.debug("Pop pageActionChain " + this + "---" + chains.size());
+		if (chains.size() > 0) {
+			ScreenData pageAction = chains.get(0);
+			chains.remove(0);
+			return pageAction;
+		} else {
+			return null;
+		}
+
+	}
+
+	public ScreenData peek() {
+		ScreenData pageAction = chains.get(0);
+		return pageAction;
+	}
+
+	public boolean hasNext() {
+		return chains.size() > 0;
+	}
 }
