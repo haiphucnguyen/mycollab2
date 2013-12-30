014a610af718a96c4da5a1a8ecf6dc70d5700dd1
Hai Nguyen <hainguyen@esofthead.com>
2013-12-30 09:42:54 +0700

    Update widgetset for cropfield

--- mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabWidgetSet/deferredjs/CDFE6BEA50F9E30EE151B26C9574ABFD/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_esofthead_mycollab_widgetset_MyCollabWidgetSet.runAsyncCallback1('L0b(3672,1,Dgh);_.gc=function $qf(){g$c((!b$c&&(b$c=new i$c),b$c),this.b.e)};ohh(Mj)(1);\n//@ sourceURL=1.js\n')
