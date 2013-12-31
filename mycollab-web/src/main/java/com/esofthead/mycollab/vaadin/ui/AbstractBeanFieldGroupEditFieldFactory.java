80d3ff6067971d747b723df3e45c9f60f9c9f8c2
Hai Nguyen <hainguyen@esofthead.com>
2013-12-31 11:36:57 +0700

    Update bean form

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/AbstractBeanFieldGroupEditFieldFactory.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/AbstractBeanFieldGroupEditFieldFactory.java
@@ -29,18 +29,18 @@ import java.util.Set;
 public abstract class AbstractBeanFieldGroupEditFieldFactory<B> implements
 		IBeanFieldGroupFieldFactory<B>, CommitHandler {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	protected GenericBeanForm<B> attachForm;
 	protected FieldGroup fieldGroup;
-    private final Validator validation;
+	private final Validator validation;
 
 	public AbstractBeanFieldGroupEditFieldFactory(GenericBeanForm<B> form) {
 		this.attachForm = form;
 		this.fieldGroup = new FieldGroup();
 		this.fieldGroup.setBuffered(true);
-        this.fieldGroup.addCommitHandler(this);
-        validation = ApplicationContextUtil
-                .getSpringBean(LocalValidatorFactoryBean.class);
+		this.fieldGroup.addCommitHandler(this);
+		validation = ApplicationContextUtil
+				.getSpringBean(LocalValidatorFactoryBean.class);
 	}
 
 	@Override
@@ -77,56 +77,63 @@ public abstract class AbstractBeanFieldGroupEditFieldFactory<B> implements
 	public void commit() {
 		try {
 			fieldGroup.commit();
-            attachForm.setValid(true);
+			attachForm.setValid(true);
 		} catch (CommitException e) {
-            attachForm.setValid(false);
-        } catch (Exception e) {
+			e.printStackTrace();
+			attachForm.setValid(false);
+		} catch (Exception e) {
 			throw new MyCollabException(e);
 		}
 	}
 
-    @Override
-    public void preCommit(FieldGroup.CommitEvent commitEvent) throws CommitException {
+	@Override
+	public void preCommit(FieldGroup.CommitEvent commitEvent)
+			throws CommitException {
 
-    }
+	}
 
-    @Override
-    public void postCommit(FieldGroup.CommitEvent commitEvent) throws CommitException {
-        for (Object propertyId : fieldGroup.getBoundPropertyIds()) {
-            fieldGroup.getField(propertyId).removeStyleName("errorField");
-        }
-        Set<ConstraintViolation<B>> violations = validation.validate(attachForm.getBean());
-        if (violations.size() > 0) {
-            StringBuilder errorMsg = new StringBuilder();
+	@Override
+	public void postCommit(FieldGroup.CommitEvent commitEvent)
+			throws CommitException {
+		for (Object propertyId : fieldGroup.getBoundPropertyIds()) {
+			fieldGroup.getField(propertyId).removeStyleName("errorField");
+		}
+		Set<ConstraintViolation<B>> violations = validation.validate(attachForm
+				.getBean());
+		if (violations.size() > 0) {
+			StringBuilder errorMsg = new StringBuilder();
 
-            for (@SuppressWarnings("rawtypes")
-            ConstraintViolation violation : violations) {
-                errorMsg.append(violation.getMessage()).append("<br/>");
+			for (@SuppressWarnings("rawtypes")
+			ConstraintViolation violation : violations) {
+				errorMsg.append(violation.getMessage()).append("<br/>");
 
-                if (violation.getPropertyPath() != null
-                        && !violation.getPropertyPath().toString().equals("")) {
-                    fieldGroup.getField(violation.getPropertyPath().toString()).addStyleName("errorField");
-                } else {
-                    Annotation validateAnno = violation
-                            .getConstraintDescriptor().getAnnotation();
-                    if (validateAnno instanceof DateComparision) {
-                        String firstDateField = ((DateComparision) validateAnno)
-                                .firstDateField();
-                        String lastDateField = ((DateComparision) validateAnno)
-                                .lastDateField();
+				if (violation.getPropertyPath() != null
+						&& !violation.getPropertyPath().toString().equals("")) {
+					fieldGroup.getField(violation.getPropertyPath().toString())
+							.addStyleName("errorField");
+				} else {
+					Annotation validateAnno = violation
+							.getConstraintDescriptor().getAnnotation();
+					if (validateAnno instanceof DateComparision) {
+						String firstDateField = ((DateComparision) validateAnno)
+								.firstDateField();
+						String lastDateField = ((DateComparision) validateAnno)
+								.lastDateField();
 
-                        fieldGroup.getField(firstDateField).addStyleName("errorField");
-                        fieldGroup.getField(lastDateField).addStyleName("errorField");
-                    }
-                }
+						fieldGroup.getField(firstDateField).addStyleName(
+								"errorField");
+						fieldGroup.getField(lastDateField).addStyleName(
+								"errorField");
+					}
+				}
 
-            }
+			}
 
-            NotificationUtil.showErrorNotification(errorMsg.toString());
-            attachForm.setValid(false);
-            throw new CommitException(errorMsg.toString());
-        }
-    }
+			NotificationUtil.showErrorNotification(errorMsg.toString());
+			attachForm.setValid(false);
+			throw new CommitException(errorMsg.toString());
+		}
+	}
 
 	abstract protected Field<?> onCreateField(Object propertyId);
 }
--- mycollab-web/src/main/java/com/esofthead/mycollab/web/MyCollabUIProvider.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/web/MyCollabUIProvider.java
@@ -1,5 +1,8 @@
 package com.esofthead.mycollab.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.esofthead.mycollab.mobile.MobileUI;
 import com.vaadin.server.UIClassSelectionEvent;
 import com.vaadin.server.UIProvider;
@@ -14,10 +17,14 @@ import com.vaadin.ui.UI;
 public class MyCollabUIProvider extends UIProvider {
 	private static final long serialVersionUID = 1L;
 
+	private static Logger log = LoggerFactory
+			.getLogger(MyCollabUIProvider.class);
+
 	@Override
 	public Class<? extends UI> getUIClass(UIClassSelectionEvent event) {
 		String userAgent = event.getRequest().getHeader("user-agent")
 				.toLowerCase();
+		log.debug("User agent: {}", userAgent);
 		if (userAgent.contains("webkit")) {
 			return MobileUI.class;
 		} else {
