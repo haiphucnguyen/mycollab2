8e3422027adf69ea75a9123e0610c259839c99c7
Nghi Tran <tpmnghi@gmail.com>
2014-01-24 00:03:58 +0700

    update mobile widgetset

---
+++ mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet/deferredjs/089679010AB66C945AC4C8D561F68A10/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_esofthead_mycollab_widgetset_MyCollabMobileWidgetSet.runAsyncCallback2('vZb(3308,1,Ypg);_.Od=function LWe(){FSc((!ASc&&(ASc=new HSc),ASc),this.b.e)};mrg(vp)(2);\n//@ sourceURL=2.js\n')
---
+++ mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet/deferredjs/7F61449345A67ACCC56C6E803D04BD2A/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_esofthead_mycollab_widgetset_MyCollabMobileWidgetSet.runAsyncCallback2('FYb(3270,1,Wmg);_.Od=function fTe(){_Oc((!WOc&&(WOc=new bPc),WOc),this.b.e)};jog(up)(2);\n//@ sourceURL=2.js\n')
