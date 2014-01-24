d60f307825d8ea3636811e47d3b7c173703a558e
Hai Nguyen <hainguyen@esofthead.com>
2014-01-24 22:48:17 +0700

    Update widgetset

---
+++ mycollab-app-community/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet/deferredjs/A9ED8BA599582A3DDAB3E6882AADF17C/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_esofthead_mycollab_widgetset_MyCollabMobileWidgetSet.runAsyncCallback2('FYb(3270,1,Vmg);_.Od=function eTe(){$Oc((!VOc&&(VOc=new aPc),VOc),this.b.e)};iog(up)(2);\n//@ sourceURL=2.js\n')
---
+++ mycollab-app-community/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet/deferredjs/D1BD3C22E67ADB7F12A8BC7B2BAFF401/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_esofthead_mycollab_widgetset_MyCollabMobileWidgetSet.runAsyncCallback2('vZb(3308,1,Xpg);_.Od=function KWe(){ESc((!zSc&&(zSc=new GSc),zSc),this.b.e)};lrg(vp)(2);\n//@ sourceURL=2.js\n')
