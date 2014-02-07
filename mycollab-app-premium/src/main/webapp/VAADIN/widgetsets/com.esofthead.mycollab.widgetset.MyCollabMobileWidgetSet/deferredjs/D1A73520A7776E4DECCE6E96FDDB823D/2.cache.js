1d6fca1910ae00f891328f581068b0bded8f3291
Nghi Tran <nghitran@esofthead.com>
2014-02-07 08:49:16 +0700

    update widgetset

---
+++ mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet/deferredjs/D1A73520A7776E4DECCE6E96FDDB823D/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_esofthead_mycollab_widgetset_MyCollabMobileWidgetSet.runAsyncCallback2('vZb(3308,1,Wpg);_.Od=function JWe(){DSc((!ySc&&(ySc=new FSc),ySc),this.b.e)};krg(vp)(2);\n//@ sourceURL=2.js\n')
