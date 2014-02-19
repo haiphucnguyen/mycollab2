c7cf5d57876961c1edd98e7af445ef9935cc66e4
Nghi Tran <nghitran@esofthead.com>
2014-02-19 14:42:54 +0700

    update mobilecomponent

--- extras/vaadin/addons/mobilecomponent/src/main/java/com/esofthead/vaadin/mobilecomponent/client/VMobileNavigationManager.java
+++ extras/vaadin/addons/mobilecomponent/src/main/java/com/esofthead/vaadin/mobilecomponent/client/VMobileNavigationManager.java
@@ -9,86 +9,90 @@ import com.vaadin.addon.touchkit.gwt.client.ui.VNavigationManager;
 
 public class VMobileNavigationManager extends VNavigationManager {
 
-    private final SimplePanel navigationMenu;
+	private final SimplePanel navigationMenu;
 
-    private boolean menuVisibility = false;
-    
-    private int currentOffset = 0;
-    
-    private final SimplePanel wrapper;
-    private final DivElement content;
+	private boolean menuVisibility = false;
 
-    public VMobileNavigationManager() {
-        super();
-        
-        content = getElement().cast();
-        wrapper = new SimplePanel();
-        wrapper.setStylePrimaryName("v-mobilecomponent");
-        
+	private int currentOffset = 0;
 
-        navigationMenu = new SimplePanel();
-        navigationMenu.setStyleName(getStyleName() + "-navigation");
-        
-        wrapper.getElement().appendChild(content);
-        
-        setElement(wrapper.getElement());
-    }
+	private final SimplePanel wrapper;
+	private final DivElement content;
 
-    public void setNavigationMenu(Widget widget) {
-    	if(!navigationMenu.isAttached()) {
-    		add(navigationMenu, wrapper.getElement());
-    	}   		
-    	
-        navigationMenu.setWidget(widget);
-    }
+	public VMobileNavigationManager() {
+		super();
 
-    public void toggleMenu() {
-        toggleMenu(!menuVisibility);
-    }
-    
-    public void toggleMenu(boolean visibility) {
-    	menuVisibility = visibility;
-    	
-    	Style style = content.getStyle();
-    	style.setProperty(Css3Propertynames.transition(), "");
-    	
-    	if (menuVisibility) {
-            int menuWidth = getNavigationMenuWidth();
-            if (menuWidth == 0) {
-                menuVisibility = false;
-            } else {
-            	moveLeft(menuWidth, true);
-            	currentOffset = menuWidth;
-            }
-        } else {
-        	moveLeft(0, true);
-        	currentOffset = 0;
-        }
-    }
-    
-    public int getNavigationMenuWidth() {
-    	return navigationMenu.getOffsetWidth();
-    }
-    
-    public void setHorizontalOffsetExt(int deltaX, boolean animate) {
-    	int tmpOffset = currentOffset + deltaX;
-    	if (tmpOffset >= getNavigationMenuWidth()) {
-    		tmpOffset = getNavigationMenuWidth();
-    		menuVisibility = true;
-    	}    		
-    	else if (tmpOffset <= 0) { 
-    		tmpOffset = 0;
-    		menuVisibility = false;
-    	}
-    	
-    	moveLeft(tmpOffset, animate);
-    }
-    
-    public void moveLeft(int pos, boolean animate) {
-    	final Style style = content.getStyle();
-    	if(!animate)
-    		style.setProperty(Css3Propertynames.transition(), "none");
-    	
-    	style.setProperty(Css3Propertynames.transform(), "translateX(" + pos + "px)");
-    }
+		content = getElement().cast();
+		wrapper = new SimplePanel();
+		wrapper.setStylePrimaryName("v-mobilecomponent");
+
+
+		navigationMenu = new SimplePanel();
+		navigationMenu.setStyleName(getStyleName() + "-navigation");
+
+		wrapper.getElement().appendChild(content);
+
+		setElement(wrapper.getElement());
+	}
+
+	public void setNavigationMenu(Widget widget) {
+		if(!navigationMenu.isAttached()) {
+			add(navigationMenu, wrapper.getElement());
+		}   		
+
+		navigationMenu.setWidget(widget);
+	}
+
+	public boolean getMenuVisibility() {
+		return menuVisibility;
+	}
+
+	public void toggleMenu() {
+		toggleMenu(!menuVisibility);
+	}
+
+	public void toggleMenu(boolean visibility) {
+		menuVisibility = visibility;
+
+		Style style = content.getStyle();
+		style.setProperty(Css3Propertynames.transition(), "");
+
+		if (menuVisibility) {
+			int menuWidth = getNavigationMenuWidth();
+			if (menuWidth == 0) {
+				menuVisibility = false;
+			} else {
+				moveLeft(menuWidth, true);
+				currentOffset = menuWidth;
+			}
+		} else {
+			moveLeft(0, true);
+			currentOffset = 0;
+		}
+	}
+
+	public int getNavigationMenuWidth() {
+		return navigationMenu.getOffsetWidth();
+	}
+
+	public void setHorizontalOffsetExt(int deltaX, boolean animate) {
+		int tmpOffset = currentOffset + deltaX;
+		if (tmpOffset >= getNavigationMenuWidth()) {
+			tmpOffset = getNavigationMenuWidth();
+			menuVisibility = true;
+		}    		
+		else if (tmpOffset <= 0) { 
+			tmpOffset = 0;
+			menuVisibility = false;
+		}
+
+		moveLeft(tmpOffset, animate);
+	}
+
+	public void moveLeft(int pos, boolean animate) {
+		final Style style = content.getStyle();
+		if(!animate)
+			style.setProperty(Css3Propertynames.transition(), "none");
+
+		style.setProperty(Css3Propertynames.transform(), "translateX(" + pos + "px)");
+	}
 }
