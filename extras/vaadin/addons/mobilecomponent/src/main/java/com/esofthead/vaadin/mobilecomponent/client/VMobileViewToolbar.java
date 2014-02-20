94b11872dda9abcec4328bb31eaee09ec52130e1
Nghi Tran <nghitran@esofthead.com>
2014-02-20 20:18:57 +0700

    update mobilecomponent

--- extras/vaadin/addons/mobilecomponent/src/main/java/com/esofthead/vaadin/mobilecomponent/client/VMobileViewToolbar.java
+++ extras/vaadin/addons/mobilecomponent/src/main/java/com/esofthead/vaadin/mobilecomponent/client/VMobileViewToolbar.java
@@ -1,35 +1,143 @@
 package com.esofthead.vaadin.mobilecomponent.client;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.dom.client.DivElement;
 import com.google.gwt.dom.client.Document;
-import com.google.gwt.user.client.ui.SimplePanel;
-import com.vaadin.client.ui.TouchScrollDelegate;
-import com.vaadin.client.ui.TouchScrollDelegate.TouchScrollHandler;
+import com.google.gwt.dom.client.Style;
+import com.google.gwt.dom.client.Style.Display;
+import com.google.gwt.dom.client.Style.Unit;
+import com.google.gwt.event.dom.client.ScrollEvent;
+import com.google.gwt.event.dom.client.ScrollHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.Element;
+import com.google.gwt.user.client.Event;
+import com.google.gwt.user.client.ui.ComplexPanel;
+import com.google.gwt.user.client.ui.Widget;
 
-public class VMobileViewToolbar extends SimplePanel {
+public class VMobileViewToolbar extends ComplexPanel implements ScrollHandler {
 	private static final String CLASSNAME = "v-mobileview-toolbar";
+
+	private static Logger log = Logger.getLogger(VMobileViewToolbar.class.getName());
 
 	private final DivElement prevBtn;
 	private final DivElement nextBtn;
-	private final TouchScrollHandler touchScrollHandler;
+	private int overflowWidth = 0;
+	private Widget onlyChild;
+	private final Element wrapper;
 
 	public VMobileViewToolbar() {
 		setElement(Document.get().createDivElement());
 		setStylePrimaryName(CLASSNAME);
 
+		wrapper = Document.get().createDivElement().cast();
+		wrapper.addClassName(CLASSNAME + "-wrap");
+		wrapper.getStyle().setWidth(100, Unit.PCT);
+		wrapper.getStyle().setHeight(100, Unit.PCT);
+		getElement().appendChild(wrapper);
+
 		prevBtn = Document.get().createDivElement();
 		prevBtn.addClassName(CLASSNAME + "-prevBtn");
-		getContainerElement().appendChild(prevBtn);
+		prevBtn.setInnerText("<<");
+		getElement().appendChild(prevBtn);
 
 		nextBtn = Document.get().createDivElement();
 		nextBtn.addClassName(CLASSNAME + "-nextBtn");
-		getContainerElement().appendChild(nextBtn);
+		nextBtn.setInnerText(">>");
+		getElement().appendChild(nextBtn);
 
-		touchScrollHandler = TouchScrollDelegate
-				.enableTouchScrolling(this, getElement());
+		DOM.sinkEvents(getElement(), Event.ONSCROLL | Event.ONCLICK);
+		/*touchScrollHandler = TouchScrollDelegate
+				.enableTouchScrolling(this, getElement());*/
+		addHandler(this, ScrollEvent.getType());
 	}
 
-	protected TouchScrollHandler getTouchScrollHanlder() {
-		return this.touchScrollHandler;
+	@Override
+	public void add(Widget w) {
+		add(w, getWrapper());
+		checkWidth();
+	}
+
+	public void setWidget(Widget w) {
+		if (onlyChild != null && onlyChild != w) {
+			remove(onlyChild);
+		}
+		onlyChild = w;
+		if (onlyChild.getParent() != this) {
+			add(onlyChild, getWrapper());
+			Scheduler.get().scheduleFinally(new Scheduler.ScheduledCommand()
+			{
+				@Override
+				public void execute() {
+					checkWidth();
+				}
+			});
+		}
+	}
+
+	@Override
+	public boolean remove(Widget w) {
+		boolean removed = super.remove(w);
+		if (removed) {
+			if (onlyChild == w) {
+				onlyChild = null;
+			}
+		}
+		return removed;
+	}
+
+	public Element getContainerElement() {
+		if (onlyChild != null) {
+			return onlyChild.getElement();
+		}
+		return null;
+	}
+
+	public Element getWrapper() {
+		return this.wrapper;
+	}
+
+	public void checkWidth() {
+
+		if (onlyChild != null && getContainerElement().getOffsetWidth() > getWrapper().getOffsetWidth()) {
+			overflowWidth = getContainerElement().getOffsetWidth() - getWrapper().getOffsetWidth();
+			nextBtn.getStyle().setDisplay(Display.BLOCK);
+		} else {
+			log.log(Level.INFO, "content's width: " + getContainerElement().getOffsetWidth());
+			overflowWidth = 0;
+			nextBtn.getStyle().setDisplay(Display.NONE);
+		}
+	}
+
+	@Override
+	public void onScroll(ScrollEvent event) {
+		int scrollLeft = getWrapper().getScrollLeft();
+		Style leftStyle = prevBtn.getStyle();
+		Style rightStyle = nextBtn.getStyle();
+
+		if (scrollLeft == 0) {
+			leftStyle.setDisplay(Display.NONE);
+			rightStyle.setDisplay(Display.BLOCK);
+		} else {
+			leftStyle.setDisplay(Display.BLOCK);
+			if (scrollLeft == overflowWidth)
+				rightStyle.setDisplay(Display.NONE);
+			else
+				rightStyle.setDisplay(Display.BLOCK);
+		}
+	}
+
+	@Override
+	public void onBrowserEvent(Event event) {
+		if (event.getTypeInt() == Event.ONCLICK) {
+			if(DOM.eventGetTarget(event) == prevBtn.cast())
+			{
+				getElement().setScrollLeft(0);
+			} else if (DOM.eventGetTarget(event) == nextBtn.cast()) {
+				getElement().setScrollLeft(overflowWidth);
+			}
+		}
 	}
 }
--- extras/vaadin/addons/mobilecomponent/src/main/java/com/esofthead/vaadin/mobilecomponent/client/ui/MobileViewToolbarConnector.java
+++ extras/vaadin/addons/mobilecomponent/src/main/java/com/esofthead/vaadin/mobilecomponent/client/ui/MobileViewToolbarConnector.java
@@ -6,17 +6,62 @@ import com.esofthead.vaadin.mobilecomponent.MobileViewToolbar;
 import com.esofthead.vaadin.mobilecomponent.client.VMobileViewToolbar;
 import com.esofthead.vaadin.mobilecomponent.client.shared.MobileViewToolbarState;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.core.client.Scheduler;
+import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.user.client.ui.Widget;
 import com.vaadin.client.ComponentConnector;
 import com.vaadin.client.ConnectorHierarchyChangeEvent;
 import com.vaadin.client.communication.StateChangeEvent;
 import com.vaadin.client.ui.AbstractComponentContainerConnector;
+import com.vaadin.client.ui.layout.ElementResizeEvent;
+import com.vaadin.client.ui.layout.ElementResizeListener;
 import com.vaadin.shared.ui.Connect;
 
 @Connect(MobileViewToolbar.class)
 public class MobileViewToolbarConnector extends AbstractComponentContainerConnector {
 
 	private static final long serialVersionUID = -2664274991068733980L;
+	private final ElementResizeListener resizeListener = new ElementResizeListener() {
+
+		@Override
+		public void onElementResize(ElementResizeEvent e) {
+			if(!alreadyLayouted) {
+				MobileViewToolbarConnector.this.getWidget().checkWidth();
+				Scheduler.get().scheduleFinally(doResetLayouting);
+				alreadyLayouted = true;
+			}
+		}
+
+	};
+
+	private boolean alreadyLayouted;
+
+	private ScheduledCommand doResetLayouting = new ScheduledCommand() {
+		@Override
+		public void execute() {
+			alreadyLayouted = false;
+		}
+	};
+
+
+
+	@Override
+	protected void init() {
+		super.init();
+
+		getLayoutManager().addElementResizeListener(getWidget().getContainerElement(), resizeListener);
+	}
+
+
+
+	@Override
+	public void onUnregister() {
+		super.onUnregister();
+
+		getLayoutManager().removeElementResizeListener(getWidget().getContainerElement(), resizeListener);
+	}
+
+
 
 	@Override
 	public void updateCaption(ComponentConnector connector) {
