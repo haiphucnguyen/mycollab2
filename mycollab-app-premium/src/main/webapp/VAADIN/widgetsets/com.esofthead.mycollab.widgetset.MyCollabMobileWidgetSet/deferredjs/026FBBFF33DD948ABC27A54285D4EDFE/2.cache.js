4791019d47894a897b923040437980b9b8f9945d
Nghi Tran <tpmnghi@gmail.com>
2014-02-23 12:34:39 +0700

    update mycollab-mobile's widgetset

---
+++ mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet/deferredjs/026FBBFF33DD948ABC27A54285D4EDFE/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_esofthead_mycollab_widgetset_MyCollabMobileWidgetSet.runAsyncCallback2('e_b(3311,1,Eqg);_.ae=function $We(){iRc((!dRc&&(dRc=new kRc),dRc),this.b.e)};Rrg(er)(2);\n//@ sourceURL=2.js\n')
