da98461cfe91a8db6a11b9f4573a64d76fc4cee2
Nghi Tran <nghitran@esofthead.com>
2014-02-24 17:23:28 +0700

    center the VMobileNavigationBar's title

--- extras/vaadin/addons/mobilecomponent/src/main/java/com/esofthead/vaadin/mobilecomponent/client/VMobileNavigationBar.java
+++ extras/vaadin/addons/mobilecomponent/src/main/java/com/esofthead/vaadin/mobilecomponent/client/VMobileNavigationBar.java
@@ -12,68 +12,68 @@ import com.vaadin.addon.touchkit.gwt.client.ui.VNavigationBar;
 public class VMobileNavigationBar extends VNavigationBar {
 
 	private static Logger log = Logger.getLogger(VMobileNavigationBar.class.getName());
-	
+
 	private Widget leftComponent;
 	private Widget rightComponent;
-	
+
 	@Override
 	public void avoidCaptionOverlap() {
 		DivElement caption = (DivElement) getElement().getFirstChildElement();
 		DivElement rightElement = (DivElement) caption.getNextSiblingElement();
 		DivElement leftElement = (DivElement) rightElement.getNextSiblingElement();
-		
+
 		int leftSize = leftComponent != null ? leftElement.getOffsetLeft() + leftElement.getOffsetWidth() : 0;
 		log.log(Level.INFO, "leftSize: " + leftSize);
-		
-		int rightSize = rightComponent != null ? getOffsetWidth() - rightElement.getOffsetLeft() : 0;
+
+		int rightSize = rightComponent != null ? getOffsetWidth() - rightElement.getOffsetLeft() : leftSize;
 		log.log(Level.INFO, "rightSize: " + rightSize);
-		
+
 		caption.getStyle().setWidth((getOffsetWidth() - leftSize - rightSize), Unit.PX);
 		caption.getStyle().setLeft(leftSize, Unit.PX);
 		caption.getStyle().setPosition(Position.ABSOLUTE);
 	}
-	
+
 	@Override
-    public void setLeftWidget(Widget left) {
+	public void setLeftWidget(Widget left) {
 		if(getWidgetCount() == 0) {
 			super.setLeftWidget(left);
 			leftComponent = left;
 		} else{
 			if (left != null) {
-	            if (leftComponent != left && leftComponent != null) {
-	            	remove(leftComponent);
-	            }
+				if (leftComponent != left && leftComponent != null) {
+					remove(leftComponent);
+				}
 
-	            leftComponent = left;
-	            if (!leftComponent.isAttached()) {
-	                super.setLeftWidget(left);
-	            }
-	        } else if (leftComponent != null) {
-	            remove(leftComponent);
-	        }
+				leftComponent = left;
+				if (!leftComponent.isAttached()) {
+					super.setLeftWidget(left);
+				}
+			} else if (leftComponent != null) {
+				remove(leftComponent);
+			}
 		}
-        
-       
-    }
 
-    @Override
-    public void setRightWidget(Widget right) {
-        if (getWidgetCount() == 1) {
-        	super.setRightWidget(right);
-        	rightComponent = right;
-        } else {
-        	if (right != null) {
-                if (rightComponent != right && rightComponent != null) {
-                    remove(rightComponent);
-                }
 
-                rightComponent = right;
-                if (!rightComponent.isAttached()) {
-                    super.setRightWidget(right);
-                }
-            } else if (rightComponent != null) {
-            	remove(rightComponent);
-            }
-        }
-    }	
+	}
+
+	@Override
+	public void setRightWidget(Widget right) {
+		if (getWidgetCount() == 1) {
+			super.setRightWidget(right);
+			rightComponent = right;
+		} else {
+			if (right != null) {
+				if (rightComponent != right && rightComponent != null) {
+					remove(rightComponent);
+				}
+
+				rightComponent = right;
+				if (!rightComponent.isAttached()) {
+					super.setRightWidget(right);
+				}
+			} else if (rightComponent != null) {
+				remove(rightComponent);
+			}
+		}
+	}	
 }
