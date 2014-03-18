d901aaa44c9c24d10b0c92d48d6001b121894f6e
Hai Nguyen <hainguyen@esofthead.com>
2014-03-18 15:36:08 +0700

    Update task filter view

---
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/task/FilterTaskViewImpl.java
@@ -0,0 +1,24 @@
+package com.esofthead.mycollab.module.project.view.task;
+
+import com.esofthead.mycollab.module.project.view.parameters.TaskFilterParameter;
+import com.esofthead.mycollab.vaadin.mvp.AbstractPageView;
+import com.esofthead.mycollab.vaadin.mvp.ViewComponent;
+
+/**
+ * 
+ * @author MyCollab Ltd.
+ * @since 4.0
+ * 
+ */
+@ViewComponent
+public class FilterTaskViewImpl extends AbstractPageView implements
+		FilterTaskView {
+	private static final long serialVersionUID = 1L;
+	
+
+	@Override
+	public void filterTasks(TaskFilterParameter filterParam) {
+
+	}
+
+}
