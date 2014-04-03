233146b334040d508571937ea10e42736ebf4057
Hai Nguyen <hainguyen@esofthead.com>
2014-04-03 19:15:08 +0700

    Update widget set of easy upload

--- mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabWidgetSet/deferredjs/5B7F14CA2DA4DBD3CC21D43354A416BD/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_esofthead_mycollab_widgetset_MyCollabWidgetSet.runAsyncCallback1('Lgc(3897,1,zAh);_.de=function qIf(){q5c((!l5c&&(l5c=new s5c),l5c),this.a.d)};pBh(Bt)(1);\n//@ sourceURL=1.js\n')
