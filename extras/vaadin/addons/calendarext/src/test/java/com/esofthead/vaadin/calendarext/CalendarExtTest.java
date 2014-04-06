2789760bda20224a45f9692fcd0375c66215fed0
Nghi Tran <tpmnghi@gmail.com>
2014-04-06 12:12:30 +0700

    add CalendarExt addon

---
+++ extras/vaadin/addons/calendarext/src/test/java/com/esofthead/vaadin/calendarext/CalendarExtTest.java
@@ -0,0 +1,13 @@
+package com.esofthead.vaadin.calendarext;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+// JUnit tests here
+public class CalendarExtTest {
+
+	@Test
+	public void thisAlwaysPasses() {
+		Assert.assertEquals(true, true);
+	}
+}
