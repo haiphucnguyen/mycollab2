0d4319042da401530d2478fde1231282223549e2
Hai Nguyen <hainguyen@esofthead.com>
2014-04-07 13:32:14 +0700

    Update mobile widgetset

--- mycollab-app-ondemand/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet/deferredjs/CFA25BF95EE0BCC6B98D77D5A071A2C3/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_esofthead_mycollab_widgetset_MyCollabMobileWidgetSet.runAsyncCallback2('d_b(3309,1,zqg);_.ae=function DWe(){NQc((!IQc&&(IQc=new PQc),IQc),this.b.e)};Mrg(er)(2);\n//@ sourceURL=2.js\n')
