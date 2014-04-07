4eb5860290747a910af864833e1d2001225172b1
Nghi Tran <nghitran@esofthead.com>
2014-04-07 10:26:06 +0700

    update app-premium's widgetset, fix issue #327

--- mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabWidgetSet/deferredjs/A192861F5916490624E0239F7CCBAF05/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_esofthead_mycollab_widgetset_MyCollabWidgetSet.runAsyncCallback1('mgc(3889,1,pyh);_.ie=function yHf(){y4c((!t4c&&(t4c=new A4c),t4c),this.b.e)};ezh(Ct)(1);\n//@ sourceURL=1.js\n')
