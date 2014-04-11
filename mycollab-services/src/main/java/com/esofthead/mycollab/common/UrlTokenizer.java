3e42caf90a1cf2f98c3a5575965248f9e19c048c
Hai Nguyen <hainguyen@esofthead.com>
2014-04-11 15:56:16 +0700

    Simplify url token analyzer

--- mycollab-services/src/main/java/com/esofthead/mycollab/common/UrlTokenizer.java
+++ mycollab-services/src/main/java/com/esofthead/mycollab/common/UrlTokenizer.java
@@ -1,5 +1,7 @@
 package com.esofthead.mycollab.common;
 
+import java.util.StringTokenizer;
+
 /**
  * 
  * @author MyCollab Ltd.
@@ -8,4 +10,32 @@ package com.esofthead.mycollab.common;
  */
 public class UrlTokenizer {
 
+	private StringTokenizer urlTokenizer;
+
+	public UrlTokenizer(String url) {
+		String internalVal = (url.startsWith("/")) ? url.substring(1) : url;
+		internalVal = UrlEncodeDecoder.decode(internalVal);
+		urlTokenizer = new StringTokenizer(internalVal, "/");
+	}
+
+	public int getInt() throws InvalidTokenException {
+		if (urlTokenizer.hasMoreTokens()) {
+			try {
+				int intVal = Integer.parseInt(urlTokenizer.nextToken());
+				return intVal;
+			} catch (NumberFormatException e) {
+				throw new InvalidTokenException(e);
+			}
+		} else {
+			throw new InvalidTokenException("Invalid token");
+		}
+	}
+
+	public String getString() throws InvalidTokenException {
+		if (urlTokenizer.hasMoreTokens()) {
+			return urlTokenizer.nextToken();
+		} else {
+			throw new InvalidTokenException("Invalid token");
+		}
+	}
 }
