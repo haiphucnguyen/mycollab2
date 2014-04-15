a150ee877e34c1d9f8e69cf873fd90eccd4f5f8e
Nghi Tran <nghitran@esofthead.com>
2014-04-15 12:03:38 +0700

    change notification email template for Contact module

---
+++ mycollab-services/src/main/java/com/esofthead/mycollab/schedule/email/crm/CrmMailLinkGenerator.java
@@ -0,0 +1,30 @@
+package com.esofthead.mycollab.schedule.email.crm;
+
+import com.esofthead.mycollab.common.GenericLinkUtils;
+import com.esofthead.mycollab.module.crm.CrmLinkGenerator;
+
+public class CrmMailLinkGenerator extends GenericLinkUtils {
+
+	private final String siteUrl;
+
+	public CrmMailLinkGenerator(String siteUrl) {
+		this.siteUrl = siteUrl;
+	}
+
+	public String generateAccountPreviewFullLink(Integer accountId) {
+		return siteUrl + GenericLinkUtils.URL_PREFIX_PARAM
+				+ CrmLinkGenerator.generateAccountPreviewLink(accountId);
+	}
+
+	public String generateContactPreviewFullLink(Integer contactId) {
+		return siteUrl + GenericLinkUtils.URL_PREFIX_PARAM
+				+ CrmLinkGenerator.generateContactPreviewLink(contactId);
+	}
+
+	@Override
+	public String getSiteUrl() {
+
+		return this.siteUrl;
+	}
+
+}
