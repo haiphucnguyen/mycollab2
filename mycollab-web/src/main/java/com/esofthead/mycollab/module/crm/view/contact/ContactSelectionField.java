79a5cb9beb413c66fed32b83cb9b2f68bafa6188
Nghi Tran <nghitran@esofthead.com>
2014-04-16 16:08:47 +0700

    update new template for notification email in Activity module

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/view/contact/ContactSelectionField.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/crm/view/contact/ContactSelectionField.java
@@ -16,7 +16,6 @@
  */
 package com.esofthead.mycollab.module.crm.view.contact;
 
-import com.esofthead.mycollab.core.MyCollabException;
 import com.esofthead.mycollab.module.crm.domain.Contact;
 import com.esofthead.mycollab.module.crm.domain.SimpleContact;
 import com.esofthead.mycollab.module.crm.service.ContactService;
@@ -27,7 +26,6 @@ import com.esofthead.mycollab.vaadin.ui.MyCollabResource;
 import com.vaadin.data.Property;
 import com.vaadin.event.MouseEvents;
 import com.vaadin.event.MouseEvents.ClickEvent;
-import com.vaadin.ui.AbstractField;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CustomField;
@@ -103,26 +101,23 @@ public class ContactSelectionField extends CustomField<Integer> implements
 		if (value instanceof Integer) {
 			setContactByVal((Integer) value);
 			super.setPropertyDataSource(newDataSource);
-
-		} else if (value instanceof SimpleContact) {
-			setInternalContact((SimpleContact) value);
-			super.setPropertyDataSource(new AbstractField<Integer>() {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public Integer getValue() {
-					return ((SimpleContact) value).getId();
-				}
-
-				@Override
-				public Class<? extends Integer> getType() {
-					return Integer.class;
-				}
-			});
 		} else {
-			throw new MyCollabException(
-					"Do not support property source different than int or SimpleContact");
+			super.setPropertyDataSource(newDataSource);
 		}
+		/*
+		 * else if (value instanceof SimpleContact) {
+		 * setInternalContact((SimpleContact) value);
+		 * super.setPropertyDataSource(new AbstractField<Integer>() { private
+		 * static final long serialVersionUID = 1L;
+		 * 
+		 * @Override public Integer getValue() { return ((SimpleContact)
+		 * value).getId(); }
+		 * 
+		 * @Override public Class<? extends Integer> getType() { return
+		 * Integer.class; } }); } else { throw new MyCollabException(
+		 * "Do not support property source different than int or SimpleContact"
+		 * ); }
+		 */
 	}
 
 	@Override
