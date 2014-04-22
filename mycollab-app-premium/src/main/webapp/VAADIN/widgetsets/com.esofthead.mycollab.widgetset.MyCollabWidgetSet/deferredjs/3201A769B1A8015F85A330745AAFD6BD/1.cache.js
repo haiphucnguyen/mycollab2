c1a6f2daeb0fa4a9c543711a071e7c3ba913956e
Hai Nguyen <hainguyen@esofthead.com>
2014-04-22 09:29:46 +0700

    Update library and fix minor bugs of member full name

--- mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabWidgetSet/deferredjs/3201A769B1A8015F85A330745AAFD6BD/1.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_esofthead_mycollab_widgetset_MyCollabWidgetSet.runAsyncCallback1('bic(3902,1,eBh);_.he=function JJf(){A6c((!v6c&&(v6c=new C6c),v6c),this.a.d)};VBh(Pu)(1);\n//@ sourceURL=1.js\n')
---
+++ mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabWidgetSet/deferredjs/E69AAE4BB4B538AC9F7697318E23B7CC/1.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_esofthead_mycollab_widgetset_MyCollabWidgetSet.runAsyncCallback1('_hc(3905,1,bBh);_.me=function SJf(){c6c((!Z5c&&(Z5c=new e6c),Z5c),this.b.e)};RBh(Qu)(1);\n//@ sourceURL=1.js\n')
