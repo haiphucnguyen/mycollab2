33e5520f0e3d05e77cc0de625818b8f0432d5a76
Nghi Tran <nghitran@esofthead.com>
2014-05-28 20:20:50 +0700

    add ThemeContainer to account settings module

---
+++ mycollab-web-premium/src/main/java/com/esofthead/mycollab/premium/module/user/accountsettings/theme/view/ThemePresenter.java
@@ -0,0 +1,47 @@
+/**
+ * This file is part of mycollab-web-premium.
+ *
+ * mycollab-web-premium is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mycollab-web-premium is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mycollab-web-premium.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.esofthead.mycollab.premium.module.user.accountsettings.theme.view;
+
+import com.esofthead.mycollab.module.user.accountsettings.theme.view.IThemeContainer;
+import com.esofthead.mycollab.module.user.accountsettings.theme.view.IThemePresenter;
+import com.esofthead.mycollab.module.user.accountsettings.view.AccountModule;
+import com.esofthead.mycollab.vaadin.mvp.ScreenData;
+import com.esofthead.mycollab.vaadin.ui.AbstractPresenter;
+import com.vaadin.ui.ComponentContainer;
+
+/**
+ * 
+ * @author MyCollab Ltd.
+ * @since 4.1
+ * 
+ */
+public class ThemePresenter extends AbstractPresenter<IThemeContainer>
+		implements IThemePresenter {
+	private static final long serialVersionUID = -4221018940384221262L;
+
+	public ThemePresenter() {
+		super(IThemeContainer.class);
+	}
+
+	@Override
+	protected void onGo(ComponentContainer container, ScreenData<?> data) {
+		AccountModule accountContainer = (AccountModule) container;
+
+		accountContainer.gotoSubView("theme");
+	}
+
+}
