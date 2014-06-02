2dcb94ed5acd66e1149fa7b6ccbfd8ac400b07b0
Hai Nguyen <hainguyen@esofthead.com>
2014-06-02 13:13:27 +0700

    Rename package of project field formats

--- mycollab-web-premium/src/main/java/com/esofthead/mycollab/premium/module/project/view/risk/RiskHistoryList.java
+++ mycollab-web-premium/src/main/java/com/esofthead/mycollab/premium/module/project/view/risk/RiskHistoryList.java
@@ -3,7 +3,7 @@ package com.esofthead.mycollab.premium.module.project.view.risk;
 import com.esofthead.mycollab.common.localization.GenericI18Enum;
 import com.esofthead.mycollab.vaadin.AppContext;
 import com.esofthead.mycollab.module.project.localization.RiskI18nEnum;
-import com.esofthead.mycollab.module.project.ui.components.ProjectMemberHistoryFieldFormat;
+import com.esofthead.mycollab.module.project.ui.format.ProjectMemberHistoryFieldFormat;
 import com.esofthead.mycollab.vaadin.ui.HistoryLogComponent;
 
 /**
