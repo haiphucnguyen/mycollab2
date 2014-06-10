a671e8d37043d2d2b79252f365ea15f23988999a
Hai Nguyen <hainguyen@esofthead.com>
2014-06-10 14:19:45 +0700

    Update gantt chart

--- mycollab-app-ondemand/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet/deferredjs/F08567CDEF23192D24EF429B189B5802/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_esofthead_mycollab_widgetset_MyCollabMobileWidgetSet.runAsyncCallback2('V_b(3347,1,Btg);_.ae=function i$e(){sUc((!nUc&&(nUc=new uUc),nUc),this.b.e)};Pug(fr)(2);\n//@ sourceURL=2.js\n')
