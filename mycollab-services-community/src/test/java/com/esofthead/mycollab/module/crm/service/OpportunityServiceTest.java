607a0064ab628a1df14d9eb254c59faf1eb610ae
Hai Nguyen <hainguyen@esofthead.com>
2014-06-30 22:11:09 +0700

    Update license headers

--- mycollab-services-community/src/test/java/com/esofthead/mycollab/module/crm/service/OpportunityServiceTest.java
+++ mycollab-services-community/src/test/java/com/esofthead/mycollab/module/crm/service/OpportunityServiceTest.java
@@ -1,18 +1,18 @@
 /**
- * This file is part of mycollab-services.
+ * This file is part of mycollab-services-community.
  *
- * mycollab-services is free software: you can redistribute it and/or modify
+ * mycollab-services-community is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * mycollab-services is distributed in the hope that it will be useful,
+ * mycollab-services-community is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with mycollab-services.  If not, see <http://www.gnu.org/licenses/>.
+ * along with mycollab-services-community.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.esofthead.mycollab.module.crm.service;
 
