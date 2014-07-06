9ade71af15e9a4ad646fb526ea574a9d7a04faad
Hai Nguyen <hainguyen@esofthead.com>
2014-07-06 15:20:22 +0700

    Update schedule test

--- mycollab-scheduler-ondemand/src/test/java/com/esofthead/mycollab/schedule/email/user/impl/BillingSendingNotificationJobsTest.java
+++ mycollab-scheduler-ondemand/src/test/java/com/esofthead/mycollab/schedule/email/user/impl/BillingSendingNotificationJobsTest.java
@@ -1,19 +1,26 @@
 package com.esofthead.mycollab.schedule.email.user.impl;
 
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import static org.mockito.Mockito.when;
 import org.mockito.Spy;
 import org.quartz.JobExecutionException;
 
+import com.esofthead.mycollab.module.billing.AccountReminderStatusContants;
 import com.esofthead.mycollab.module.billing.service.BillingService;
+import com.esofthead.mycollab.module.user.domain.BillingAccount;
 import com.esofthead.mycollab.module.user.domain.BillingAccountWithOwners;
+import com.esofthead.mycollab.module.user.domain.BillingPlan;
 import com.esofthead.mycollab.module.user.domain.SimpleUser;
 import com.esofthead.mycollab.schedule.email.GenericJobTest;
 
@@ -41,5 +48,86 @@ public class BillingSendingNotificationJobsTest extends GenericJobTest {
 		when(billingService.getTrialAccountsWithOwners()).thenReturn(accounts);
 
 		billingNotificationJob.executeJob(context);
+
+		ArgumentCaptor<BillingAccount> billingAccountArgument = ArgumentCaptor
+				.forClass(BillingAccount.class);
+
+		ArgumentCaptor<String> strArgument = ArgumentCaptor
+				.forClass(String.class);
+
+		verify(billingAccountService).updateWithSessionWithSelective(
+				billingAccountArgument.capture(), strArgument.capture());
+		Assert.assertEquals("", strArgument.getValue());
+		Assert.assertEquals(
+				AccountReminderStatusContants.REMIND_ACCOUNT_IS_ABOUT_END_1ST_TIME,
+				billingAccountArgument.getValue().getReminderstatus());
+	}
+
+	@Test
+	public void testSendEmailForAccountExceed25daysAndAfter29days()
+			throws JobExecutionException {
+		BillingAccountWithOwners account = new BillingAccountWithOwners();
+		account.setReminderstatus(AccountReminderStatusContants.REMIND_ACCOUNT_IS_ABOUT_END_1ST_TIME);
+		SimpleUser owner = new SimpleUser();
+		account.setOwners(Arrays.asList(owner));
+
+		GregorianCalendar currentTime = new GregorianCalendar();
+		currentTime.add(Calendar.DATE, -30);
+		account.setCreatedtime(currentTime.getTime());
+
+		List<BillingAccountWithOwners> accounts = Arrays.asList(account);
+		when(billingService.getTrialAccountsWithOwners()).thenReturn(accounts);
+
+		billingNotificationJob.executeJob(context);
+
+		ArgumentCaptor<BillingAccount> billingAccountArgument = ArgumentCaptor
+				.forClass(BillingAccount.class);
+
+		ArgumentCaptor<String> strArgument = ArgumentCaptor
+				.forClass(String.class);
+
+		verify(billingAccountService).updateWithSessionWithSelective(
+				billingAccountArgument.capture(), strArgument.capture());
+		Assert.assertEquals("", strArgument.getValue());
+		Assert.assertEquals(
+				AccountReminderStatusContants.REMIND_ACCOUNT_IS_ABOUT_END_2ST_TIME,
+				billingAccountArgument.getValue().getReminderstatus());
+	}
+
+	@Test
+	public void testAccountIsExpireAndConvertToFreePlan()
+			throws JobExecutionException {
+		BillingAccountWithOwners account = new BillingAccountWithOwners();
+		account.setReminderstatus(AccountReminderStatusContants.REMIND_ACCOUNT_IS_ABOUT_END_2ST_TIME);
+		SimpleUser owner = new SimpleUser();
+		account.setOwners(Arrays.asList(owner));
+
+		GregorianCalendar currentTime = new GregorianCalendar();
+		currentTime.add(Calendar.DATE, -35);
+		account.setCreatedtime(currentTime.getTime());
+
+		List<BillingAccountWithOwners> accounts = Arrays.asList(account);
+		when(billingService.getTrialAccountsWithOwners()).thenReturn(accounts);
+
+		BillingPlan freePlan = new BillingPlan();
+		freePlan.setId(1);
+		when(billingService.getFreeBillingPlan()).thenReturn(freePlan);
+
+		billingNotificationJob.executeJob(context);
+
+		ArgumentCaptor<BillingAccount> billingAccountArgument = ArgumentCaptor
+				.forClass(BillingAccount.class);
+
+		ArgumentCaptor<String> strArgument = ArgumentCaptor
+				.forClass(String.class);
+
+		verify(billingAccountService).updateWithSessionWithSelective(
+				billingAccountArgument.capture(), strArgument.capture());
+		Assert.assertEquals("", strArgument.getValue());
+		Assert.assertEquals(
+				AccountReminderStatusContants.REMIND_ACCOUNT_IS_CONVERTED_TO_FREE_PLAN,
+				billingAccountArgument.getValue().getReminderstatus());
+		Assert.assertEquals(new Integer(1), billingAccountArgument.getValue()
+				.getBillingplanid());
 	}
 }
