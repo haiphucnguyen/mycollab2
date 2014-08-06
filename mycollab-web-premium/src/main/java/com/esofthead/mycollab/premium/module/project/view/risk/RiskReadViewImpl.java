2a214eaeccfe66445252fef19c968a6aa3ca774f
Hai Nguyen <hainguyen@esofthead.com>
2014-08-06 11:32:26 +0700

    Update component and version views

--- mycollab-web-premium/src/main/java/com/esofthead/mycollab/premium/module/project/view/risk/RiskReadViewImpl.java
+++ mycollab-web-premium/src/main/java/com/esofthead/mycollab/premium/module/project/view/risk/RiskReadViewImpl.java
@@ -1,18 +1,24 @@
 package com.esofthead.mycollab.premium.module.project.view.risk;
 
+import org.apache.commons.beanutils.PropertyUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.vaadin.teemu.ratingstars.RatingStars;
 
 import com.esofthead.mycollab.common.CommentType;
 import com.esofthead.mycollab.common.ModuleNameConstants;
 import com.esofthead.mycollab.common.i18n.OptionI18nEnum.StatusI18nEnum;
+import com.esofthead.mycollab.core.arguments.ValuedBean;
+import com.esofthead.mycollab.core.utils.BeanUtility;
 import com.esofthead.mycollab.module.project.CurrentProjectVariables;
 import com.esofthead.mycollab.module.project.ProjectRolePermissionCollections;
 import com.esofthead.mycollab.module.project.ProjectTypeConstants;
 import com.esofthead.mycollab.module.project.domain.SimpleRisk;
 import com.esofthead.mycollab.module.project.i18n.ProjectCommonI18nEnum;
 import com.esofthead.mycollab.module.project.i18n.RiskI18nEnum;
-import com.esofthead.mycollab.module.project.ui.components.AbstractPreviewItemComp;
+import com.esofthead.mycollab.module.project.ui.components.AbstractPreviewItemComp2;
 import com.esofthead.mycollab.module.project.ui.components.CommentDisplay;
+import com.esofthead.mycollab.module.project.ui.components.DateInfoComp;
 import com.esofthead.mycollab.module.project.view.settings.component.ProjectUserFormLinkField;
 import com.esofthead.mycollab.schedule.email.project.ProjectRiskRelayEmailNotificationAction;
 import com.esofthead.mycollab.vaadin.AppContext;
@@ -29,8 +35,13 @@ import com.esofthead.mycollab.vaadin.ui.MyCollabResource;
 import com.esofthead.mycollab.vaadin.ui.ProjectPreviewFormControlsGenerator;
 import com.esofthead.mycollab.vaadin.ui.TabsheetLazyLoadComp;
 import com.esofthead.mycollab.vaadin.ui.UIConstants;
+import com.esofthead.mycollab.vaadin.ui.UserLink;
+import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.Field;
+import com.vaadin.ui.GridLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.VerticalLayout;
 
 /**
  * 
@@ -39,13 +50,19 @@ import com.vaadin.ui.Field;
  * 
  */
 @ViewComponent
-public class RiskReadViewImpl extends AbstractPreviewItemComp<SimpleRisk>
+public class RiskReadViewImpl extends AbstractPreviewItemComp2<SimpleRisk>
 		implements RiskReadView {
 
 	private static final long serialVersionUID = 1L;
 
+	private static Logger log = LoggerFactory.getLogger(RiskReadViewImpl.class);
+
 	private CommentDisplay commentDisplay;
 	private RiskHistoryList historyList;
+
+	private DateInfoComp dateInfoComp;
+
+	private PeopleInfoComp peopleInfoComp;
 
 	public RiskReadViewImpl() {
 		super(AppContext.getMessage(RiskI18nEnum.FORM_READ_TITLE),
@@ -79,6 +96,12 @@ public class RiskReadViewImpl extends AbstractPreviewItemComp<SimpleRisk>
 
 		historyList = new RiskHistoryList(ModuleNameConstants.PRJ,
 				ProjectTypeConstants.RISK);
+
+		dateInfoComp = new DateInfoComp();
+		addToSideBar(dateInfoComp);
+
+		peopleInfoComp = new PeopleInfoComp();
+		addToSideBar(peopleInfoComp);
 	}
 
 	@Override
@@ -89,6 +112,9 @@ public class RiskReadViewImpl extends AbstractPreviewItemComp<SimpleRisk>
 
 		commentDisplay.loadComments(beanItem.getId());
 		historyList.loadHistory(beanItem.getId());
+
+		dateInfoComp.displayEntryDateTime(beanItem);
+		peopleInfoComp.displayEntryPeople(beanItem);
 	}
 
 	@Override
@@ -167,5 +193,67 @@ public class RiskReadViewImpl extends AbstractPreviewItemComp<SimpleRisk>
 	@Override
 	public HasPreviewFormHandlers<SimpleRisk> getPreviewFormHandlers() {
 		return previewForm;
+	}
+
+	private class PeopleInfoComp extends VerticalLayout {
+		private static final long serialVersionUID = 1L;
+
+		public void displayEntryPeople(ValuedBean bean) {
+			this.removeAllComponents();
+			this.setSpacing(true);
+			this.setMargin(new MarginInfo(false, false, false, true));
+
+			Label peopleInfoHeader = new Label(
+					AppContext
+							.getMessage(ProjectCommonI18nEnum.SUB_INFO_PEOPLE));
+			peopleInfoHeader.setStyleName("info-hdr");
+			this.addComponent(peopleInfoHeader);
+
+			GridLayout layout = new GridLayout(2, 2);
+			layout.setSpacing(true);
+			layout.setWidth("100%");
+			layout.setMargin(new MarginInfo(false, false, false, true));
+			try {
+				Label createdLbl = new Label(
+						AppContext
+								.getMessage(ProjectCommonI18nEnum.ITEM_CREATED_PEOPLE));
+				createdLbl.setSizeUndefined();
+				layout.addComponent(createdLbl, 0, 0);
+
+				String createdUserName = (String) PropertyUtils.getProperty(
+						bean, "raisedbyuser");
+				String createdUserAvatarId = (String) PropertyUtils
+						.getProperty(bean, "raisedByUserAvatarId");
+				String createdUserDisplayName = (String) PropertyUtils
+						.getProperty(bean, "raisedByUserFullName");
+
+				UserLink createdUserLink = new UserLink(createdUserName,
+						createdUserAvatarId, createdUserDisplayName);
+				layout.addComponent(createdUserLink, 1, 0);
+				layout.setColumnExpandRatio(1, 1.0f);
+
+				Label assigneeLbl = new Label(
+						AppContext
+								.getMessage(ProjectCommonI18nEnum.ITEM_ASSIGN_PEOPLE));
+				assigneeLbl.setSizeUndefined();
+				layout.addComponent(assigneeLbl, 0, 1);
+				String assignUserName = (String) PropertyUtils.getProperty(
+						bean, "assigntouser");
+				String assignUserAvatarId = (String) PropertyUtils.getProperty(
+						bean, "assignToUserAvatarId");
+				String assignUserDisplayName = (String) PropertyUtils
+						.getProperty(bean, "assignedToUserFullName");
+
+				UserLink assignUserLink = new UserLink(assignUserName,
+						assignUserAvatarId, assignUserDisplayName);
+				layout.addComponent(assignUserLink, 1, 1);
+			} catch (Exception e) {
+				log.error("Can not build user link {} ",
+						BeanUtility.printBeanObj(bean));
+			}
+
+			this.addComponent(layout);
+
+		}
 	}
 }
