645811e8a50a9c4534506d2e2c80c94d6731cd3e
Nghi Tran <nghitran@esofthead.com>
2014-08-11 17:11:39 +0700

    add ProjectNavigationMenu to mobile's Project module

--- mycollab-mobile/src/main/java/com/esofthead/mycollab/mobile/module/project/view/ProjectModuleController.java
+++ mycollab-mobile/src/main/java/com/esofthead/mycollab/mobile/module/project/view/ProjectModuleController.java
@@ -1,9 +1,12 @@
 package com.esofthead.mycollab.mobile.module.project.view;
 
+import com.esofthead.mycollab.eventmanager.ApplicationEventListener;
 import com.esofthead.mycollab.eventmanager.EventBusFactory;
+import com.esofthead.mycollab.mobile.module.project.events.ProjectEvent;
 import com.esofthead.mycollab.vaadin.mvp.IController;
 import com.esofthead.vaadin.mobilecomponent.MobileNavigationManager;
 import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
 
 /**
  * @author MyCollab Inc.
@@ -21,6 +24,21 @@ public class ProjectModuleController implements IController {
 	public ProjectModuleController(MobileNavigationManager navigationManager) {
 		this.navManager = navigationManager;
 		this.eventBus = EventBusFactory.getInstance();
+
+		bindProjectEvents();
+	}
+
+	private void bindProjectEvents() {
+		eventBus.register(new ApplicationEventListener<ProjectEvent.GotoDashboard>() {
+
+			private static final long serialVersionUID = -9006615798118115613L;
+
+			@Subscribe
+			@Override
+			public void handle(ProjectEvent.GotoDashboard event) {
+
+			}
+		});
 	}
 
 }
