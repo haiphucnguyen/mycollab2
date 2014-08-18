ee2843555a3955f723a8178fbc6c650fef43b0a6
Hai Nguyen <hainguyen@esofthead.com>
2014-08-18 21:51:26 +0700

    Update project list

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/user/MyProjectListComponent.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/user/MyProjectListComponent.java
@@ -16,8 +16,6 @@
  */
 package com.esofthead.mycollab.module.project.view.user;
 
-import java.util.List;
-
 import org.vaadin.hene.popupbutton.PopupButton;
 
 import com.esofthead.mycollab.common.i18n.OptionI18nEnum.StatusI18nEnum;
@@ -50,6 +48,7 @@ import com.esofthead.mycollab.vaadin.ui.UIConstants;
 import com.vaadin.ui.Alignment;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.Component;
 import com.vaadin.ui.CssLayout;
 import com.vaadin.ui.HorizontalLayout;
@@ -66,14 +65,10 @@ import com.vaadin.ui.VerticalLayout;
 public class MyProjectListComponent extends Depot {
 	private static final long serialVersionUID = 1L;
 
-	private List<Integer> prjKeys;
-
 	private ProjectPagedList projectList;
 
 	public MyProjectListComponent() {
-		super(AppContext
-				.getMessage(ProjectCommonI18nEnum.WIDGET_MY_PROJECTS_TITLE),
-				null, new VerticalLayout(), "565", "200");
+		super("", null, new VerticalLayout(), "565", "200");
 
 		this.projectList = new ProjectPagedList();
 		this.addStyleName("activity-panel");
@@ -92,35 +87,99 @@ public class MyProjectListComponent extends Depot {
 
 		Button allProjectsBtn = new Button(
 				AppContext
-						.getMessage(ProjectCommonI18nEnum.BUTTON_ALL_PROJECTS));
+						.getMessage(ProjectCommonI18nEnum.BUTTON_ALL_PROJECTS),
+				new ClickListener() {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public void buttonClick(ClickEvent event) {
+						displayAllProjects();
+						projectsPopup.setPopupVisible(false);
+					}
+				});
 		allProjectsBtn.setStyleName("link");
 		filterBtnLayout.addComponent(allProjectsBtn);
 
-		Button archiveProjectsBtn = new Button("Archive Projects");
-		archiveProjectsBtn.setStyleName("link");
-		filterBtnLayout.addComponent(archiveProjectsBtn);
+		Button activeProjectsBtn = new Button(
+				AppContext
+						.getMessage(ProjectCommonI18nEnum.BUTTON_ACTIVE_PROJECTS),
+				new ClickListener() {
+					private static final long serialVersionUID = 1L;
 
-		Button activeProjectsBtn = new Button("Active Projects");
+					@Override
+					public void buttonClick(ClickEvent event) {
+						displayActiveProjects();
+						projectsPopup.setPopupVisible(false);
+					}
+				});
 		activeProjectsBtn.setStyleName("link");
 		filterBtnLayout.addComponent(activeProjectsBtn);
 
+		Button archiveProjectsBtn = new Button(
+				AppContext
+						.getMessage(ProjectCommonI18nEnum.BUTTON_ARCHIVE_PROJECTS),
+				new ClickListener() {
+					private static final long serialVersionUID = 1L;
+
+					@Override
+					public void buttonClick(ClickEvent event) {
+						displayArchiveProjects();
+						projectsPopup.setPopupVisible(false);
+					}
+				});
+		archiveProjectsBtn.setStyleName("link");
+		filterBtnLayout.addComponent(archiveProjectsBtn);
+
 		projectsPopup.setContent(filterBtnLayout);
 		this.addHeaderElement(projectsPopup);
+
+		this.bodyContent.addComponent(this.projectList);
 	}
 
-	public void showActiveProjects(final List<Integer> prjKeys) {
-		this.prjKeys = prjKeys;
-		this.bodyContent.removeAllComponents();
-		this.bodyContent.addComponent(this.projectList);
+	public void displayDefaultProjectsList() {
+		displayActiveProjects();
+	}
+
+	private void displayAllProjects() {
+		final ProjectSearchCriteria searchCriteria = new ProjectSearchCriteria();
+		searchCriteria.setInvolvedMember(new StringSearchField(SearchField.AND,
+				AppContext.getUsername()));
+		this.projectList.setSearchCriteria(searchCriteria);
+
+		int totalCount = this.projectList.getTotalCount(searchCriteria);
+		this.setTitle(AppContext.getMessage(
+				ProjectCommonI18nEnum.WIDGET_ACTIVE_PROJECTS_TITLE, totalCount));
+	}
+
+	private void displayActiveProjects() {
 		final ProjectSearchCriteria searchCriteria = new ProjectSearchCriteria();
 		searchCriteria.setInvolvedMember(new StringSearchField(SearchField.AND,
 				AppContext.getUsername()));
 		searchCriteria.setProjectStatuses(new SetSearchField<String>(
 				new String[] { StatusI18nEnum.Open.name() }));
 		this.projectList.setSearchCriteria(searchCriteria);
+
+		int totalCount = this.projectList.getTotalCount(searchCriteria);
+		this.setTitle(AppContext.getMessage(
+				ProjectCommonI18nEnum.WIDGET_ACTIVE_PROJECTS_TITLE, totalCount));
 	}
 
-	public static class ProjectPagedList extends
+	private void displayArchiveProjects() {
+		final ProjectSearchCriteria searchCriteria = new ProjectSearchCriteria();
+		searchCriteria.setInvolvedMember(new StringSearchField(SearchField.AND,
+				AppContext.getUsername()));
+		searchCriteria.setProjectStatuses(new SetSearchField<String>(
+				new String[] { StatusI18nEnum.Archived.name() }));
+		this.projectList.setSearchCriteria(searchCriteria);
+
+		int totalCount = this.projectList.getTotalCount(searchCriteria);
+		this.setTitle(AppContext
+				.getMessage(
+						ProjectCommonI18nEnum.WIDGET_ARCHIVE_PROJECTS_TITLE,
+						totalCount));
+	}
+
+	static class ProjectPagedList extends
 			BeanList<ProjectService, ProjectSearchCriteria, SimpleProject> {
 		private static final long serialVersionUID = 1L;
 
