968c3d378648448fe4dc1107809d80f21dd5adba
Hai Nguyen <hainguyen@esofthead.com>
2014-10-07 03:56:18 +0700

    Update unit test

--- mycollab-api-impl/src/test/java/com/esofthead/mycollab/rest/server/resource/AccountResourceTest.java
+++ mycollab-api-impl/src/test/java/com/esofthead/mycollab/rest/server/resource/AccountResourceTest.java
@@ -1,27 +1,40 @@
 package com.esofthead.mycollab.rest.server.resource;
 
-import static com.jayway.restassured.RestAssured.get;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import com.esofthead.mycollab.configuration.SiteConfiguration;
 import com.esofthead.mycollab.test.DataSet;
 import com.esofthead.mycollab.test.WebServer;
+import com.jayway.restassured.http.ContentType;
 
 public class AccountResourceTest extends RestServiceTest {
 
-	@BeforeClass
-	public static void setUp() {
-		SiteConfiguration.loadInstance(8080);
+	@WebServer
+	@DataSet
+	@Test
+	public void testSignupSuccessfully() {
+		given().contentType(ContentType.URLENC).formParam("subdomain", "xyz")
+				.formParam("planId", "1").formParam("password", "1a2b3c4d5e")
+				.formParam("email", "baohan@esofthead.com")
+				.formParam("timezoneId", "3")
+				.formParam("isEmailVerified", false).when()
+				.post("account/signup").then().assertThat().statusCode(200)
+				.and().header("Access-Control-Allow-Origin", "*").and()
+				.body(equalTo("http://localhost:8080/mycollab-web/"));
 	}
 
 	@WebServer
 	@DataSet
 	@Test
-	public void testSignup() {
-		System.out.println("A");
-		get("/account/signup").then().assertThat().statusCode(200);
-		System.out.println("B");
+	public void testSignupWithExistingDomain() {
+		given().contentType(ContentType.URLENC).formParam("subdomain", "abc")
+				.formParam("planId", "1").formParam("password", "1a2b3c4d5e")
+				.formParam("email", "baohan@esofthead.com")
+				.formParam("timezoneId", "3")
+				.formParam("isEmailVerified", false).when()
+				.post("account/signup").then().assertThat().statusCode(400)
+				.and().header("Access-Control-Allow-Origin", "*");
 	}
 }
