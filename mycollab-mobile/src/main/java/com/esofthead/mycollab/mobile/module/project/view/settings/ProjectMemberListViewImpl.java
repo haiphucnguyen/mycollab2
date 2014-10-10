63f03ea60db1a968c998fa09b7f604a47207dcda
Nghi Tran <nghitran@esofthead.com>
2014-10-10 17:16:06 +0700

    update ProjectMemberListViewImpl

--- mycollab-mobile/src/main/java/com/esofthead/mycollab/mobile/module/project/view/settings/ProjectMemberListViewImpl.java
+++ mycollab-mobile/src/main/java/com/esofthead/mycollab/mobile/module/project/view/settings/ProjectMemberListViewImpl.java
@@ -1,8 +1,10 @@
 package com.esofthead.mycollab.mobile.module.project.view.settings;
 
 import com.esofthead.mycollab.mobile.module.project.ui.AbstractListViewComp;
+import com.esofthead.mycollab.mobile.ui.AbstractPagedBeanList;
 import com.esofthead.mycollab.module.project.domain.SimpleProjectMember;
 import com.esofthead.mycollab.module.project.domain.criteria.ProjectMemberSearchCriteria;
+import com.vaadin.ui.Component;
 
 /**
  * @author MyCollab Ltd.
@@ -12,5 +14,18 @@ import com.esofthead.mycollab.module.project.domain.criteria.ProjectMemberSearch
 public class ProjectMemberListViewImpl extends
 		AbstractListViewComp<ProjectMemberSearchCriteria, SimpleProjectMember>
 		implements ProjectMemberListView {
+
+	private static final long serialVersionUID = 3008732621100597514L;
+
+	@Override
+	protected AbstractPagedBeanList<ProjectMemberSearchCriteria, SimpleProjectMember> createBeanTable() {
+		return new ProjectMemberListDisplay();
+	}
+
+	@Override
+	protected Component createRightComponent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
