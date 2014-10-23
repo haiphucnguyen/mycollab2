eeae3ff4735737b5dc0577738124be85345091c0
Hai Nguyen <hainguyen@esofthead.com>
2014-10-23 23:46:43 +0700

    Update

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/ReOpenWindow.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/bug/ReOpenWindow.java
@@ -52,7 +52,6 @@ import com.vaadin.ui.Button;
 import com.vaadin.ui.ComponentContainer;
 import com.vaadin.ui.Field;
 import com.vaadin.ui.HorizontalLayout;
-import com.vaadin.ui.RichTextArea;
 import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 
