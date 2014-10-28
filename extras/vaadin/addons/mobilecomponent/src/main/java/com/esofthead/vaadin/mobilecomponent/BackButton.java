be9297990bb9c41887ebdc488c8d46b59b180f88
Nghi Tran <nghitran@esofthead.com>
2014-10-28 14:03:00 +0700

    add BackButton to mobilecomponent addon

---
+++ extras/vaadin/addons/mobilecomponent/src/main/java/com/esofthead/vaadin/mobilecomponent/BackButton.java
@@ -0,0 +1,8 @@
+package com.esofthead.vaadin.mobilecomponent;
+
+import com.vaadin.ui.AbstractComponent;
+
+public class BackButton extends AbstractComponent {
+	private static final long serialVersionUID = -1199963133954837786L;
+
+}
