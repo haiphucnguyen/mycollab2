7e320c3a6f59fe261f7c1076c94fa4a15a664e80
Nghiem Le <nghiemle@bitbucket.com>
2014-10-30 18:18:34 +0700

    Update

--- mycollab-dao/src/main/java/com/esofthead/mycollab/core/persistence/service/DefaultSearchService.java
+++ mycollab-dao/src/main/java/com/esofthead/mycollab/core/persistence/service/DefaultSearchService.java
@@ -51,10 +51,9 @@ public abstract class DefaultSearchService<S extends SearchCriteria> implements
 	}
 
 	@Override
-	public List findAbsoluteListByCriteria(SearchRequest<S> searchRequest,
-			int firstIndex, int numberOftems) {
-		return getSearchMapper().findPagableListByCriteria(
-				searchRequest.getSearchCriteria(),
+	public List findAbsoluteListByCriteria(S searchCriteria, int firstIndex,
+			int numberOftems) {
+		return getSearchMapper().findPagableListByCriteria(searchCriteria,
 				new RowBounds(firstIndex, numberOftems));
 	}
 
