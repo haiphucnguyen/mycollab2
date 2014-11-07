1a060a4360905452bd417a7142b24bb3cb292869
Nghi Tran <nghitran@esofthead.com>
2014-11-07 14:17:30 +0700

    add AttachmentPanel to MessageListView

---
+++ mycollab-mobile/src/main/java/com/esofthead/mycollab/mobile/ui/IMobileView.java
@@ -0,0 +1,12 @@
+package com.esofthead.mycollab.mobile.ui;
+
+import com.vaadin.ui.Component;
+
+/**
+ * @author MyCollab Ltd.
+ *
+ * @since 4.5.2
+ */
+public interface IMobileView {
+	void setPreviousComponent(Component comp);
+}
