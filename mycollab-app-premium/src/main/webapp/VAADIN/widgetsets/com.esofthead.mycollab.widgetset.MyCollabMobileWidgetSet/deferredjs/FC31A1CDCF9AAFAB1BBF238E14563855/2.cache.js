dafc004ed88d92a674e442cd1cfdd8a2e67cc7b1
Hai Nguyen <hainguyen@esofthead.com>
2014-11-07 16:04:52 +0700

    Update van version

---
+++ mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet/deferredjs/FC31A1CDCF9AAFAB1BBF238E14563855/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_esofthead_mycollab_widgetset_MyCollabMobileWidgetSet.runAsyncCallback2('epb(1304,1,hTd);_.Ie=function myc(){Bkc((!ukc&&(ukc=new Hkc),ukc),this.b.e)};MKd(Tq)(2);\n//# sourceURL=com.esofthead.mycollab.widgetset.MyCollabMobileWidgetSet-2.js\n')
