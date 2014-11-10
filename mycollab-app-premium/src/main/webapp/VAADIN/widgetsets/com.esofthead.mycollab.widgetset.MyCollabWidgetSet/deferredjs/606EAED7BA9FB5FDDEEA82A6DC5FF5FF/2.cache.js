422b9d74de71026253bf9bd6b8834c2a38504cf9
Hai Nguyen <hainguyen@esofthead.com>
2014-11-10 10:04:11 +0700

    Update widget sets

--- mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabWidgetSet/deferredjs/606EAED7BA9FB5FDDEEA82A6DC5FF5FF/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_esofthead_mycollab_widgetset_MyCollabWidgetSet.runAsyncCallback2('vvb(2248,1,_ie);_.Ie=function uBc(){Jkc((!Ckc&&(Ckc=new Okc),Ckc),this.b.e)};Eae(rs)(2);\n//# sourceURL=com.esofthead.mycollab.widgetset.MyCollabWidgetSet-2.js\n')
