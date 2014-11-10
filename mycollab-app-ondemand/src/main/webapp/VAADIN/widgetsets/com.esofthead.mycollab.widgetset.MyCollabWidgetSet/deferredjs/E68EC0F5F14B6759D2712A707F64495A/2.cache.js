f3afafd4553be71d12ad8f01c2b88582927f19a2
Hai Nguyen <hainguyen@esofthead.com>
2014-11-10 10:08:59 +0700

    Update widgetset for on demand

---
+++ mycollab-app-ondemand/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabWidgetSet/deferredjs/E68EC0F5F14B6759D2712A707F64495A/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_esofthead_mycollab_widgetset_MyCollabWidgetSet.runAsyncCallback2('vvb(908,1,aje);_.Ie=function uBc(){Jkc((!Ckc&&(Ckc=new Okc),Ckc),this.b.e)};Eae(rs)(2);\n//# sourceURL=com.esofthead.mycollab.widgetset.MyCollabWidgetSet-2.js\n')
