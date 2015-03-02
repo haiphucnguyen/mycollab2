6fef0d4e77e1fc16732484cb741611900e54fb5e
Hai Nguyen <hainguyen@esofthead.com>
2015-03-02 11:11:52 +0700

    Update

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/user/accountsettings/team/view/RoleSearchPanel.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/user/accountsettings/team/view/RoleSearchPanel.java
@@ -27,132 +27,131 @@ import com.esofthead.mycollab.module.user.domain.criteria.RoleSearchCriteria;
 import com.esofthead.mycollab.module.user.events.RoleEvent;
 import com.esofthead.mycollab.security.RolePermissionCollections;
 import com.esofthead.mycollab.vaadin.AppContext;
+import com.esofthead.mycollab.vaadin.ui.DefaultGenericSearchPanel;
 import com.esofthead.mycollab.vaadin.ui.GenericSearchPanel;
-import com.esofthead.mycollab.vaadin.ui.MyCollabResource;
+import com.esofthead.mycollab.vaadin.ui.HeaderWithFontAwesome;
 import com.esofthead.mycollab.vaadin.ui.UIConstants;
+import com.vaadin.event.ShortcutAction;
+import com.vaadin.event.ShortcutListener;
 import com.vaadin.server.FontAwesome;
-import com.vaadin.shared.ui.MarginInfo;
-import com.vaadin.shared.ui.label.ContentMode;
 import com.vaadin.ui.*;
 import org.apache.commons.lang3.StringUtils;
 import org.vaadin.maddon.layouts.MHorizontalLayout;
 
 /**
- * 
  * @author MyCollab Ltd.
  * @since 1.0
  */
-public class RoleSearchPanel extends GenericSearchPanel<RoleSearchCriteria> {
-	private static final long serialVersionUID = 1L;
-	private RoleSearchCriteria searchCriteria;
-
-	public RoleSearchPanel() {
-		this.setCompositionRoot(new RoleBasicSearchLayout());
-	}
-
-	private HorizontalLayout createSearchTopPanel() {
-		final MHorizontalLayout layout = new MHorizontalLayout()
-				.withStyleName(UIConstants.HEADER_VIEW).withWidth("100%")
-				.withMargin(new MarginInfo(true, false, true, false));
-		layout.setDefaultComponentAlignment(Alignment.MIDDLE_LEFT);
-
-		final Label searchTitleLbl = new Label(FontAwesome.USERS.getHtml() + " " +
-				AppContext.getMessage(RoleI18nEnum.VIEW_LIST_TITLE), ContentMode.HTML);
-		searchTitleLbl.setStyleName(UIConstants.HEADER_TEXT);
-		layout.with(searchTitleLbl).expand(searchTitleLbl);
-
-		final Button createBtn = new Button(
-				AppContext.getMessage(GenericI18Enum.BUTTON_CREATE),
-				new Button.ClickListener() {
-					private static final long serialVersionUID = 1L;
-
-					@Override
-					public void buttonClick(final Button.ClickEvent event) {
-						EventBusFactory.getInstance().post(
-								new RoleEvent.GotoAdd(this, null));
-					}
-				});
-		createBtn.setStyleName(UIConstants.THEME_GREEN_LINK);
-		createBtn.setIcon(FontAwesome.PLUS);
-		createBtn.setEnabled(AppContext
-				.canWrite(RolePermissionCollections.ACCOUNT_ROLE));
-
-		layout.with(createBtn).withAlign(createBtn, Alignment.MIDDLE_RIGHT);
-
-		return layout;
-	}
-
-	@SuppressWarnings("rawtypes")
-	private class RoleBasicSearchLayout extends
-			GenericSearchPanel.BasicSearchLayout {
-
-		@SuppressWarnings("unchecked")
-		public RoleBasicSearchLayout() {
-			super(RoleSearchPanel.this);
-		}
-
-		private static final long serialVersionUID = 1L;
-		private TextField nameField;
-
-		@Override
-		public ComponentContainer constructHeader() {
-			return RoleSearchPanel.this.createSearchTopPanel();
-		}
-
-		@Override
-		public ComponentContainer constructBody() {
-			final MHorizontalLayout basicSearchBody = new MHorizontalLayout().withMargin(true).with(new Label("Name"));
-
-			this.nameField = new TextField();
-			this.nameField.setWidth(UIConstants.DEFAULT_CONTROL_WIDTH);
-			basicSearchBody.addComponent(this.nameField);
-
-			final Button searchBtn = new Button(
-					AppContext.getMessage(GenericI18Enum.BUTTON_SEARCH));
-			searchBtn.setIcon(FontAwesome.SEARCH);
-			searchBtn.setStyleName(UIConstants.THEME_GREEN_LINK);
-			searchBtn.addClickListener(new Button.ClickListener() {
-				private static final long serialVersionUID = 1L;
-
-				@Override
-				public void buttonClick(final Button.ClickEvent event) {
-					RoleBasicSearchLayout.this.callSearchAction();
-				}
-			});
-			basicSearchBody.addComponent(searchBtn);
-
-			final Button clearBtn = new Button(
-					AppContext.getMessage(GenericI18Enum.BUTTON_CLEAR),
-					new Button.ClickListener() {
-						private static final long serialVersionUID = 1L;
-
-						@Override
-						public void buttonClick(final Button.ClickEvent event) {
-							RoleBasicSearchLayout.this.nameField.setValue("");
-						}
-					});
-			clearBtn.setStyleName(UIConstants.THEME_GRAY_LINK);
-			clearBtn.addStyleName("cancel-button");
-			basicSearchBody.addComponent(clearBtn);
-			basicSearchBody.setComponentAlignment(clearBtn,
-					Alignment.MIDDLE_LEFT);
-			return basicSearchBody;
-		}
-
-		@Override
-		protected SearchCriteria fillUpSearchCriteria() {
-			RoleSearchPanel.this.searchCriteria = new RoleSearchCriteria();
-			if (StringUtils.isNotBlank(this.nameField.getValue())) {
-				RoleSearchPanel.this.searchCriteria
-						.setRoleName(new StringSearchField(SearchField.AND,
-								this.nameField.getValue()));
-			}
-			return RoleSearchPanel.this.searchCriteria;
-		}
-	}
+public class RoleSearchPanel extends DefaultGenericSearchPanel<RoleSearchCriteria> {
+    private static final long serialVersionUID = 1L;
 
     @Override
-    public void setTotalCountNumber(int totalCountNumber) {
+    protected SearchLayout<RoleSearchCriteria> createBasicSearchLayout() {
+        return new RoleBasicSearchLayout();
+    }
 
+    @Override
+    protected SearchLayout<RoleSearchCriteria> createAdvancedSearchLayout() {
+        return null;
+    }
+
+    @Override
+    protected HeaderWithFontAwesome buildSearchTitle() {
+        return new HeaderWithFontAwesome(FontAwesome.USERS, AppContext.getMessage(RoleI18nEnum.VIEW_LIST_TITLE));
+    }
+
+    @Override
+    protected void buildExtraControls() {
+        final Button createBtn = new Button(
+                AppContext.getMessage(GenericI18Enum.BUTTON_CREATE),
+                new Button.ClickListener() {
+                    private static final long serialVersionUID = 1L;
+
+                    @Override
+                    public void buttonClick(final Button.ClickEvent event) {
+                        EventBusFactory.getInstance().post(
+                                new RoleEvent.GotoAdd(this, null));
+                    }
+                });
+        createBtn.setStyleName(UIConstants.THEME_GREEN_LINK);
+        createBtn.setIcon(FontAwesome.PLUS);
+        createBtn.setEnabled(AppContext
+                .canWrite(RolePermissionCollections.ACCOUNT_ROLE));
+        this.addHeaderRight(createBtn);
+    }
+
+    @SuppressWarnings("rawtypes")
+    private class RoleBasicSearchLayout extends
+            GenericSearchPanel.BasicSearchLayout {
+
+        @SuppressWarnings("unchecked")
+        public RoleBasicSearchLayout() {
+            super(RoleSearchPanel.this);
+        }
+
+        private static final long serialVersionUID = 1L;
+        private TextField nameField;
+
+        @Override
+        public ComponentContainer constructHeader() {
+            return RoleSearchPanel.this.constructHeader();
+        }
+
+        @Override
+        public ComponentContainer constructBody() {
+            final MHorizontalLayout basicSearchBody = new MHorizontalLayout().withMargin(true).with(new Label("Name"));
+
+            this.nameField = new TextField();
+            this.nameField.setWidth(UIConstants.DEFAULT_CONTROL_WIDTH);
+            this.nameField.addShortcutListener(new ShortcutListener("RoleSearchName", ShortcutAction.KeyCode.ENTER,
+                    null) {
+                @Override
+                public void handleAction(Object o, Object o1) {
+                    callSearchAction();
+                }
+            });
+            basicSearchBody.addComponent(this.nameField);
+
+            final Button searchBtn = new Button(
+                    AppContext.getMessage(GenericI18Enum.BUTTON_SEARCH));
+            searchBtn.setIcon(FontAwesome.SEARCH);
+            searchBtn.setStyleName(UIConstants.THEME_GREEN_LINK);
+            searchBtn.addClickListener(new Button.ClickListener() {
+                private static final long serialVersionUID = 1L;
+
+                @Override
+                public void buttonClick(final Button.ClickEvent event) {
+                    RoleBasicSearchLayout.this.callSearchAction();
+                }
+            });
+            basicSearchBody.addComponent(searchBtn);
+
+            final Button clearBtn = new Button(
+                    AppContext.getMessage(GenericI18Enum.BUTTON_CLEAR),
+                    new Button.ClickListener() {
+                        private static final long serialVersionUID = 1L;
+
+                        @Override
+                        public void buttonClick(final Button.ClickEvent event) {
+                            RoleBasicSearchLayout.this.nameField.setValue("");
+                        }
+                    });
+            clearBtn.setStyleName(UIConstants.THEME_GRAY_LINK);
+            clearBtn.addStyleName("cancel-button");
+            basicSearchBody.addComponent(clearBtn);
+            basicSearchBody.setComponentAlignment(clearBtn, Alignment.MIDDLE_LEFT);
+            return basicSearchBody;
+        }
+
+        @Override
+        protected SearchCriteria fillUpSearchCriteria() {
+            RoleSearchCriteria searchCriteria = new RoleSearchCriteria();
+            if (StringUtils.isNotBlank(this.nameField.getValue())) {
+                searchCriteria
+                        .setRoleName(new StringSearchField(SearchField.AND,
+                                this.nameField.getValue()));
+            }
+            return searchCriteria;
+        }
     }
 }
