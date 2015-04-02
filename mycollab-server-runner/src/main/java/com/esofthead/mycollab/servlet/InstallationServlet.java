1502d28f091b1a67e9d07acf5f05eef00963858d
Hai Nguyen <hainguyen@esofthead.com>
2015-04-02 11:25:07 +0700

    Update

--- mycollab-server-runner/src/main/java/com/esofthead/mycollab/servlet/InstallationServlet.java
+++ mycollab-server-runner/src/main/java/com/esofthead/mycollab/servlet/InstallationServlet.java
@@ -1,16 +1,16 @@
 /**
  * This file is part of mycollab-server-runner.
- * <p>
+ *
  * mycollab-server-runner is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * <p>
+ *
  * mycollab-server-runner is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with mycollab-server-runner.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -78,6 +78,7 @@ public class InstallationServlet extends HttpServlet {
         } catch (Exception e) {
             PrintWriter out = response.getWriter();
             out.write("Cannot establish connection to database. Make sure your inputs are correct.");
+            LOG.error("Can not connect database", e);
             return;
         }
 
@@ -93,9 +94,10 @@ public class InstallationServlet extends HttpServlet {
                 smtpPassword,
                 true, enctype);
         if (!validSMTPAccount) {
-            PrintWriter out = response.getWriter();
-            out.write("Cannot authenticate mail server successfully. Make sure your inputs are correct.");
-            return;
+//            PrintWriter out = response.getWriter();
+//            out.write("Cannot authenticate mail server successfully. Make sure your inputs are correct.");
+//            return;
+            LOG.warn("Cannot authenticate mail server successfully. Make sure your inputs are correct.");
         }
 
         templateContext.put("smtpAddress", smtpHost);
