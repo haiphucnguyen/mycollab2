a9efd2e7ec59fc649c570af423b45cf2757ed92d
hainguyen <hainguyen@esofthead.com>
2015-04-09 15:09:56 +0700

    Update

--- mycollab-web/src/main/java/com/esofthead/mycollab/shell/view/UpgradeConfirmWindow.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/shell/view/UpgradeConfirmWindow.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of mycollab-web.
+ *
+ * mycollab-web is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mycollab-web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mycollab-web.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.esofthead.mycollab.shell.view;
 
 import com.esofthead.mycollab.configuration.SiteConfiguration;
@@ -125,6 +141,7 @@ public class UpgradeConfirmWindow extends Window {
 
                     progressWindow.close();
                     Page.getCurrent().setLocation(SiteConfiguration.getSiteUrl(AppContext.getSubDomain()) + "upgrade");
+                    UI.getCurrent().setPollInterval(-1);
                     ServerInstance.getInstance().upgrade(tmpFile);
                 } else {
                     NotificationUtil.showErrorNotification("Can not download the latest MyCollab distribution. You could try again or install MyCollab manually");
@@ -134,7 +151,9 @@ public class UpgradeConfirmWindow extends Window {
             } catch (IOException e) {
                 NotificationUtil.showErrorNotification("Can not download the latest MyCollab distribution. You could try again or install MyCollab manually");
             } finally {
-                UI.getCurrent().setPollInterval(-1);
+                if (UI.getCurrent() != null) {
+                    UI.getCurrent().setPollInterval(-1);
+                }
             }
 
         }
