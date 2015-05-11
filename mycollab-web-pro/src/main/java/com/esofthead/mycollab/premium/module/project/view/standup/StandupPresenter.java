f4b2ed7f77988d75d12ba6f646b1070148caac5b
hainguyen <hainguyen@esofthead.com>
2015-05-11 16:15:30 +0700

    Update

--- mycollab-web-pro/src/main/java/com/esofthead/mycollab/premium/module/project/view/standup/StandupPresenter.java
+++ mycollab-web-pro/src/main/java/com/esofthead/mycollab/premium/module/project/view/standup/StandupPresenter.java
@@ -6,7 +6,6 @@ import com.esofthead.mycollab.module.project.view.ProjectView;
 import com.esofthead.mycollab.module.project.view.parameters.StandupScreenData;
 import com.esofthead.mycollab.module.project.view.standup.IStandupContainer;
 import com.esofthead.mycollab.module.project.view.standup.IStandupPresenter;
-import com.esofthead.mycollab.vaadin.AppContext;
 import com.esofthead.mycollab.vaadin.mvp.PageActionChain;
 import com.esofthead.mycollab.vaadin.mvp.PresenterResolver;
 import com.esofthead.mycollab.vaadin.mvp.ScreenData;
@@ -14,60 +13,52 @@ import com.esofthead.mycollab.vaadin.ui.AbstractPresenter;
 import com.vaadin.ui.ComponentContainer;
 
 /**
- * 
  * @author MyCollab Ltd.
  * @since 1.0
- * 
  */
 public class StandupPresenter extends AbstractPresenter<IStandupContainer>
-		implements IStandupPresenter {
-	private static final long serialVersionUID = 1L;
+        implements IStandupPresenter {
+    private static final long serialVersionUID = 1L;
 
-	public StandupPresenter() {
-		super(IStandupContainer.class);
-	}
+    public StandupPresenter() {
+        super(IStandupContainer.class);
+    }
 
-	@Override
-	public void go(ComponentContainer container, ScreenData<?> data) {
-		super.go(container, data, false);
-	}
+    @Override
+    public void go(ComponentContainer container, ScreenData<?> data) {
+        super.go(container, data, false);
+    }
 
-	@Override
-	protected void onGo(ComponentContainer container, ScreenData<?> data) {
-		ProjectView projectViewContainer = (ProjectView) container;
-		projectViewContainer.gotoSubView(ProjectTypeConstants.STANDUP);
+    @Override
+    protected void onGo(ComponentContainer container, ScreenData<?> data) {
+        ProjectView projectViewContainer = (ProjectView) container;
+        projectViewContainer.gotoSubView(ProjectTypeConstants.STANDUP);
 
-		view.removeAllComponents();
+        view.removeAllComponents();
 
-		AbstractPresenter presenter;
+        AbstractPresenter presenter;
 
-		if (AppContext.isStandupComponentEnable()) {
-			if (data instanceof StandupScreenData.Search) {
-				presenter = PresenterResolver
-						.getPresenter(StandupListPresenter.class);
-			} else if (data instanceof StandupScreenData.Add
-					|| data instanceof StandupScreenData.Edit) {
-				presenter = PresenterResolver
-						.getPresenter(StandupAddPresenter.class);
-			} else if (data instanceof StandupScreenData.Read) {
-				presenter = PresenterResolver
-						.getPresenter(StandupReadPresenter.class);
-			} else {
-				throw new MyCollabException("Do not support screen data: "
-						+ data);
-			}
-		} else {
-			presenter = PresenterResolver
-					.getPresenter(StandupAdvertisementPresenter.class);
-		}
+        if (data instanceof StandupScreenData.Search) {
+            presenter = PresenterResolver
+                    .getPresenter(StandupListPresenter.class);
+        } else if (data instanceof StandupScreenData.Add
+                || data instanceof StandupScreenData.Edit) {
+            presenter = PresenterResolver
+                    .getPresenter(StandupAddPresenter.class);
+        } else if (data instanceof StandupScreenData.Read) {
+            presenter = PresenterResolver
+                    .getPresenter(StandupReadPresenter.class);
+        } else {
+            throw new MyCollabException(String.format("Do not support screen data: %s", data));
+        }
 
-		presenter.go(view, data);
-	}
+        presenter.go(view, data);
+    }
 
-	@Override
-	public void handleChain(ComponentContainer container,
-			PageActionChain pageActionChain) {
-		ScreenData pageAction = pageActionChain.pop();
-		onGo(container, pageAction);
-	}
+    @Override
+    public void handleChain(ComponentContainer container,
+                            PageActionChain pageActionChain) {
+        ScreenData pageAction = pageActionChain.pop();
+        onGo(container, pageAction);
+    }
 }
