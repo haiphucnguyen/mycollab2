9b23edaa4e0121b1255fdecf5bea8ac025d4b9fc
Hai Nguyen <hainguyen@esofthead.com>
2015-06-02 15:21:37 +0700

    Update

---
+++ mycollab-core/src/test/java/com/esofthead/mycollab/core/utils/FormatHtmlTest.java
@@ -0,0 +1,19 @@
+package com.esofthead.mycollab.core.utils;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author MyCollab Ltd
+ * @since 5.0.8
+ */
+public class FormatHtmlTest {
+    @Test
+    public void testFormatHtml() {
+        String mixTextAndHtml = StringUtils.formatRichText("Hello world https://community.mycollab.com <b>Hai Nguyen</b>");
+        Assert.assertEquals("Hello world <a href=\"https://community.mycollab.com\" target=\"_blank\">https://community.mycollab.com</a> <b>Hai Nguyen</b>", mixTextAndHtml);
+
+        String pureHtml = StringUtils.formatRichText("https://mycollab.com");
+        Assert.assertEquals("<a href=\"https://mycollab.com\" target=\"_blank\">https://mycollab.com</a>", pureHtml);
+    }
+}
