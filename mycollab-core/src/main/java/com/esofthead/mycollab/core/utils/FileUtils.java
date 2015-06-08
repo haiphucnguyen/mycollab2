382e82b0fbac1b38238348ee27507f55280619e7
Hai Nguyen <hainguyen@esofthead.com>
2015-06-09 00:05:54 +0700

    * User can upload non english file name document

--- mycollab-core/src/main/java/com/esofthead/mycollab/core/utils/FileUtils.java
+++ mycollab-core/src/main/java/com/esofthead/mycollab/core/utils/FileUtils.java
@@ -21,6 +21,7 @@ import com.esofthead.mycollab.core.MyCollabException;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
 
 /**
  * @author MyCollab Ltd.
@@ -81,5 +82,33 @@ public class FileUtils {
         } catch (IOException e) {
             throw new MyCollabException(e);
         }
+    }
+
+    private static final String[] INVALID_RESOURCE_BASENAMES = new String[] {"aux", "com1", "com2", "com3", "com4", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+            "com5", "com6", "com7", "com8", "com9", "con", "lpt1", "lpt2", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
+            "lpt3", "lpt4", "lpt5", "lpt6", "lpt7", "lpt8", "lpt9", "nul", "prn"}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$ //$NON-NLS-9$
+
+    private static final String[] INVALID_RESOURCE_FULLNAMES = new String[] {"clock$"}; //$NON-NLS-1$;
+
+    public static boolean isValidFileName(String name) {
+        if (name.equals(".") || name.equals("..")) //$NON-NLS-1$ //$NON-NLS-2$
+            return false;
+        //empty names are not valid
+        final int length = name.length();
+        if (length == 0)
+            return false;
+        final char lastChar = name.charAt(length-1);
+        // filenames ending in dot are not valid
+        if (lastChar == '.')
+            return false;
+        // file names ending with whitespace are truncated (bug 118997)
+        if (Character.isWhitespace(lastChar))
+            return false;
+        int dot = name.indexOf('.');
+        //on windows, filename suffixes are not relevant to name validity
+        String basename = dot == -1 ? name : name.substring(0, dot);
+        if (Arrays.binarySearch(INVALID_RESOURCE_BASENAMES, basename.toLowerCase()) >= 0)
+            return false;
+        return Arrays.binarySearch(INVALID_RESOURCE_FULLNAMES, name.toLowerCase()) < 0;
     }
 }
