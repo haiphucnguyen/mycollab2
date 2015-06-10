514897c066da30b81af7d387c01e7dd2f979ac48
Hai Nguyen <hainguyen@esofthead.com>
2015-06-10 14:50:04 +0700

    Update

--- mycollab-web/src/main/scala/com/esofthead/mycollab/vaadin/ui/DefaultMassItemActionHandlerContainer.scala
+++ mycollab-web/src/main/scala/com/esofthead/mycollab/vaadin/ui/DefaultMassItemActionHandlerContainer.scala
@@ -1,3 +1,19 @@
+/**
+ * This file is part of mycollab-web.
+ *
+ * mycollab-web is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mycollab-web is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mycollab-web.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.esofthead.mycollab.vaadin.ui
 
 import java.io.InputStream
@@ -19,6 +35,7 @@ import org.vaadin.peter.buttongroup.ButtonGroup
 class DefaultMassItemActionHandlerContainer extends MHorizontalLayout with HasMassItemActionHandler {
     private var actionHandler: MassItemActionHandler = null
     private val groupMap: scala.collection.mutable.Map[String, ButtonGroup] = scala.collection.mutable.Map()
+        .withDefaultValue(null)
 
     /**
      *
