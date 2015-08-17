3dd161192e555051064f7718068fb50c75460d9d
Hai Nguyen <hainguyen@esofthead.com>
2015-08-17 10:44:02 +0700

    Update

--- mycollab-web-premium/src/main/java/com/esofthead/mycollab/vaadin/MyCollabServlet.java
+++ mycollab-web-premium/src/main/java/com/esofthead/mycollab/vaadin/MyCollabServlet.java
@@ -16,7 +16,7 @@ import javax.servlet.annotation.WebServlet;
 @WebServlet(name = "MyCollabApplication", urlPatterns = "/*", asyncSupported = true, loadOnStartup = 0, initParams =
         {@WebInitParam(name = "closeIdleSessions", value = "true"),
                 @WebInitParam(name = "resourceCacheTime", value = "8640000"),
-                @WebInitParam(name = "pushmode", value = "manual")
+                @WebInitParam(name = "maxIdleTime", value = "10000")
         })
 public class MyCollabServlet extends TouchKitServlet {
     private static final long serialVersionUID = 1L;
