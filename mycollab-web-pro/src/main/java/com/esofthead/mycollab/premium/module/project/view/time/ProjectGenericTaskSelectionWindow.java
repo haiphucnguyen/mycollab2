2ee85694480690de6d49f06cb4437f47d58d20cc
Hai Nguyen <hainguyen@esofthead.com>
2015-09-22 14:37:40 +0700

    Update

--- mycollab-web-pro/src/main/java/com/esofthead/mycollab/premium/module/project/view/time/ProjectGenericTaskSelectionWindow.java
+++ mycollab-web-pro/src/main/java/com/esofthead/mycollab/premium/module/project/view/time/ProjectGenericTaskSelectionWindow.java
@@ -21,92 +21,91 @@ import org.vaadin.viritin.layouts.MVerticalLayout;
 import java.util.Arrays;
 
 /**
- * 
  * @author MyCollab Ltd.
  * @since 4.0
- * 
  */
 class ProjectGenericTaskSelectionWindow extends Window {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private GenericTaskTableDisplay taskTableDisplay;
-	private TextField nameField;
-	private ProjectGenericTaskSearchCriteria searchCriteria;
+    private GenericTaskTableDisplay assignmentTableDisplay;
+    private TextField nameField;
+    private ProjectGenericTaskSearchCriteria searchCriteria;
 
-	public ProjectGenericTaskSelectionWindow(final AssignmentSelectableComp timeEntryWindow) {
-		super("Select Assignments");
-		this.center();
-		this.setResizable(false);
-		this.setModal(true);
-		this.setWidth("800px");
-		MVerticalLayout content = new MVerticalLayout();
-		taskTableDisplay = new GenericTaskTableDisplay(Arrays.asList(GenericTaskTableFieldDef.name));
-		taskTableDisplay.addTableListener(new TableClickListener() {
-			private static final long serialVersionUID = 1L;
+    public ProjectGenericTaskSelectionWindow(final AssignmentSelectableComp timeEntryWindow) {
+        super("Select Assignments");
+        this.center();
+        this.setResizable(false);
+        this.setModal(true);
+        this.setWidth("800px");
+        MVerticalLayout content = new MVerticalLayout();
+        assignmentTableDisplay = new GenericTaskTableDisplay(Arrays.asList(GenericTaskTableFieldDef.name,
+                GenericTaskTableFieldDef.assignUser));
+        assignmentTableDisplay.setDisplayNumItems(10);
+        assignmentTableDisplay.addTableListener(new TableClickListener() {
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public void itemClick(final TableClickEvent event) {
-				final ProjectGenericTask task = (ProjectGenericTask) event.getData();
-				if ("name".equals(event.getFieldName())) {
-					timeEntryWindow.updateLinkTask(task);
-					ProjectGenericTaskSelectionWindow.this.close();
-				}
-			}
-		});
+            @Override
+            public void itemClick(final TableClickEvent event) {
+                final ProjectGenericTask task = (ProjectGenericTask) event.getData();
+                if ("name".equals(event.getFieldName())) {
+                    timeEntryWindow.updateLinkTask(task);
+                    ProjectGenericTaskSelectionWindow.this.close();
+                }
+            }
+        });
 
-		searchCriteria = new ProjectGenericTaskSearchCriteria();
-		searchCriteria.setProjectIds(new SetSearchField<>(CurrentProjectVariables.getProjectId()));
-		taskTableDisplay.setSearchCriteria(searchCriteria);
+        searchCriteria = new ProjectGenericTaskSearchCriteria();
+        searchCriteria.setProjectIds(new SetSearchField<>(CurrentProjectVariables.getProjectId()));
+        assignmentTableDisplay.setSearchCriteria(searchCriteria);
 
-		content.addComponent(constructTopPanel());
-		content.addComponent(taskTableDisplay);
+        content.addComponent(constructTopPanel());
+        content.addComponent(assignmentTableDisplay);
 
-		this.setContent(content);
-	}
+        this.setContent(content);
+    }
 
-	private ComponentContainer constructTopPanel() {
-		final MHorizontalLayout basicSearchBody = new MHorizontalLayout().withMargin(true);
+    private ComponentContainer constructTopPanel() {
+        final MHorizontalLayout basicSearchBody = new MHorizontalLayout().withMargin(true);
 
-		Label nameLbl = new Label("Name:");
-		basicSearchBody.with(nameLbl).withAlign(nameLbl, Alignment.MIDDLE_LEFT);
+        Label nameLbl = new Label("Name:");
+        basicSearchBody.with(nameLbl).withAlign(nameLbl, Alignment.MIDDLE_LEFT);
 
-		this.nameField = new TextField();
-		this.nameField.setWidth(UIConstants.DEFAULT_CONTROL_WIDTH);
-		basicSearchBody.with(nameField).withAlign(nameField, Alignment.MIDDLE_CENTER);
+        this.nameField = new TextField();
+        this.nameField.setWidth(UIConstants.DEFAULT_CONTROL_WIDTH);
+        basicSearchBody.with(nameField).withAlign(nameField, Alignment.MIDDLE_CENTER);
 
-		final Button searchBtn = new Button(AppContext.getMessage(GenericI18Enum.BUTTON_SEARCH));
-		searchBtn.setIcon(FontAwesome.SEARCH);
-		searchBtn.setStyleName(UIConstants.THEME_GREEN_LINK);
+        final Button searchBtn = new Button(AppContext.getMessage(GenericI18Enum.BUTTON_SEARCH));
+        searchBtn.setIcon(FontAwesome.SEARCH);
+        searchBtn.setStyleName(UIConstants.THEME_GREEN_LINK);
 
-		searchBtn.addClickListener(new Button.ClickListener() {
-			private static final long serialVersionUID = 1L;
+        searchBtn.addClickListener(new Button.ClickListener() {
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public void buttonClick(final ClickEvent event) {
-				callSearchAction();
-			}
-		});
-		searchBtn.setStyleName(UIConstants.THEME_GREEN_LINK);
-		basicSearchBody.with(searchBtn).withAlign(searchBtn,
-				Alignment.MIDDLE_LEFT);
+            @Override
+            public void buttonClick(final ClickEvent event) {
+                callSearchAction();
+            }
+        });
+        searchBtn.setStyleName(UIConstants.THEME_GREEN_LINK);
+        basicSearchBody.with(searchBtn).withAlign(searchBtn, Alignment.MIDDLE_LEFT);
 
-		final Button cancelBtn = new Button(AppContext.getMessage(GenericI18Enum.BUTTON_CLEAR));
-		cancelBtn.addClickListener(new Button.ClickListener() {
-			private static final long serialVersionUID = 1L;
+        final Button cancelBtn = new Button(AppContext.getMessage(GenericI18Enum.BUTTON_CLEAR));
+        cancelBtn.addClickListener(new Button.ClickListener() {
+            private static final long serialVersionUID = 1L;
 
-			@Override
-			public void buttonClick(final ClickEvent event) {
-				nameField.setValue("");
-			}
-		});
-		cancelBtn.setStyleName(UIConstants.THEME_GRAY_LINK);
-		basicSearchBody.addComponent(cancelBtn);
+            @Override
+            public void buttonClick(final ClickEvent event) {
+                nameField.setValue("");
+            }
+        });
+        cancelBtn.setStyleName(UIConstants.THEME_GRAY_LINK);
+        basicSearchBody.addComponent(cancelBtn);
 
-		return basicSearchBody;
-	}
+        return basicSearchBody;
+    }
 
-	private void callSearchAction() {
-		searchCriteria.setName(new StringSearchField(this.nameField.getValue().trim()));
-		taskTableDisplay.setSearchCriteria(searchCriteria);
-	}
+    private void callSearchAction() {
+        searchCriteria.setName(new StringSearchField(this.nameField.getValue().trim()));
+        assignmentTableDisplay.setSearchCriteria(searchCriteria);
+    }
 }
