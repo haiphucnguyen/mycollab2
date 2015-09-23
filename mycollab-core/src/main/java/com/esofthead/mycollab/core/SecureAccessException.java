075ba7e8ab67fbe06a27589caea65858554571db
Hai Nguyen <hainguyen@esofthead.com>
2015-09-23 20:20:45 +0700

    Update

---
+++ mycollab-core/src/main/java/com/esofthead/mycollab/core/SecureAccessException.java
@@ -0,0 +1,11 @@
+package com.esofthead.mycollab.core;
+
+/**
+ * @author MyCollab Ltd
+ * @since 5.1.3
+ */
+public class SecureAccessException extends MyCollabException {
+    public SecureAccessException(String message) {
+        super(message);
+    }
+}
