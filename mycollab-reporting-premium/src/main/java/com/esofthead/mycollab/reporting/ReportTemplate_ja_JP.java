ce9e2f7586b6a4c93f4ced555ab000f67c93f182
Hai Nguyen <hainguyen@esofthead.com>
2015-09-25 09:17:41 +0700

    Update

--- mycollab-reporting-premium/src/main/java/com/esofthead/mycollab/reporting/ReportTemplate_ja_JP.java
+++ mycollab-reporting-premium/src/main/java/com/esofthead/mycollab/reporting/ReportTemplate_ja_JP.java
@@ -16,98 +16,83 @@
  */
 package com.esofthead.mycollab.reporting;
 
-import static net.sf.dynamicreports.report.builder.DynamicReports.cmp;
-import static net.sf.dynamicreports.report.builder.DynamicReports.hyperLink;
-import static net.sf.dynamicreports.report.builder.DynamicReports.stl;
-import static net.sf.dynamicreports.report.builder.DynamicReports.template;
-
-import java.awt.Color;
-import java.util.Locale;
-
 import net.sf.dynamicreports.report.builder.HyperLinkBuilder;
 import net.sf.dynamicreports.report.builder.component.ComponentBuilder;
 import net.sf.dynamicreports.report.builder.style.PaddingBuilder;
 import net.sf.dynamicreports.report.builder.style.StyleBuilder;
-import net.sf.dynamicreports.report.constant.HorizontalAlignment;
-import net.sf.dynamicreports.report.constant.VerticalAlignment;
+import net.sf.dynamicreports.report.constant.HorizontalImageAlignment;
+import net.sf.dynamicreports.report.constant.HorizontalTextAlignment;
+import net.sf.dynamicreports.report.constant.VerticalTextAlignment;
+
+import java.awt.*;
+import java.util.Locale;
+
+import static net.sf.dynamicreports.report.builder.DynamicReports.*;
 
 /**
- * 
  * @author MyCollab Ltd.
  * @since 4.1.2
- * 
  */
 class ReportTemplate_ja_JP extends AbstractReportTemplate {
 
-	ReportTemplate_ja_JP() {
-		rootStyle = stl.style().setPadding(2).setFontName("Takao");
-		boldStyle = stl.style(rootStyle).bold();
-		italicStyle = stl.style(rootStyle).italic();
-		underlineStyle = stl.style(rootStyle).underline();
-		boldCenteredStyle = stl.style(boldStyle).setAlignment(
-				HorizontalAlignment.CENTER, VerticalAlignment.MIDDLE);
+    ReportTemplate_ja_JP() {
+        rootStyle = stl.style().setPadding(2).setFontName("Takao");
+        boldStyle = stl.style(rootStyle).bold();
+        italicStyle = stl.style(rootStyle).italic();
+        underlineStyle = stl.style(rootStyle).underline();
+        boldCenteredStyle = stl.style(boldStyle).setTextAlignment(HorizontalTextAlignment.CENTER, VerticalTextAlignment.MIDDLE);
 
-		PaddingBuilder padding = stl.padding();
-		padding.setLeft(8);
-		boldLeftStyle = stl
-				.style(boldStyle)
-				.setAlignment(HorizontalAlignment.LEFT,
-						VerticalAlignment.MIDDLE).setPadding(padding);
+        PaddingBuilder padding = stl.padding();
+        padding.setLeft(8);
+        boldLeftStyle = stl.style(boldStyle).setTextAlignment(HorizontalTextAlignment.LEFT, VerticalTextAlignment.MIDDLE)
+                .setPadding(padding);
 
-		bold12CenteredStyle = stl.style(boldCenteredStyle).setFontSize(12);
-		bold18CenteredStyle = stl.style(boldCenteredStyle).setFontSize(18);
-		bold22CenteredStyle = stl.style(boldCenteredStyle).setFontSize(22);
-		bold12TitleStyle = stl.style(boldLeftStyle).setFontSize(12);
-		bold18TitleStyle = stl.style(boldLeftStyle).setFontSize(18);
+        bold12CenteredStyle = stl.style(boldCenteredStyle).setFontSize(12);
+        bold18CenteredStyle = stl.style(boldCenteredStyle).setFontSize(18);
+        bold22CenteredStyle = stl.style(boldCenteredStyle).setFontSize(22);
+        bold12TitleStyle = stl.style(boldLeftStyle).setFontSize(12);
+        bold18TitleStyle = stl.style(boldLeftStyle).setFontSize(18);
 
-		columnStyle = stl.style(rootStyle).setVerticalAlignment(
-				VerticalAlignment.MIDDLE);
-		columnTitleStyle = stl.style(columnStyle).setBorder(stl.pen1Point())
-				.setHorizontalAlignment(HorizontalAlignment.CENTER)
-				.setBackgroundColor(Color.LIGHT_GRAY).bold();
-		groupStyle = stl.style(boldStyle).setHorizontalAlignment(
-				HorizontalAlignment.LEFT);
-		subtotalStyle = stl.style(boldStyle).setTopBorder(stl.pen1Point());
+        columnStyle = stl.style(rootStyle).setVerticalTextAlignment(VerticalTextAlignment.MIDDLE);
+        columnTitleStyle = stl.style(columnStyle).setBorder(stl.pen1Point())
+                .setHorizontalTextAlignment(HorizontalTextAlignment.LEFT)
+                .setBackgroundColor(Color.LIGHT_GRAY).bold();
+        groupStyle = stl.style(boldStyle).setHorizontalImageAlignment(HorizontalImageAlignment.LEFT);
+        subtotalStyle = stl.style(boldStyle).setTopBorder(stl.pen1Point());
 
-		StyleBuilder crosstabGroupStyle = stl.style(columnTitleStyle);
-		StyleBuilder crosstabGroupTotalStyle = stl.style(columnTitleStyle)
-				.setBackgroundColor(new Color(170, 170, 170));
-		StyleBuilder crosstabGrandTotalStyle = stl.style(columnTitleStyle)
-				.setBackgroundColor(new Color(140, 140, 140));
-		StyleBuilder crosstabCellStyle = stl.style(columnStyle).setBorder(
-				stl.pen1Point());
+        StyleBuilder crosstabGroupStyle = stl.style(columnTitleStyle);
+        StyleBuilder crosstabGroupTotalStyle = stl.style(columnTitleStyle)
+                .setBackgroundColor(new Color(170, 170, 170));
+        StyleBuilder crosstabGrandTotalStyle = stl.style(columnTitleStyle)
+                .setBackgroundColor(new Color(140, 140, 140));
+        StyleBuilder crosstabCellStyle = stl.style(columnStyle).setBorder(
+                stl.pen1Point());
 
-		reportTemplateBuilder = template().setLocale(Locale.ENGLISH)
-				.setColumnStyle(columnStyle)
-				.setColumnTitleStyle(columnTitleStyle)
-				.setGroupStyle(groupStyle).setGroupTitleStyle(groupStyle)
-				.setSubtotalStyle(subtotalStyle).highlightDetailEvenRows()
-				.crosstabHighlightEvenRows()
-				.setCrosstabGroupStyle(crosstabGroupStyle)
-				.setCrosstabGroupTotalStyle(crosstabGroupTotalStyle)
-				.setCrosstabGrandTotalStyle(crosstabGrandTotalStyle)
-				.setCrosstabCellStyle(crosstabCellStyle);
-	}
+        reportTemplateBuilder = template().setLocale(Locale.ENGLISH)
+                .setColumnStyle(columnStyle)
+                .setColumnTitleStyle(columnTitleStyle)
+                .setGroupStyle(groupStyle).setGroupTitleStyle(groupStyle)
+                .setSubtotalStyle(subtotalStyle).highlightDetailEvenRows()
+                .crosstabHighlightEvenRows()
+                .setCrosstabGroupStyle(crosstabGroupStyle)
+                .setCrosstabGroupTotalStyle(crosstabGroupTotalStyle)
+                .setCrosstabGrandTotalStyle(crosstabGrandTotalStyle)
+                .setCrosstabCellStyle(crosstabCellStyle);
+    }
 
-	/**
-	 * Creates custom component which is possible to add to any report band
-	 * component
-	 */
-	public ComponentBuilder<?, ?> createTitleComponent(String label) {
-		HyperLinkBuilder link = hyperLink("https://www.mycollab.com");
-		ComponentBuilder<?, ?> dynamicReportsComponent = cmp.horizontalList(
-				cmp.image(
-						ReportTemplateFactory.class.getClassLoader()
-								.getResourceAsStream("images/logo.png"))
-						.setFixedDimension(150, 28), cmp.verticalList(
-						cmp.text(label)
-								.setStyle(bold22CenteredStyle)
-								.setHorizontalAlignment(
-										HorizontalAlignment.LEFT),
-						cmp.text("https://www.mycollab.com")
-								.setStyle(italicStyle).setHyperLink(link)));
+    /**
+     * Creates custom component which is possible to add to any report band
+     * component
+     */
+    public ComponentBuilder<?, ?> createTitleComponent(String label) {
+        HyperLinkBuilder link = hyperLink("https://www.mycollab.com");
+        ComponentBuilder<?, ?> dynamicReportsComponent = cmp.horizontalList(
+                cmp.image(ReportTemplateFactory.class.getClassLoader().getResourceAsStream("images/logo.png"))
+                        .setFixedDimension(150, 28), cmp.verticalList(cmp.text(label)
+                                .setStyle(bold22CenteredStyle).setHorizontalTextAlignment(HorizontalTextAlignment.LEFT),
+                        cmp.text("https://www.mycollab.com").setStyle(italicStyle).setHyperLink(link)));
 
-		return cmp.horizontalList().add(dynamicReportsComponent).newRow()
-				.add(cmp.line()).newRow().add(cmp.verticalGap(10));
-	}
+        return cmp.horizontalList().add(dynamicReportsComponent).newRow()
+                .add(cmp.line()).newRow().add(cmp.verticalGap(10));
+    }
 }
