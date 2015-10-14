07caca1e53bbba72ab5fbc0aa9b21679e32681a4
Hai Nguyen <hainguyen@esofthead.com>
2015-10-14 17:51:36 +0700

    Update

--- mycollab-app-ondemand/src/main/webapp/VAADIN/widgetsets/com.esofthead.mycollab.widgetset.MyCollabWidgetSet/deferredjs/661B9DC67F53904093F7A4F15A3E2CBD/2.cache.js
+++
@@ -1,1 +0,0 @@
-$wnd.com_esofthead_mycollab_widgetset_MyCollabWidgetSet.runAsyncCallback2('Sxb(1878,1,KZe);_.me=function cMc(){Src((!Lrc&&(Lrc=new Xrc),Lrc),this.a.d)};mQe(Cq)(2);\n//# sourceURL=com.esofthead.mycollab.widgetset.MyCollabWidgetSet-2.js\n')
