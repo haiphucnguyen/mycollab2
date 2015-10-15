a79741c8f8e6e3857fc6fbe86923c15872a16098
Hai Nguyen <hainguyen@esofthead.com>
2015-10-15 11:23:24 +0700

    Update

--- mycollab-core/src/main/java/com/esofthead/mycollab/core/MyCollabVersion.java
+++ mycollab-core/src/main/java/com/esofthead/mycollab/core/MyCollabVersion.java
@@ -29,7 +29,7 @@ public class MyCollabVersion {
     public static final String THEME_VERSION = "mycollab_20151015";
 
 	public static String getVersion() {
-		return "5.1.4";
+		return "5.2.0";
 	}
 
     static int[] getVersionNumbers(String ver) {
