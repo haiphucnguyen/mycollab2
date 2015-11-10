52a68e3460ea063d614ce0c7c3f281b2fc0e47fd
Hai Nguyen <hainguyen@esofthead.com>
2015-11-10 13:58:29 +0700

    Update

---
+++ mycollab-migration/src/main/java/db/migration/V20151109_2__Simplify_Theme_Management.java
@@ -0,0 +1,19 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.spring.SpringJdbcMigration;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+/**
+ * @author MyCollab Ltd
+ * @since 5.2.2
+ */
+public class V20151109_2__Simplify_Theme_Management implements SpringJdbcMigration {
+    @Override
+    public void migrate(JdbcTemplate jdbcTemplate) throws Exception {
+        jdbcTemplate.execute("ALTER TABLE `s_account_theme` \n" +
+                "DROP COLUMN `tabsheetTextSelected`,\n" +
+                "DROP COLUMN `tabsheetText`,\n" +
+                "DROP COLUMN `tabsheetBgSelected`,\n" +
+                "DROP COLUMN `tabsheetBg`;");
+    }
+}
