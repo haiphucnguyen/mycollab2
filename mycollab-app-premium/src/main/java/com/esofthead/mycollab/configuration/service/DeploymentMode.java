172898d3aacfd947629829d73557bf23795ced13
Hai Nguyen <hainguyen@esofthead.com>
2015-11-14 22:37:59 +0700

    Update

--- mycollab-app-premium/src/main/java/com/esofthead/mycollab/configuration/service/DeploymentMode.java
+++ mycollab-app-premium/src/main/java/com/esofthead/mycollab/configuration/service/DeploymentMode.java
@@ -1,11 +1,13 @@
 package com.esofthead.mycollab.configuration.service;
 
 import com.esofthead.mycollab.configuration.IDeploymentMode;
+import org.springframework.stereotype.Service;
 
 /**
  * @author MyCollab Ltd
  * @since 5.2.2
  */
+@Service
 public class DeploymentMode implements IDeploymentMode {
     @Override
     public boolean isDemandEdition() {
