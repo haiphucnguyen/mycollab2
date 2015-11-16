3e46638158ba056764e7f8c81eacc2f9960079e4
Hai Nguyen <hainguyen@esofthead.com>
2015-11-16 14:40:44 +0700

    Update

--- mycollab-app-ondemand/src/main/java/com/esofthead/mycollab/configuration/service/DeploymentMode.java
+++ mycollab-app-ondemand/src/main/java/com/esofthead/mycollab/configuration/service/DeploymentMode.java
@@ -1,12 +1,14 @@
 package com.esofthead.mycollab.configuration.service;
 
 import com.esofthead.mycollab.configuration.IDeploymentMode;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 /**
  * @author MyCollab Ltd
  * @since 5.2.2
  */
+@Order(value=1)
 @Service
 public class DeploymentMode implements IDeploymentMode {
     @Override
--- mycollab-app-premium/src/main/java/com/esofthead/mycollab/configuration/service/DeploymentMode.java
+++ mycollab-app-premium/src/main/java/com/esofthead/mycollab/configuration/service/DeploymentMode.java
@@ -1,6 +1,7 @@
 package com.esofthead.mycollab.configuration.service;
 
 import com.esofthead.mycollab.configuration.IDeploymentMode;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Service;
 
 /**
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
  * @since 5.2.2
  */
 @Service
+@Order(value = 1)
 public class DeploymentMode implements IDeploymentMode {
     @Override
     public boolean isDemandEdition() {
