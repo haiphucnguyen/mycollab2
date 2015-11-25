c0cb375137a176f18939f6e3cf70f29013bc6713
Hai Nguyen <hainguyen@esofthead.com>
2015-11-25 11:45:33 +0700

    Update

--- mycollab-mobile/src/main/java/com/esofthead/mycollab/mobile/module/crm/view/contact/ContactReadFormFieldFactory.java
+++ mycollab-mobile/src/main/java/com/esofthead/mycollab/mobile/module/crm/view/contact/ContactReadFormFieldFactory.java
@@ -1,25 +1,25 @@
 /**
  * This file is part of mycollab-mobile.
  *
- * mycollab-web is free software: you can redistribute it and/or modify
+ * mycollab-mobile is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * mycollab-web is distributed in the hope that it will be useful,
+ * mycollab-mobile is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with mycollab-web.  If not, see <http://www.gnu.org/licenses/>.
+ * along with mycollab-mobile.  If not, see <http://www.gnu.org/licenses/>.
  */
 package com.esofthead.mycollab.mobile.module.crm.view.contact;
 
 import com.esofthead.mycollab.common.i18n.GenericI18Enum;
-import com.esofthead.mycollab.vaadin.ui.AbstractBeanFieldGroupViewFieldFactory;
 import com.esofthead.mycollab.module.crm.domain.SimpleContact;
 import com.esofthead.mycollab.vaadin.AppContext;
+import com.esofthead.mycollab.vaadin.ui.AbstractBeanFieldGroupViewFieldFactory;
 import com.esofthead.mycollab.vaadin.ui.GenericBeanForm;
 import com.esofthead.mycollab.vaadin.ui.form.field.DefaultViewField;
 import com.esofthead.mycollab.vaadin.ui.form.field.EmailViewField;
