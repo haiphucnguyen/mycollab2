f7e90d15ad57eae23f830ce1caa499eb84958695
Hai Nguyen <hainguyen@esofthead.com>
2016-01-12 07:37:30 +0700

    Update

--- mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/I18nValueComboBox.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/vaadin/ui/I18nValueComboBox.java
@@ -1,19 +1,3 @@
-/**
- * This file is part of mycollab-ui.
- *
- * mycollab-ui is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mycollab-ui is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with mycollab-ui.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.esofthead.mycollab.vaadin.ui;
 
 import com.esofthead.mycollab.vaadin.AppContext;
@@ -25,12 +9,11 @@ import java.util.List;
  * @author MyCollab Ltd.
  * @since 4.3.0
  */
-public class I18nValueComboBox extends StyleComboBox {
+public class I18nValueComboBox extends ValueComboBox {
     private static final long serialVersionUID = 1L;
 
     public I18nValueComboBox() {
         super();
-//        this.setPageLength(20);
     }
 
     public I18nValueComboBox(boolean nullIsAllowable, Enum<?>... keys) {
