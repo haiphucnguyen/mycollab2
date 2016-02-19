b2febc97925cbaf6ac80c1b4ed90834d0e4f508f
Hai Nguyen <hainguyen@esofthead.com>
2016-02-20 01:37:53 +0700

    Update

--- mycollab-web-ondemand/src/main/java/com/esofthead/mycollab/vaadin/ui/service/GoogleAnalyticsServiceImpl.java
+++ mycollab-web-ondemand/src/main/java/com/esofthead/mycollab/vaadin/ui/service/GoogleAnalyticsServiceImpl.java
@@ -1,7 +1,9 @@
 package com.esofthead.mycollab.vaadin.ui.service;
 
+import com.esofthead.mycollab.configuration.IDeploymentMode;
 import com.esofthead.mycollab.vaadin.ui.MyCollabSession;
 import com.vaadin.ui.UI;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.vaadin.googleanalytics.tracking.GoogleAnalyticsTracker;
 
@@ -11,19 +13,25 @@ import org.vaadin.googleanalytics.tracking.GoogleAnalyticsTracker;
  */
 @Service
 public class GoogleAnalyticsServiceImpl implements GoogleAnalyticsService {
+    @Autowired
+    private IDeploymentMode deploymentMode;
 
     @Override
     public void registerUI(UI ui) {
-        GoogleAnalyticsTracker tracker = new GoogleAnalyticsTracker("UA-46116533-1", "mycollab.com");
-        tracker.extend(ui);
-        MyCollabSession.putVariable("tracker", tracker);
+        if (deploymentMode.isDemandEdition()) {
+            GoogleAnalyticsTracker tracker = new GoogleAnalyticsTracker("UA-46116533-1", "mycollab.com");
+            tracker.extend(ui);
+            MyCollabSession.putVariable("tracker", tracker);
+        }
     }
 
     @Override
     public void trackPageView(String pageId) {
-        GoogleAnalyticsTracker tracker = (GoogleAnalyticsTracker) MyCollabSession.getVariable("tracker");
-        if (tracker != null) {
-            tracker.trackPageview(pageId);
+        if (deploymentMode.isDemandEdition()) {
+            GoogleAnalyticsTracker tracker = (GoogleAnalyticsTracker) MyCollabSession.getVariable("tracker");
+            if (tracker != null) {
+                tracker.trackPageview(pageId);
+            }
         }
     }
 }
