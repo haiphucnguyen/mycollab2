e5653246c5814c0a99089b8782df1021c5d7bede
Hai Nguyen <hainguyen@esofthead.com>
2016-02-24 20:14:23 +0700

    Update

--- mycollab-services-ondemand/src/main/java/com/esofthead/mycollab/module/support/dao/BillingAccountMapperExt.java
+++ mycollab-services-ondemand/src/main/java/com/esofthead/mycollab/module/support/dao/BillingAccountMapperExt.java
@@ -1,8 +1,16 @@
 package com.esofthead.mycollab.module.support.dao;
 
+import com.esofthead.mycollab.module.support.domain.SimpleBillingAccount;
+import com.esofthead.mycollab.module.support.domain.criteria.BillingAccountSearchCriteria;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.session.RowBounds;
+
+import java.util.List;
+
 /**
  * @author MyCollab Ltd
  * @since 5.2.8
  */
 public interface BillingAccountMapperExt {
+    List<SimpleBillingAccount> findPagableListByCriteria(@Param("searchCriteria") BillingAccountSearchCriteria criteria, RowBounds rowBounds);
 }
