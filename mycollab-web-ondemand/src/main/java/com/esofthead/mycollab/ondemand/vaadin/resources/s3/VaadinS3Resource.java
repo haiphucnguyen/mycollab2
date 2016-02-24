0889202e7c20080becfb27491a0f75a293e8a91f
Hai Nguyen <hainguyen@esofthead.com>
2016-02-24 07:26:53 +0700

    Update

--- mycollab-web-ondemand/src/main/java/com/esofthead/mycollab/ondemand/vaadin/resources/s3/VaadinS3Resource.java
+++ mycollab-web-ondemand/src/main/java/com/esofthead/mycollab/ondemand/vaadin/resources/s3/VaadinS3Resource.java
@@ -4,15 +4,13 @@ import com.esofthead.mycollab.vaadin.resources.VaadinResource;
 import com.vaadin.server.Resource;
 
 /**
- * 
  * @author MyCollab Ltd.
  * @since 4.5.1
- *
  */
 public class VaadinS3Resource extends VaadinResource {
 
-	@Override
-	public Resource getStreamResource(String documentPath) {
-		return new S3StreamDownloadResource(documentPath);
-	}
+    @Override
+    public Resource getStreamResource(String documentPath) {
+        return new S3StreamDownloadResource(documentPath);
+    }
 }
