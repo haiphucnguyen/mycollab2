ad22ac20360c316aa3cae95a084231ceef5b88af
Hai Nguyen <hainguyen@esofthead.com>
2016-02-25 09:11:18 +0700

    Update

--- mycollab-api/src/main/java/com/esofthead/mycollab/rest/server/resource/GenericController.java
+++ mycollab-api/src/main/java/com/esofthead/mycollab/rest/server/resource/GenericController.java
@@ -25,7 +25,7 @@ public class GenericController {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity rulesFoException(HttpServletRequest req, Exception e) {
-        LOG.error("Error", e);
+        LOG.error("Error " + req.getContextPath(), e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
