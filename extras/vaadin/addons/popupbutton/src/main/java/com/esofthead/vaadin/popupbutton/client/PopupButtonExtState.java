2b5008113ec8d1671e2234c3fbe3e141400a7eff
Hai Nguyen <hainguyen@esofthead.com>
2016-03-08 22:13:51 +0700

    Update

--- extras/vaadin/addons/popupbutton/src/main/java/com/esofthead/vaadin/popupbutton/client/PopupButtonExtState.java
+++
@@ -1,8 +0,0 @@
-package com.esofthead.vaadin.popupbutton.client;
-
-import org.vaadin.hene.popupbutton.widgetset.client.ui.PopupButtonState;
-
-public class PopupButtonExtState extends PopupButtonState {
-	private static final long serialVersionUID = -8654320275299112250L;
-
-}
