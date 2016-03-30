982558faad25a7175ba73c7f77d9846c995ca4a5
Hai Nguyen <hainguyen@esofthead.com>
2016-03-30 16:16:39 +0700

    Update

--- mycollab-web/src/main/java/com/esofthead/mycollab/shell/view/MainViewImpl.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/shell/view/MainViewImpl.java
@@ -1,16 +1,16 @@
 /**
  * This file is part of mycollab-web.
- *
+ * <p>
  * mycollab-web is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ * <p>
  * mycollab-web is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU General Public License
  * along with mycollab-web.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -78,6 +78,7 @@ import org.vaadin.hene.popupbutton.PopupButton;
 import org.vaadin.sliderpanel.SliderPanel;
 import org.vaadin.sliderpanel.SliderPanelBuilder;
 import org.vaadin.sliderpanel.client.SliderMode;
+import org.vaadin.sliderpanel.client.SliderPanelListener;
 import org.vaadin.sliderpanel.client.SliderTabPosition;
 import org.vaadin.teemu.VaadinIcons;
 import org.vaadin.viritin.button.MButton;
@@ -147,9 +148,7 @@ public final class MainViewImpl extends AbstractPageView implements MainView {
 
         IDeploymentMode mode = ApplicationContextUtil.getSpringBean(IDeploymentMode.class);
         if (mode.isCommunityEdition()) {
-            SliderPanel sliderPanel = new SliderPanelBuilder(new CommunitySliderContent())
-                    .caption("Community").flowInContent(true).mode(SliderMode.RIGHT)
-                    .tabPosition(SliderTabPosition.MIDDLE).build();
+            SliderPanel sliderPanel = CommunitySliderPanel.buildCommunitySliderPanel();
             bodyLayout.addComponent(sliderPanel);
         }
         bodyLayout.setExpandRatio(widget, 1.0f);
