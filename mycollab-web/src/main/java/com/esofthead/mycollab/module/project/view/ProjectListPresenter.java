de5c4d2a3b9fdf6cf0fb18fb31863c62de4ffe34
Hai Nguyen <hainguyen@esofthead.com>
2016-04-09 09:00:45 +0700

    Update

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/ProjectListPresenter.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/ProjectListPresenter.java
@@ -16,24 +16,19 @@
  */
 package com.esofthead.mycollab.module.project.view;
 
+import com.esofthead.mycollab.core.arguments.SetSearchField;
 import com.esofthead.mycollab.core.persistence.service.ISearchableService;
 import com.esofthead.mycollab.module.project.domain.Project;
-import com.esofthead.mycollab.module.project.domain.Risk;
 import com.esofthead.mycollab.module.project.domain.SimpleProject;
-import com.esofthead.mycollab.module.project.domain.SimpleRisk;
 import com.esofthead.mycollab.module.project.domain.criteria.ProjectSearchCriteria;
-import com.esofthead.mycollab.module.project.domain.criteria.RiskSearchCriteria;
 import com.esofthead.mycollab.module.project.service.ProjectService;
 import com.esofthead.mycollab.spring.ApplicationContextUtil;
 import com.esofthead.mycollab.vaadin.AppContext;
-import com.esofthead.mycollab.vaadin.events.ViewItemAction;
 import com.esofthead.mycollab.vaadin.mvp.ScreenData;
-import com.esofthead.mycollab.vaadin.web.ui.AbstractPresenter;
 import com.esofthead.mycollab.vaadin.web.ui.DefaultMassEditActionHandler;
 import com.esofthead.mycollab.vaadin.web.ui.ListSelectionPresenter;
-import com.esofthead.mycollab.vaadin.web.ui.MailFormWindow;
 import com.vaadin.ui.ComponentContainer;
-import com.vaadin.ui.UI;
+import org.apache.commons.collections.CollectionUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -76,8 +71,16 @@ public class ProjectListPresenter extends ListSelectionPresenter<ProjectListView
 
     @Override
     protected void onGo(ComponentContainer container, ScreenData<?> data) {
-        searchCriteria = (ProjectSearchCriteria) data.getParams();
-        doSearch(searchCriteria);
+        ProjectSearchCriteria searchCriteria = new ProjectSearchCriteria();
+        Collection<Integer> prjKeys = projectService.getProjectKeysUserInvolved(AppContext.getUsername(), AppContext.getAccountId());
+        if (CollectionUtils.isNotEmpty(prjKeys)) {
+            searchCriteria.setProjectKeys(new SetSearchField<>(prjKeys));
+            searchCriteria = (ProjectSearchCriteria) data.getParams();
+            doSearch(searchCriteria);
+        } else {
+            
+        }
+
     }
 
     @Override
