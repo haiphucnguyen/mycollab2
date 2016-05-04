730719082c110cab38d1ffd3a08dfec3ffb7d015
Hai Nguyen <hainguyen@esofthead.com>
2016-05-04 17:03:16 +0700

    Update

--- mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/time/IFinanceContainer.java
+++ mycollab-web/src/main/java/com/esofthead/mycollab/module/project/view/time/IFinanceContainer.java
@@ -16,7 +16,6 @@
  */
 package com.esofthead.mycollab.module.project.view.time;
 
-import com.esofthead.mycollab.vaadin.mvp.PageView;
 import com.esofthead.mycollab.vaadin.web.ui.InitializingView;
 import com.vaadin.ui.Component;
 
