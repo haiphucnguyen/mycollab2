99502178b548f584ee62c56ca9c912085a20dbfb
Hai Nguyen <hainguyen@esofthead.com>
2016-05-09 09:47:17 +0700

    Update

--- mycollab-web/src/main/scala/com/esofthead/mycollab/module/crm/view/CrmLocalizationTypeMap.scala
+++ mycollab-web/src/main/scala/com/esofthead/mycollab/module/crm/view/CrmLocalizationTypeMap.scala
@@ -1,47 +1,27 @@
-/**
- * This file is part of mycollab-web.
- *
- * mycollab-web is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mycollab-web is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with mycollab-web.  If not, see <http://www.gnu.org/licenses/>.
- */
 package com.esofthead.mycollab.module.crm.view
 
-import com.esofthead.mycollab.core.MyCollabException
 import com.esofthead.mycollab.module.crm.CrmTypeConstants
-import com.esofthead.mycollab.module.crm.i18n.CrmTypeI18nEnum
+import com.esofthead.mycollab.module.crm.i18n._
+import com.esofthead.mycollab.vaadin.AppContext
 
 /**
- * @author MyCollab Ltd
- * @since 5.0.9
- */
+  * @author MyCollab Ltd
+  * @since 5.0.9
+  */
 object CrmLocalizationTypeMap {
-    private val typeMap: Map[String, CrmTypeI18nEnum] = Map(
-        CrmTypeConstants.ACCOUNT -> CrmTypeI18nEnum.ACCOUNT,
-        CrmTypeConstants.CALL -> CrmTypeI18nEnum.CALL,
-        CrmTypeConstants.CAMPAIGN -> CrmTypeI18nEnum.CAMPAIGN,
-        CrmTypeConstants.CASE -> CrmTypeI18nEnum.CASES,
-        CrmTypeConstants.CONTACT -> CrmTypeI18nEnum.CONTACT,
-        CrmTypeConstants.LEAD -> CrmTypeI18nEnum.LEAD,
-        CrmTypeConstants.MEETING -> CrmTypeI18nEnum.MEETING,
-        CrmTypeConstants.OPPORTUNITY -> CrmTypeI18nEnum.OPPORTUNITY,
-        CrmTypeConstants.TASK -> CrmTypeI18nEnum.TASK)
-    typeMap.withDefaultValue(null)
 
-    def getType(key: String): CrmTypeI18nEnum = {
-        val result: CrmTypeI18nEnum = typeMap(key)
-        if (result == null) {
-            throw new MyCollabException("CAN NOT GET VALUE FOR KEY: " + key)
-        }
-        result
+  def getType(key: String): String = {
+    key match {
+      case CrmTypeConstants.ACCOUNT => AppContext.getMessage(AccountI18nEnum.SINGLE)
+      case CrmTypeConstants.CALL => AppContext.getMessage(CallI18nEnum.SINGLE)
+      case CrmTypeConstants.CAMPAIGN => AppContext.getMessage(CampaignI18nEnum.SINGLE)
+      case CrmTypeConstants.CASE => AppContext.getMessage(CaseI18nEnum.SINGLE)
+      case CrmTypeConstants.CONTACT => AppContext.getMessage(ContactI18nEnum.SINGLE)
+      case CrmTypeConstants.LEAD => AppContext.getMessage(LeadI18nEnum.SINGLE)
+      case CrmTypeConstants.MEETING => AppContext.getMessage(MeetingI18nEnum.SINGLE)
+      case CrmTypeConstants.OPPORTUNITY => AppContext.getMessage(OpportunityI18nEnum.SINGLE)
+      case CrmTypeConstants.TASK => AppContext.getMessage(TaskI18nEnum.SINGLE)
+      case _ => ""
     }
+  }
 }
