f6a81c7483f7424bf2ecb6a16a861c6f66840a05
Hai Nguyen <hainguyen@esofthead.com>
2016-05-23 21:37:50 +0700

    Update

--- mycollab-esb/src/main/scala/com/esofthead/mycollab/module/user/esb/impl/SendUserInvitationCommand.scala
+++ mycollab-esb/src/main/scala/com/esofthead/mycollab/module/user/esb/impl/SendUserInvitationCommand.scala
@@ -7,7 +7,6 @@ import com.esofthead.mycollab.configuration.SiteConfiguration
 import com.esofthead.mycollab.html.LinkUtils
 import com.esofthead.mycollab.i18n.LocalizationHelper
 import com.esofthead.mycollab.module.GenericCommand
-import com.esofthead.mycollab.module.billing.RegisterStatusConstants
 import com.esofthead.mycollab.module.mail.service.{ExtMailService, IContentGenerator}
 import com.esofthead.mycollab.module.user.accountsettings.localization.UserI18nEnum
 import com.esofthead.mycollab.module.user.esb.SendUserInvitationEvent
@@ -28,27 +27,17 @@ import org.springframework.stereotype.Component
   @AllowConcurrentEvents
   @Subscribe
   def execute(event: SendUserInvitationEvent): Unit = {
-    try {
-      val inviteeUser = userService.findUserByUserNameInAccount(event.invitee, event.sAccountId)
-      contentGenerator.putVariable("urlAccept", LinkUtils.generateUserAcceptLink(event.subdomain,
-        event.sAccountId, event.invitee))
-      contentGenerator.putVariable("siteUrl", SiteConfiguration.getSiteUrl(inviteeUser.getSubdomain))
-      contentGenerator.putVariable("invitee", inviteeUser)
-      contentGenerator.putVariable("password", event.password)
-      extMailService.sendHTMLMail(SiteConfiguration.getNoReplyEmail, SiteConfiguration.getDefaultSiteName,
-        Arrays.asList(new MailRecipientField(event.invitee, event.invitee)), null, null,
-        contentGenerator.parseString(LocalizationHelper.getMessage(Locale.US,
-          UserI18nEnum.MAIL_INVITE_USER_SUBJECT, SiteConfiguration.getDefaultSiteName)),
-        contentGenerator.parseFile("templates/email/user/userInvitationNotifier.mt",
-          Locale.US), null)
-      if (inviteeUser.getRegisterstatus != RegisterStatusConstants.ACTIVE) {
-        userService.updateUserAccountStatus(event.invitee, event.sAccountId,
-          RegisterStatusConstants.SENT_VERIFICATION_EMAIL)
-      }
-    } catch {
-      case e: Exception => {
-        userService.updateUserAccountStatus(event.invitee, event.sAccountId, RegisterStatusConstants.VERIFICATING)
-      }
-    }
+    val inviteeUser = userService.findUserByUserNameInAccount(event.invitee, event.sAccountId)
+    contentGenerator.putVariable("urlAccept", LinkUtils.generateUserAcceptLink(event.subdomain,
+      event.sAccountId, event.invitee))
+    contentGenerator.putVariable("siteUrl", SiteConfiguration.getSiteUrl(inviteeUser.getSubdomain))
+    contentGenerator.putVariable("invitee", inviteeUser)
+    contentGenerator.putVariable("password", event.password)
+    extMailService.sendHTMLMail(SiteConfiguration.getNoReplyEmail, SiteConfiguration.getDefaultSiteName,
+      Arrays.asList(new MailRecipientField(event.invitee, event.invitee)), null, null,
+      contentGenerator.parseString(LocalizationHelper.getMessage(Locale.US,
+        UserI18nEnum.MAIL_INVITE_USER_SUBJECT, SiteConfiguration.getDefaultSiteName)),
+      contentGenerator.parseFile("templates/email/user/userInvitationNotifier.mt",
+        Locale.US), null)
   }
 }
