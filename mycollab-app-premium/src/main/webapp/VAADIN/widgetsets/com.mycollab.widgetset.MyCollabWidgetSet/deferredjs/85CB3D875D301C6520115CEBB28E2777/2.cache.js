2eb2ea24ec3e68044ce4f583e56baa0fa8d22e6f
Hai Nguyen <hainguyen@esofthead.com>
2016-07-06 18:39:08 +0700

    Update

---
+++ mycollab-app-premium/src/main/webapp/VAADIN/widgetsets/com.mycollab.widgetset.MyCollabWidgetSet/deferredjs/85CB3D875D301C6520115CEBB28E2777/2.cache.js
@@ -0,0 +1,1 @@
+$wnd.com_mycollab_widgetset_MyCollabWidgetSet.runAsyncCallback2('wwb(1938,1,n_e);_.xe=function JIc(){bnc((!Wmc&&(Wmc=new gnc),Wmc),this.a.d)};SSe(cq)(2);\n//# sourceURL=com.mycollab.widgetset.MyCollabWidgetSet-2.js\n')
