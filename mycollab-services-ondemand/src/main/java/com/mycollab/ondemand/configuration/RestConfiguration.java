05c8affe49d8eca867c3ecc2fc67cc1ed3b4b12c
Hai Nguyen <hainguyen@esofthead.com>
2016-08-03 17:09:14 +0700

    Update

--- mycollab-services-ondemand/src/main/java/com/mycollab/ondemand/configuration/RestConfiguration.java
+++ mycollab-services-ondemand/src/main/java/com/mycollab/ondemand/configuration/RestConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
  * @author MyCollab Ltd
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @ComponentScan(basePackages = "com.mycollab.rest.server.resource",
         includeFilters = {@ComponentScan.Filter(classes = {RestController.class})})
+@EnableWebMvc
 @Profile("production")
 public class RestConfiguration {
 }
