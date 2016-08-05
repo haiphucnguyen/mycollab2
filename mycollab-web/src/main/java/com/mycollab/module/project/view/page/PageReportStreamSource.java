1974aa6d3496d69d37e704c565114554c270ed42
Hai Nguyen <hainguyen@esofthead.com>
2016-08-05 16:13:49 +0700

    Update

---
+++ mycollab-web/src/main/java/com/mycollab/module/project/view/page/PageReportStreamSource.java
@@ -0,0 +1,30 @@
+package com.mycollab.module.project.view.page;
+
+import com.mycollab.core.MyCollabException;
+import com.mycollab.module.page.domain.Page;
+import com.mycollab.reporting.ReportStreamSource;
+
+import java.util.Map;
+
+/**
+ * @author MyCollab Ltd
+ * @since 5.4.0
+ */
+class PageReportStreamSource extends ReportStreamSource {
+
+    private Page page;
+
+    PageReportStreamSource(Page page) {
+        super(new PageReportTemplateExecutor("Page"));
+        this.page = page;
+    }
+
+    @Override
+    protected void initReportParameters(Map<String, Object> parameters) {
+        if (page != null) {
+            parameters.put("bean", page);
+        } else {
+            throw new MyCollabException("Bean must be not null");
+        }
+    }
+}
