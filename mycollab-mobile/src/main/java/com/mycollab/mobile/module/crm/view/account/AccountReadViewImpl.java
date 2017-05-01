3889ef605e32f6df58fd49f886cd2aa2c0b84a04
Hai Nguyen <hainguyen@esofthead.com>
2017-05-01 22:23:21 +0700

    Update

--- mycollab-mobile/src/main/java/com/mycollab/mobile/module/crm/view/account/AccountReadViewImpl.java
+++ mycollab-mobile/src/main/java/com/mycollab/mobile/module/crm/view/account/AccountReadViewImpl.java
@@ -35,11 +35,20 @@ import com.mycollab.security.RolePermissionCollections;
 import com.mycollab.vaadin.UserUIContext;
 import com.mycollab.vaadin.events.HasPreviewFormHandlers;
 import com.mycollab.vaadin.mvp.ViewComponent;
+import com.mycollab.vaadin.touchkit.NavigationBarQuickMenu;
 import com.mycollab.vaadin.ui.AbstractBeanFieldGroupViewFieldFactory;
 import com.mycollab.vaadin.ui.IFormLayoutFactory;
+import com.mycollab.vaadin.ui.UIConstants;
+import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.ComponentContainer;
+import com.vaadin.ui.VerticalLayout;
+import org.vaadin.viritin.button.MButton;
+import org.vaadin.viritin.layouts.MHorizontalLayout;
 import org.vaadin.viritin.layouts.MVerticalLayout;
+
+import static com.mycollab.mobile.module.crm.ui.CrmPreviewFormControlsGenerator.CLONE_BTN_PRESENTED;
+import static com.mycollab.mobile.module.crm.ui.CrmPreviewFormControlsGenerator.DELETE_BTN_PRESENTED;
 
 /**
  * @author MyCollab Ltd.
@@ -100,7 +109,13 @@ public class AccountReadViewImpl extends AbstractPreviewItemComp<SimpleAccount> 
 
     @Override
     protected ComponentContainer createButtonControls() {
-        return new CrmPreviewFormControlsGenerator<>(previewForm).createButtonControls(RolePermissionCollections.CRM_ACCOUNT);
+        VerticalLayout buttonControls = new CrmPreviewFormControlsGenerator<>(previewForm)
+                .createButtonControls(CLONE_BTN_PRESENTED | DELETE_BTN_PRESENTED,
+                        RolePermissionCollections.CRM_ACCOUNT);
+        MButton editBtn = new MButton("", clickEvent -> EventBusFactory.getInstance().post(new AccountEvent.GotoEdit(this, beanItem)))
+                .withIcon(FontAwesome.EDIT).withStyleName(UIConstants.CIRCLE_BOX)
+                .withVisible(UserUIContext.canWrite(RolePermissionCollections.CRM_ACCOUNT));
+        return new MHorizontalLayout(editBtn, new NavigationBarQuickMenu(buttonControls));
     }
 
     @Override
