4724b3c122f86f483d0e1d47cca4aa9d9482de8a
Hai Nguyen <hainguyen@esofthead.com>
2017-05-05 22:30:29 +0700

    Update

---
+++ mycollab-mobile/src/main/java/com/mycollab/mobile/module/crm/view/cases/CaseListDisplayView.java
@@ -0,0 +1,29 @@
+package com.mycollab.mobile.module.crm.view.cases;
+
+import com.mycollab.common.i18n.GenericI18Enum;
+import com.mycollab.mobile.ui.AbstractMobilePageView;
+import com.mycollab.module.crm.domain.criteria.CaseSearchCriteria;
+import com.mycollab.module.crm.i18n.ContactI18nEnum;
+import com.mycollab.vaadin.UserUIContext;
+
+/**
+ * @author MyCollab Ltd
+ * @since 5.4.9
+ */
+class CaseListDisplayView extends AbstractMobilePageView {
+    private CaseSearchCriteria criteria;
+    private final CaseListDisplay itemList;
+
+    CaseListDisplayView(CaseSearchCriteria criteria) {
+        this.criteria = criteria;
+        itemList = new CaseListDisplay();
+        this.setContent(itemList);
+        displayItems();
+    }
+
+    private void displayItems() {
+        Integer numItems = itemList.search(criteria);
+        this.setCaption(UserUIContext.getMessage(GenericI18Enum.OPT_ITEM_VALUE,
+                UserUIContext.getMessage(ContactI18nEnum.SINGLE), numItems));
+    }
+}
---
+++ mycollab-mobile/src/main/java/com/mycollab/mobile/module/crm/view/cases/RelatedCaseNavigatorButton.java
@@ -0,0 +1,42 @@
+package com.mycollab.mobile.module.crm.view.cases;
+
+import com.mycollab.common.i18n.GenericI18Enum;
+import com.mycollab.db.arguments.NumberSearchField;
+import com.mycollab.module.crm.domain.criteria.CaseSearchCriteria;
+import com.mycollab.module.crm.i18n.CaseI18nEnum;
+import com.mycollab.module.crm.i18n.ContactI18nEnum;
+import com.mycollab.module.crm.service.CaseService;
+import com.mycollab.spring.AppContextUtil;
+import com.mycollab.vaadin.UserUIContext;
+import com.vaadin.addon.touchkit.ui.NavigationButton;
+
+/**
+ * @author Hai Nguyen
+ * @since 5.4.9
+ */
+public class RelatedCaseNavigatorButton extends NavigationButton {
+    private CaseSearchCriteria criteria;
+
+    public RelatedCaseNavigatorButton() {
+        super(UserUIContext.getMessage(GenericI18Enum.OPT_ITEM_VALUE,
+                UserUIContext.getMessage(CaseI18nEnum.SINGLE), 0));
+        this.addClickListener(navigationButtonClickEvent -> {
+            if (criteria != null) {
+                getNavigationManager().navigateTo(new CaseListDisplayView(criteria));
+            }
+        });
+    }
+
+    void displayTotalCases(CaseSearchCriteria criteria) {
+        this.criteria = criteria;
+        CaseService caseService = AppContextUtil.getSpringBean(CaseService.class);
+        this.setCaption(UserUIContext.getMessage(GenericI18Enum.OPT_ITEM_VALUE,
+                UserUIContext.getMessage(ContactI18nEnum.SINGLE), caseService.getTotalCount(criteria)));
+    }
+
+    public void displayRelatedByAccount(Integer accountId) {
+        CaseSearchCriteria searchCriteria = new CaseSearchCriteria();
+        searchCriteria.setAccountId(NumberSearchField.equal(accountId));
+        displayTotalCases(searchCriteria);
+    }
+}
