d66dffe47afef332b99929a580ad94ad29cc5044
ABSOLUTE\hainguyen <hainguyen@absolute.com>
2017-05-21 11:25:57 +0700

    Update

--- mycollab-mobile-premium/src/main/java/com/mycollab/premium/mobile/module/crm/view/activity/CallAddViewImpl.java
+++ mycollab-mobile-premium/src/main/java/com/mycollab/premium/mobile/module/crm/view/activity/CallAddViewImpl.java
@@ -1,22 +1,8 @@
-/**
- * This file is part of mycollab-mobile.
- *
- * mycollab-mobile is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mycollab-mobile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with mycollab-mobile.  If not, see <http://www.gnu.org/licenses/>.
- */
-package com.mycollab.mobile.module.crm.view.activity;
+package com.mycollab.premium.mobile.module.crm.view.activity;
 
 import com.mycollab.mobile.form.view.DynaFormLayout;
+import com.mycollab.mobile.module.crm.view.activity.CallAddView;
+import com.mycollab.mobile.module.crm.view.activity.CallDefaultFormLayoutFactory;
 import com.mycollab.mobile.module.user.ui.components.ActiveUserComboBox;
 import com.mycollab.mobile.ui.AbstractEditItemComp;
 import com.mycollab.mobile.ui.ValueListSelect;
--- mycollab-mobile-premium/src/main/java/com/mycollab/premium/mobile/module/crm/view/activity/MeetingEditFormFieldFactory.java
+++ mycollab-mobile-premium/src/main/java/com/mycollab/premium/mobile/module/crm/view/activity/MeetingEditFormFieldFactory.java
@@ -1,20 +1,4 @@
-/**
- * This file is part of mycollab-mobile.
- *
- * mycollab-mobile is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * mycollab-mobile is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with mycollab-mobile.  If not, see <http://www.gnu.org/licenses/>.
- */
-package com.mycollab.mobile.module.crm.view.activity;
+package com.mycollab.premium.mobile.module.crm.view.activity;
 
 import com.mycollab.module.crm.domain.MeetingWithBLOBs;
 import com.mycollab.vaadin.ui.AbstractBeanFieldGroupEditFieldFactory;
@@ -30,12 +14,11 @@ import com.vaadin.ui.TextField;
  * @since 4.1
  */
 public class MeetingEditFormFieldFactory extends AbstractBeanFieldGroupEditFieldFactory<MeetingWithBLOBs> {
+    private static final long serialVersionUID = 1L;
 
-    public MeetingEditFormFieldFactory(GenericBeanForm<MeetingWithBLOBs> form) {
+    MeetingEditFormFieldFactory(GenericBeanForm<MeetingWithBLOBs> form) {
         super(form);
     }
-
-    private static final long serialVersionUID = 1L;
 
     @Override
     protected Field<?> onCreateField(Object propertyId) {
