bc57d92e7fbed84fdbe9b7b01d6313fa19f0348f
ABSOLUTE\hainguyen <hainguyen@absolute.com>
2017-07-09 06:36:29 -0700

    Update

--- mycollab-server-runner/src/main/java/com/mycollab/server/DefaultServerRunner.java
+++ mycollab-server-runner/src/main/java/com/mycollab/server/DefaultServerRunner.java
@@ -1,10 +1,27 @@
+/**
+ * This file is part of mycollab-server-runner.
+ *
+ * mycollab-server-runner is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * mycollab-server-runner is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with mycollab-server-runner.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.mycollab.server;
 
 import com.mycollab.core.utils.FileUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -17,7 +34,8 @@ import java.net.URI;
  * @since 5.5.0
  */
 @Configuration
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class, FreeMarkerAutoConfiguration.class,
+        FlywayAutoConfiguration.class})
 @ComponentScan(basePackages = {"com.mycollab"})
 public class DefaultServerRunner {
     public static void main(String[] args) {
