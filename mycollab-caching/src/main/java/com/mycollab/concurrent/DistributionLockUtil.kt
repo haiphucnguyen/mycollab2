3d1453d1e5b3ef61e0d5b0fd754b2d86e3a07e00
Hai Nguyen <hainguyen@esofthead.com>
2017-10-11 11:06:40 +0700

    Update

--- mycollab-caching/src/main/java/com/mycollab/concurrent/DistributionLockUtil.kt
+++ mycollab-caching/src/main/java/com/mycollab/concurrent/DistributionLockUtil.kt
@@ -19,7 +19,7 @@
 /**
  * mycollab-caching - Parent pom providing dependency and plugin management for applications
 		built with Maven
- * Copyright © ${project.inceptionYear} MyCollab (support@mycollab.com)
+ * Copyright © 2017 MyCollab (support@mycollab.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- mycollab-test/src/main/java/com/mycollab/test/rule/EssentialInitRule.java
+++ mycollab-test/src/main/java/com/mycollab/test/rule/EssentialInitRule.java
@@ -1,7 +1,7 @@
 /**
  * mycollab-test - Parent pom providing dependency and plugin management for applications
 		built with Maven
- * Copyright © ${project.inceptionYear} MyCollab (support@mycollab.com)
+ * Copyright © 2017 MyCollab (support@mycollab.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
