6ef1ee7b57ca8fccf945e456fefc9fb97e11678e
Hai Nguyen <hainguyen@esofthead.com>
2017-10-12 20:37:47 +0700

    Update

--- mycollab-services-premium/src/main/java/com/mycollab/premium/module/billing/service/impl/BillingPlanCheckerServiceImpl.kt
+++ mycollab-services-premium/src/main/java/com/mycollab/premium/module/billing/service/impl/BillingPlanCheckerServiceImpl.kt
@@ -1,11 +1,13 @@
 package com.mycollab.premium.module.billing.service.impl
 
-import com.mycollab.license.LicenseInfo
+import com.mycollab.db.arguments.NumberSearchField
+import com.mycollab.db.arguments.SetSearchField
+import com.mycollab.license.service.LicenseResolver
+import com.mycollab.module.billing.RegisterStatusConstants
 import com.mycollab.module.billing.UsageExceedBillingPlanException
 import com.mycollab.module.billing.service.BillingPlanCheckerService
-import com.mycollab.module.user.service.UserService
-import com.mycollab.license.service.LicenseResolver
-import org.springframework.beans.factory.annotation.Autowired
+import com.mycollab.module.user.dao.UserMapperExt
+import com.mycollab.module.user.domain.criteria.UserSearchCriteria
 import org.springframework.stereotype.Service
 
 /**
@@ -14,15 +16,20 @@ import org.springframework.stereotype.Service
  */
 @Service
 class BillingPlanCheckerServiceImpl(private val licenseResolver: LicenseResolver,
-                                    private val userService: UserService) : BillingPlanCheckerService {
+                                    private val userMapper: UserMapperExt) : BillingPlanCheckerService {
 
     @Throws(UsageExceedBillingPlanException::class)
-    override fun validateAccountCanCreateMoreProject(sAccountId: Int) {}
+    override fun validateAccountCanCreateMoreProject(sAccountId: Int) {
+    }
 
     @Throws(UsageExceedBillingPlanException::class)
     override fun validateAccountCanCreateNewUser(sAccountId: Int) {
         val licenseInfo = licenseResolver.licenseInfo
-        val numOfUsers = userService.getTotalActiveUsersInAccount(sAccountId)
+
+        val criteria = UserSearchCriteria()
+        criteria.registerStatuses = SetSearchField(RegisterStatusConstants.ACTIVE)
+        criteria.saccountid = NumberSearchField(sAccountId)
+        val numOfUsers = userMapper.getTotalCount(criteria)
         if (licenseInfo != null) {
             if (numOfUsers >= licenseInfo.maxUsers) {
                 throw UsageExceedBillingPlanException()
@@ -31,5 +38,6 @@ class BillingPlanCheckerServiceImpl(private val licenseResolver: LicenseResolver
     }
 
     @Throws(UsageExceedBillingPlanException::class)
-    override fun validateAccountCanUploadMoreFiles(sAccountId: Int, extraBytes: Long) {}
+    override fun validateAccountCanUploadMoreFiles(sAccountId: Int, extraBytes: Long) {
+    }
 }
