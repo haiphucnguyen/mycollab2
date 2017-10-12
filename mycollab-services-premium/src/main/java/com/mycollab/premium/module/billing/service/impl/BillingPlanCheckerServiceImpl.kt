423f8930dcb7d3183c92ce3569d607358cc0446c
Hai Nguyen <hainguyen@esofthead.com>
2017-10-12 19:11:13 +0700

    Update

---
+++ mycollab-services-premium/src/main/java/com/mycollab/premium/module/billing/service/impl/BillingPlanCheckerServiceImpl.kt
@@ -0,0 +1,35 @@
+package com.mycollab.premium.module.billing.service.impl
+
+import com.mycollab.license.LicenseInfo
+import com.mycollab.module.billing.UsageExceedBillingPlanException
+import com.mycollab.module.billing.service.BillingPlanCheckerService
+import com.mycollab.module.user.service.UserService
+import com.mycollab.license.service.LicenseResolver
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+/**
+ * @author MyCollab Ltd.
+ * @since 4.1
+ */
+@Service
+class BillingPlanCheckerServiceImpl(private val licenseResolver: LicenseResolver,
+                                    private val userService: UserService) : BillingPlanCheckerService {
+
+    @Throws(UsageExceedBillingPlanException::class)
+    override fun validateAccountCanCreateMoreProject(sAccountId: Int) {}
+
+    @Throws(UsageExceedBillingPlanException::class)
+    override fun validateAccountCanCreateNewUser(sAccountId: Int) {
+        val licenseInfo = licenseResolver.licenseInfo
+        val numOfUsers = userService.getTotalActiveUsersInAccount(sAccountId!!)
+        if (licenseInfo != null) {
+            if (numOfUsers >= licenseInfo.maxUsers) {
+                throw UsageExceedBillingPlanException()
+            }
+        }
+    }
+
+    @Throws(UsageExceedBillingPlanException::class)
+    override fun validateAccountCanUploadMoreFiles(sAccountId: Int, extraBytes: Long) {}
+}
---
+++ mycollab-services-premium/src/main/java/com/mycollab/premium/module/file/service/RawContentServiceFactoryBean.kt
@@ -0,0 +1,29 @@
+package com.mycollab.premium.module.file.service
+
+import com.mycollab.cache.IgnoreCacheClass
+import com.mycollab.db.persistence.service.IService
+import com.mycollab.module.file.service.RawContentService
+import com.mycollab.module.file.service.impl.FileRawContentServiceImpl
+import org.springframework.beans.factory.config.AbstractFactoryBean
+import org.springframework.stereotype.Service
+
+/**
+ * Factory spring bean to solve resolution of MyCollab raw content service
+ * should be `FileRawContentServiceImpl` if MyCollab is installed in
+ * local server (dev, community or premium mode) or
+ * `AmazonRawContentServiceImpl` if MyCollab is installed on MyCollab
+ * server.
+ */
+@Service
+@IgnoreCacheClass
+class RawContentServiceFactoryBean : AbstractFactoryBean<RawContentService>(), IService {
+
+    @Throws(Exception::class)
+    override fun createInstance(): RawContentService {
+        return FileRawContentServiceImpl()
+    }
+
+    override fun getObjectType(): Class<RawContentService> {
+        return RawContentService::class.java
+    }
+}
