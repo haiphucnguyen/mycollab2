4b9b7ec4c24283158696e350197e372524a417a4
Hai Nguyen <hainguyen@esofthead.com>
2017-10-12 19:01:51 +0700

    Update

--- mycollab-app-community/src/main/scripts/service.bat
+++ mycollab-app-community/src/main/scripts/service.bat
@@ -43,13 +43,13 @@ set PR_CLASSPATH=%MYCOLLAB_HOME%\executor.jar
 REM Startup configuration
 set PR_STARTUP=auto
 set PR_STARTMODE=jvm
-set PR_STARTCLASS=com.esofthead.mycollab.Executor
+set PR_STARTCLASS=com.mycollab.runner.Executor
 set PR_STARTPATH=%MYCOLLAB_HOME%
 set PR_STARTMETHOD=start
 
 REM Shutdown configuration
 set PR_STOPMODE=jvm
-set PR_STOPCLASS=com.esofthead.mycollab.Executor
+set PR_STOPCLASS=com.mycollab.runner.Executor
 set PR_STOPMETHOD=stop
 set PR_STOPPATH=%MYCOLLAB_HOME%
 
