d151ffaf198e514cdcb70152f08bb84f33d0ddf4
Hai Nguyen <haiphucnguyen@gmail.com>
2018-04-09 13:02:36 +0700

    Update to vaadin 8

--- mycollab-web/src/main/java/com/mycollab/module/crm/view/campaign/CampaignSelectionField.java
+++ mycollab-web/src/main/java/com/mycollab/module/crm/view/campaign/CampaignSelectionField.java
@@ -23,8 +23,6 @@ import com.mycollab.spring.AppContextUtil;
 import com.mycollab.vaadin.AppUI;
 import com.mycollab.vaadin.ui.FieldSelection;
 import com.mycollab.vaadin.web.ui.WebThemes;
-import com.vaadin.data.Property;
-import com.vaadin.data.Validator;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.ui.*;
 import org.vaadin.viritin.button.MButton;
@@ -34,29 +32,30 @@ import org.vaadin.viritin.layouts.MHorizontalLayout;
  * @author MyCollab Ltd.
  * @since 1.0
  */
+// TODO: revise
 public class CampaignSelectionField extends CustomField<Integer> implements FieldSelection<CampaignWithBLOBs> {
     private CampaignWithBLOBs internalValue = new CampaignWithBLOBs();
 
     private TextField campaignName = new TextField();
 
-    @Override
-    public void setPropertyDataSource(Property newDataSource) {
-        Object value = newDataSource.getValue();
-        if (value instanceof Integer) {
-            setCampaignByVal((Integer) value);
-        }
-        super.setPropertyDataSource(newDataSource);
-    }
-
-    @Override
-    public void commit() throws SourceException, Validator.InvalidValueException {
-        if (internalValue != null) {
-            setInternalValue(internalValue.getId());
-        } else {
-            setInternalValue(null);
-        }
-        super.commit();
-    }
+//    @Override
+//    public void setPropertyDataSource(Property newDataSource) {
+//        Object value = newDataSource.getValue();
+//        if (value instanceof Integer) {
+//            setCampaignByVal((Integer) value);
+//        }
+//        super.setPropertyDataSource(newDataSource);
+//    }
+//
+//    @Override
+//    public void commit() throws SourceException, Validator.InvalidValueException {
+//        if (internalValue != null) {
+//            setInternalValue(internalValue.getId());
+//        } else {
+//            setInternalValue(null);
+//        }
+//        super.commit();
+//    }
 
     private void setCampaignByVal(Integer campaignId) {
         CampaignService campaignService = AppContextUtil.getSpringBean(CampaignService.class);
@@ -88,15 +87,20 @@ public class CampaignSelectionField extends CustomField<Integer> implements Fiel
     }
 
     @Override
-    public Class<Integer> getType() {
-        return Integer.class;
-    }
-
-    @Override
     public void fireValueChange(CampaignWithBLOBs data) {
         this.internalValue = data;
         if (internalValue != null) {
             campaignName.setValue(internalValue.getCampaignname());
         }
+    }
+
+    @Override
+    protected void doSetValue(Integer integer) {
+
+    }
+
+    @Override
+    public Integer getValue() {
+        return null;
     }
 }
