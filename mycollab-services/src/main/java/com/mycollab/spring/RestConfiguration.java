db7cb85619245a204c8f3b65855e2da60c5485f4
hainguyen <haiphucnguyen@gmail.com>
2018-06-04 17:59:24 +0700

    Update

--- mycollab-services/src/main/java/com/mycollab/spring/RestConfiguration.java
+++ mycollab-services/src/main/java/com/mycollab/spring/RestConfiguration.java
@@ -16,11 +16,19 @@
  */
 package com.mycollab.spring;
 
+import org.springframework.boot.autoconfigure.web.WebMvcRegistrationsAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.AnnotationUtils;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.Method;
 
 /**
  * @author MyCollab Ltd
@@ -32,4 +40,32 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @EnableWebMvc
 @Profile("production")
 public class RestConfiguration {
+
+    @Bean
+    public WebMvcRegistrationsAdapter webMvcRegistrationsHandlerMapping() {
+        return new WebMvcRegistrationsAdapter() {
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return new RequestMappingHandlerMapping() {
+                    private final static String API_BASE_PATH = "api";
+
+                    @Override
+                    protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
+                        Class<?> beanType = method.getDeclaringClass();
+                        if (AnnotationUtils.findAnnotation(beanType, RestController.class) != null) {
+                            PatternsRequestCondition apiPattern = new PatternsRequestCondition(API_BASE_PATH)
+                                    .combine(mapping.getPatternsCondition());
+
+                            mapping = new RequestMappingInfo(mapping.getName(), apiPattern,
+                                    mapping.getMethodsCondition(), mapping.getParamsCondition(),
+                                    mapping.getHeadersCondition(), mapping.getConsumesCondition(),
+                                    mapping.getProducesCondition(), mapping.getCustomCondition());
+                        }
+
+                        super.registerHandlerMethod(handler, method, mapping);
+                    }
+                };
+            }
+        };
+    }
 }
