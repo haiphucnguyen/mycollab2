b686f4f7d0c7dfb89c297d2c792fef80c45b3084
hainguyen <haiphucnguyen@gmail.com>
2018-12-02 08:46:45 -0600

    Update

--- mycollab-api/src/main/java/com/mycollab/rest/server/resource/CampaignController.kt
+++ mycollab-api/src/main/java/com/mycollab/rest/server/resource/CampaignController.kt
@@ -1,15 +1,15 @@
 package com.mycollab.rest.server.resource
 
-import com.mycollab.common.domain.MailRecipientField
 import com.mycollab.configuration.ApplicationConfiguration
 import com.mycollab.core.Version
 import com.mycollab.core.utils.FileUtils
+import com.mycollab.core.utils.StringUtils
 import com.mycollab.module.mail.service.ExtMailService
 import com.mycollab.module.mail.service.IContentGenerator
+import com.mycollab.ondemand.configuration.EditionInfo
 import com.mycollab.ondemand.module.support.dao.CommunityLeadMapper
 import com.mycollab.ondemand.module.support.domain.CommunityLead
 import com.mycollab.ondemand.module.support.domain.CommunityLeadExample
-import com.mycollab.ondemand.configuration.EditionInfo
 import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.joda.time.LocalDate
@@ -39,53 +39,56 @@ class CampaignController(private val communityLeadMapper: CommunityLeadMapper,
 
     @CrossOrigin
     @RequestMapping(path = ["/register-ce"], method = [(RequestMethod.POST)], headers = ["Content-Type=application/x-www-form-urlencoded", "Accept=application/json"])
-    fun registerCE(@RequestParam("firstname") firstname: String,
-                   @RequestParam("lastname") lastname: String,
-                   @RequestParam("email") email: String,
-                   @RequestParam("role") role: String,
-                   @RequestParam("company") company: String,
-                   @RequestParam("phone") phone: String,
-                   @RequestParam("country") country: String,
+    fun registerCE(@RequestParam("firstname", required = false) firstname: String,
+                   @RequestParam("lastname", required = false) lastname: String,
+                   @RequestParam("email", required = false) email: String,
+                   @RequestParam("role", required = false) role: String,
+                   @RequestParam("company", required = false) company: String,
+                   @RequestParam("phone", required = false) phone: String,
+                   @RequestParam("country", required = false) country: String,
                    @RequestParam("edition") edition: String): Map<*, *> {
 
         object : Thread() {
             override fun run() {
-                val communityLead = CommunityLead()
-                communityLead.firstname = firstname
-                communityLead.lastname = lastname
-                communityLead.email = email
-                communityLead.company = company
-                communityLead.role = role
-                communityLead.phone = phone
-                communityLead.country = country
-                communityLead.registerdate = LocalDate().toDate()
-                communityLead.version = Version.getVersion()
-                communityLead.edition = edition
+                if (StringUtils.isNotBlank(firstname) && StringUtils.isNotBlank(lastname) &&
+                        StringUtils.isNotBlank(email)) {
+                    val communityLead = CommunityLead()
+                    communityLead.firstname = firstname
+                    communityLead.lastname = lastname
+                    communityLead.email = email
+                    communityLead.company = company
+                    communityLead.role = role
+                    communityLead.phone = phone
+                    communityLead.country = country
+                    communityLead.registerdate = LocalDate().toDate()
+                    communityLead.version = Version.getVersion()
+                    communityLead.edition = edition
 
-                val ex = CommunityLeadExample()
-                ex.createCriteria().andFirstnameEqualTo(firstname).andLastnameEqualTo(lastname).andEmailEqualTo(email)
-                        .andVersionEqualTo(Version.getVersion())
-                if (communityLeadMapper.countByExample(ex) == 0) {
-                    communityLeadMapper.insert(communityLead)
+                    val ex = CommunityLeadExample()
+                    ex.createCriteria().andFirstnameEqualTo(firstname).andLastnameEqualTo(lastname).andEmailEqualTo(email)
+                            .andVersionEqualTo(Version.getVersion())
+                    if (communityLeadMapper.countByExample(ex) == 0) {
+                        communityLeadMapper.insert(communityLead)
+                    }
+
+//                contentGenerator.putVariable("lastname", lastname)
+//                contentGenerator.putVariable("version", editionInfo.version)
+//                if ("Ultimate" == edition) {
+//                    contentGenerator.putVariable("downloadLink", "https://api.mycollab.com/download/verify?email=$email&&edition=Ultimate")
+//                } else {
+//                    contentGenerator.putVariable("downloadLink", "https://api.mycollab.com/download/verify?email=$email")
+//                }
+//
+//                extMailService.sendHTMLMail(applicationConfiguration.notifyEmail, applicationConfiguration.siteName,
+//                        listOf(MailRecipientField(email, "$firstname $lastname")),
+//                        "MyCollab is ready for download", contentGenerator.parseFile("mailDownloadInfo.ftl"))
                 }
-
-                contentGenerator.putVariable("lastname", lastname)
-                contentGenerator.putVariable("version", editionInfo.version)
-                if ("Ultimate" == edition) {
-                    contentGenerator.putVariable("downloadLink", "https://api.mycollab.com/download/verify?email=$email&&edition=Ultimate")
-                } else {
-                    contentGenerator.putVariable("downloadLink", "https://api.mycollab.com/download/verify?email=$email")
-                }
-
-                extMailService.sendHTMLMail(applicationConfiguration.notifyEmail, applicationConfiguration.siteName,
-                        listOf(MailRecipientField(email, "$firstname $lastname")),
-                        "MyCollab is ready for download", contentGenerator.parseFile("mailDownloadInfo.ftl"))
             }
         }.start()
 
         val name = "MyCollab-All-${editionInfo.version}.zip"
-        val link = editionInfo.communityDownloadLink
-        val altLink = editionInfo.altCommunityDownloadLink
-        return mapOf("name" to name, "link" to link, "altlink" to altLink)
+        val link = if ("Ultimate" == edition) editionInfo.premiumDownloadLink else editionInfo.communityDownloadLink
+        val altLink = ""
+        return mapOf("name" to name, "link" to link, "link" to altLink)
     }
 }
