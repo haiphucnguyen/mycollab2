b4ed8ddd8b57937874ca3245ac4ef6fb6b7338a0
hainguyen <haiphucnguyen@gmail.com>
2019-01-27 21:50:42 -0600

    * Fix set up views * Prepare the community build

--- mycollab-web/src/main/java/com/mycollab/vaadin/web/ui/WebThemes.java
+++ mycollab-web/src/main/java/com/mycollab/vaadin/web/ui/WebThemes.java
@@ -63,7 +63,7 @@ public class WebThemes {
 
     public static final String FORM_SECTION = "section";
     public static final String ARROW_BTN = "arrow-btn";
-    public static final String CURSOR_POINTER = "cursor_pointer";
+    public static final String CURSOR_POINTER = "cursor-pointer";
     public static final String HOVER_EFFECT_NOT_BOX = "hover-effect-not-box";
     public static final String FLEX_DISPLAY = "flex";
     public static final String PANEL_HEADER = "panel-header";
