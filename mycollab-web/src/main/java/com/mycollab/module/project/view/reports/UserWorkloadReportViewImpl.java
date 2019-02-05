1b685d6629b580cae2a84405c0c7a89eb200d7bf
hainguyen <haiphucnguyen@gmail.com>
2019-02-05 11:48:43 -0600

    LEt admin can view all projects

--- mycollab-web/src/main/java/com/mycollab/module/project/view/reports/UserWorkloadReportViewImpl.java
+++ mycollab-web/src/main/java/com/mycollab/module/project/view/reports/UserWorkloadReportViewImpl.java
@@ -1,15 +1,21 @@
 package com.mycollab.module.project.view.reports;
 
 import com.mycollab.core.Tuple2;
+import com.mycollab.db.arguments.BasicSearchRequest;
+import com.mycollab.db.arguments.SetSearchField;
+import com.mycollab.module.project.domain.ProjectTicket;
 import com.mycollab.module.project.domain.criteria.ProjectTicketSearchCriteria;
 import com.mycollab.module.project.service.ProjectRoleService;
 import com.mycollab.module.project.service.ProjectService;
+import com.mycollab.module.project.service.ProjectTicketService;
 import com.mycollab.security.PermissionMap;
 import com.mycollab.spring.AppContextUtil;
 import com.mycollab.vaadin.AppUI;
+import com.mycollab.vaadin.AsyncInvoker;
 import com.mycollab.vaadin.UserUIContext;
 import com.mycollab.vaadin.mvp.AbstractVerticalPageView;
 import com.mycollab.vaadin.mvp.ViewComponent;
+import com.mycollab.vaadin.ui.ELabel;
 import org.vaadin.viritin.layouts.MVerticalLayout;
 
 import java.util.List;
@@ -59,9 +65,23 @@ public class UserWorkloadReportViewImpl extends AbstractVerticalPageView impleme
         } else {
             projectsPermissions = roleService.findProjectsPermissions(UserUIContext.getUsername(), projects, AppUI.getAccountId());
         }
-        projectsPermissions.forEach(prjPermission -> {
-            System.out.println("Out");
+
+        AsyncInvoker.access(getUI(), new AsyncInvoker.PageCommand() {
+            public void run() {
+                ProjectTicketService projectTicketService = AppContextUtil.getSpringBean(ProjectTicketService.class);
+
+                projectsPermissions.forEach(prjPermission -> {
+                    Integer projectId = prjPermission.getItem1();
+                    PermissionMap permissionMap = prjPermission.getItem2();
+                    ProjectTicketSearchCriteria ticketSearchCriteria = new ProjectTicketSearchCriteria();
+                    ticketSearchCriteria.setProjectIds(new SetSearchField<>(projectId));
+                    List<ProjectTicket> ticketsByCriteria = (List<ProjectTicket>) projectTicketService.findTicketsByCriteria(new BasicSearchRequest<>(ticketSearchCriteria));
+                    ticketsByCriteria.forEach(ticket -> addComponent(new ELabel(ticket.getName())));
+                    push();
+                });
+            }
         });
+
     }
 
     @Override
