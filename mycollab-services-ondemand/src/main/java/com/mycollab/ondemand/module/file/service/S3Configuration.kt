8fd9d9647efcf1011589a02cb1645f5aa4b4f2be
hainguyen <haiphucnguyen@gmail.com>
2019-02-18 19:59:37 -0600

    Default constructor

--- mycollab-services-ondemand/src/main/java/com/mycollab/ondemand/module/file/service/S3Configuration.kt
+++ mycollab-services-ondemand/src/main/java/com/mycollab/ondemand/module/file/service/S3Configuration.kt
@@ -5,4 +5,4 @@ import org.springframework.stereotype.Component
 
 @Component
 @ConfigurationProperties(prefix = "s3")
-data class S3Configuration(var key:String, var secretKey:String, var bucket:String)
+data class S3Configuration(var key:String = "", var secretKey:String = "", var bucket:String = "")
