b40b370cf1537f56ac95f85d1735428e892c3a67
hainguyen <haiphucnguyen@gmail.com>
2019-04-13 22:59:49 -0500

    Update

--- mycollab-services-community/src/test/java/com/mycollab/module/project/service/TicketRelationServiceTest.kt
+++ mycollab-services-community/src/test/java/com/mycollab/module/project/service/TicketRelationServiceTest.kt
@@ -4,7 +4,8 @@ import com.mycollab.module.project.ProjectTypeConstants
 import com.mycollab.test.DataSet
 import com.mycollab.test.rule.DbUnitInitializerRule
 import com.mycollab.test.spring.IntegrationServiceTest
-import org.assertj.core.api.Assertions
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.groups.Tuple.tuple
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -20,6 +21,10 @@ class TicketRelationServiceTest : IntegrationServiceTest() {
     @Test
     fun testFindRelatedTickets() {
         val ticketRelations = ticketRelationService.findRelatedTickets(1, ProjectTypeConstants.BUG)
-        Assertions.assertThat(ticketRelations.size).isEqualTo(2)
+        assertThat(ticketRelations.size).isEqualTo(3)
+        assertThat(ticketRelations).extracting("ticketKey", "ticketid", "ticketName", "tickettype", "typeKey", "typeid", "typeName", "type", "rel", "ltr")
+                .contains(tuple(2, 1, "Bug 1", "Project-Bug", 1, 1, "Task 1", "Project-Task", "Duplicated", true),
+                        tuple(4, 2, "Task 2", "Project-Task", 2, 1, "Bug 1", "Project-Bug", "Block", false),
+                        tuple(2, 1, "Bug 1", "Project-Bug", 3, 2, "Bug 2", "Project-Bug", "Block", true))
     }
 }
