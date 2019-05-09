b411db4ea5946d7841355733af930c916776fb3c
hainguyen <haiphucnguyen@gmail.com>
2019-05-09 14:58:16 -0500

    Update

--- mycollab-api/src/main/java/com/mycollab/rest/server/resource/Main.kt
+++ mycollab-api/src/main/java/com/mycollab/rest/server/resource/Main.kt
@@ -4,9 +4,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.boot.autoconfigure.freemarker.FreeMarkerAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @SpringBootApplication(exclude = [FlywayAutoConfiguration::class, FreeMarkerAutoConfiguration::class],
         scanBasePackages = ["com.mycollab.**.spring", "com.mycollab.**.jobs, com.mycollab.**.configuration", "com.mycollab.rest.server.resource"])
+@EnableDiscoveryClient
 class Main
 
 fun main(args: Array<String>) {
